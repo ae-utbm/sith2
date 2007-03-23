ebbea48f73b516c3a843543ff44118ae1b8b84cf
Ayolo <ayolo@troll>
2007-03-23 23:32:49 +0000

    blurp

--- include/site.inc.php
+++ include/site.inc.php
@@ -236,10 +236,10 @@ class site extends interfaceweb
 
     $cts = new contents("AE Academy");
 
-    $last_m = @filemtime("/var/www/ae/www/var/img/webcam.mini.jpg");
+    $last_m = @filemtime("/var/www/ae/www/var/img/webcam.mini.png");
 
-    if (file_exists("/var/www/ae/www/var/img/webcam.mini.jpg"))
-      $imgsrc = $topdir . "var/img/webcam.mini.jpg?".$last_m;
+    if (file_exists("/var/www/ae/www/var/img/webcam.mini.png"))
+      $imgsrc = $topdir . "var/img/webcam.mini.png?".$last_m;
     else
       $imgsrc = $topdir . "images/na.gif";
 
