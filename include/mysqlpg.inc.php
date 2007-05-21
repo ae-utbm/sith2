d2559c73ad06fb9bd5d87ee509cc56d163d24b1c
Ayolo <ayolo@troll>
2007-05-21 02:20:37 +0000

    test svn hook

--- include/mysqlpg.inc.php
+++ include/mysqlpg.inc.php
@@ -1,10 +1,7 @@
 <?php
 class mysqlpg extends mysql {
 
-  function mysqlpg ($type = "ro") {
-
+  function mysqlpg ($type = "ro")
     $this->mysql('petitgeni', 'dc75f4d3', 'localhost', 'petitgeni');
-
-  }
 }
 ?>
