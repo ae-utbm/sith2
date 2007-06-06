404d193da5ee32b22dfdfd8b04abc4d49d142526
Feu <feu@troll>
2007-06-06 08:38:50 +0000

    debug

--- forum2/index.php
+++ forum2/index.php
@@ -51,7 +51,12 @@ if ( !$forum->is_valid() )
 
 if ( !$forum->is_right($site->user,DROIT_LECTURE) )
 {
-  header("Location: ".$wwwtopdir);
+  //header("Location: ".$wwwtopdir);
+  echo "<!--";
+  print_r($forum);
+  print_r($site->user);
+  echo "-->";
+
   exit();
 }
 
