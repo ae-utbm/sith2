5cae9188afa28c23a2eff15d0a2ae25c11964af4
Feu <feu@troll>
2007-06-06 18:47:47 +0000

    bleh

--- include/cts/forum.inc.php
+++ include/cts/forum.inc.php
@@ -95,6 +95,14 @@ class sujetslist extends stdcontents
     
     $this->buffer .= "<div class=\"forumsujetsliste\">\n";
     
+
+        $this->buffer .= "<div class=\"forumhead\">\n";
+      $this->buffer .= "<p class=\"auteur\">Auteur</p>\n";
+      $this->buffer .= "<p class=\"nbmessages\">Réponses</p>\n";
+      $this->buffer .= "<p class=\"dernier\">Dernier message</p>\n";
+      $this->buffer .= "</div>\n";
+    
+    
     foreach ( $rows as $row )
     {
       
