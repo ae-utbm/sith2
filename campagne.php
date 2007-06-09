c875129d82015beb5762b2b87fa73ed15c9cdad9
Ayolo <ayolo@troll>
2007-06-09 22:20:39 +0000

    fix

--- campagne.php
+++ campagne.php
@@ -30,115 +30,120 @@ require_once($topdir. "include/cts/sqltable.inc.php");
 $site = new site ();
 
 if ( !$site->user->is_valid() )
-	error_403();
-	
+  error_403();
+  
 $cpg = new campagne($site->db,$site->dbrw);
 
 if (  isset($_REQUEST["answord"]) && (isset($_REQUEST["discard"]) || isset($_REQUEST["reponses"])) )
 {
-	if(isset($_REQUEST["discard"]) )
-		$_REQUEST["reponses"]=array();
+  if(isset($_REQUEST["discard"]) )
+    $_REQUEST["reponses"]=array();
 
-	$cpg->load_lastest();
-	if ( $cpg->id > 0 && $_REQUEST["id_campagne"] == $cpg->id )
-		$cpg->repondre($site->user->id,$_REQUEST["reponses"]);
+  $cpg->load_lastest();
+  if ( $cpg->id > 0 && $_REQUEST["id_campagne"] == $cpg->id )
+    $cpg->repondre($site->user->id,$_REQUEST["reponses"]);
 
-	$res = new contents("Merci","Votre participation.");
-}	
+  $res = new contents("Merci","Votre participation.");
+}  
 else if ( isset($_REQUEST["id_campagne"]) )
-	$cpg->load_by_id($_REQUEST["id_campagne"]);
+  $cpg->load_by_id($_REQUEST["id_campagne"]);
 
 if ( $cpg->id > 0 && $site->user->is_in_group_id($cpg->group) && !$cpg->a_repondu($site->user->id) )
 {
-	$questions = $cpg->get_questions();
-	if (!empty($questions))
-	{
-  	$site->start_page("accueil","Campagne");	
-		
-	  $cts = new contents("Campagne");
+  $questions = $cpg->get_questions();
+  if (!empty($questions))
+  {
+    $site->start_page("accueil","Campagne");  
+    
+    $cts = new contents("Campagne");
 
     $frm = new form("discard","campagne.php",true,"POST","Recrutement : ".$cpg->nom);
-	  $frm->add_hidden("answord","true");
-		$frm->add_hidden("id_campagne",$cpg->id);
-		$frm->add_checkbox ( "discard", "Je ne souhaite pas participer.", false );	
-		$frm->add_submit("save","Enregistrer");
+    $frm->add_hidden("answord","true");
+    $frm->add_hidden("id_campagne",$cpg->id);
+    $frm->add_checkbox ( "discard", "Je ne souhaite pas participer.", false );  
+    $frm->add_submit("save","Enregistrer");
 
-		$cts->add($frm,true);
-
-		$frm = new form("apply","campagne.php".$user->id,true,"POST","Formulaire d'inscription");
-		$frm->add_hidden("answord","true");
-		$frm->add_hidden("id_campagne",$cpg->id);
-    foreach($questions as $id => $question)
-		{
-			if($question["type"]=="text")
-			{
-				$frm->add_info("<b>".$question["description"]."</b><br />");
-				$frm->add_text_field("reponses[$id]","","",false,80);
-				$frm->add_info("<br />");
-			}
-
-			elseif($question["type"]=="list")
-			{
-				$frm->add_info("<b>".$question["description"]."</b><br />");
-				$values=explode(";",$question["reponses"]);
-				foreach($values as $value)
-				{	
-					$value=explode("|", $value,2);
-					$keys[$value[0]]=$value[1];
-				}
-				$frm->add_select_field( "reponses[$id]", "", $keys);
-				$frm->add_info("<br />");
-			}
-			elseif($question["type"]=="radio")
-			{
-				$frm->add_info("<b>".$question["description"]."</b><br />");
-				$values=explode(";",$question["reponses"]);
-				foreach($values as $value)
-				{
-					$value=explode("|", $value, 2);
-					$keys=array();
-					$keys[$value[0]]=$value[1];
-					$frm->add_radiobox_field( "reponses[$id]", "", $keys, "", false, false );
-				}
-				$frm->add_info("<br />");
-			}
-			elseif($question["type"]=="checkbox")
-			{
-				$frm->add_info("<b>".$question["description"]."</b><br />");
-				$frm->add_checkbox( "reponses[$id]","");
-				$frm->add_info("<br />");
-			}
-
-		}
-
-		$frm->add_submit("save","Enregistrer");
     $cts->add($frm,true);
 
-	  $site->add_contents($cts);
-	
-	  $site->end_page();
-	  exit();
-	}
+    $frm = new form("apply","campagne.php".$user->id,true,"POST","Formulaire d'inscription");
+    $frm->add_hidden("answord","true");
+    $frm->add_hidden("id_campagne",$cpg->id);
+    foreach($questions as $id => $question)
+    {
+      if($question["type"]=="text")
+      {
+        $frm->add_info("<b>".$question["description"]."</b><br />");
+        $frm->add_text_field("reponses[$id]","","",false,80);
+        $frm->add_info("<br />");
+      }
+      elseif(($question["type"]=="textarea")
+      {
+        $frm->add_info("<b>".$question["description"]."</b><br />");
+        $frm->add_text_area("reponses[$id]","");
+        $frm->add_info("<br />");
+      }
+      elseif($question["type"]=="list")
+      {
+        $frm->add_info("<b>".$question["description"]."</b><br />");
+        $values=explode(";",$question["reponses"]);
+        foreach($values as $value)
+        {  
+          $value=explode("|", $value,2);
+          $keys[$value[0]]=$value[1];
+        }
+        $frm->add_select_field( "reponses[$id]", "", $keys);
+        $frm->add_info("<br />");
+      }
+      elseif($question["type"]=="radio")
+      {
+        $frm->add_info("<b>".$question["description"]."</b><br />");
+        $values=explode(";",$question["reponses"]);
+        foreach($values as $value)
+        {
+          $value=explode("|", $value, 2);
+          $keys=array();
+          $keys[$value[0]]=$value[1];
+          $frm->add_radiobox_field( "reponses[$id]", "", $keys, "", false, false );
+        }
+        $frm->add_info("<br />");
+      }
+      elseif($question["type"]=="checkbox")
+      {
+        $frm->add_info("<b>".$question["description"]."</b><br />");
+        $frm->add_checkbox( "reponses[$id]","");
+        $frm->add_info("<br />");
+      }
+
+    }
+
+    $frm->add_submit("save","Enregistrer");
+    $cts->add($frm,true);
+
+    $site->add_contents($cts);
+  
+    $site->end_page();
+    exit();
+  }
 }
 
 $site->start_page("accueil","Campagne");
 
 
-$req = new requete($site->db, "SELECT * FROM `cpg_campagne` ORDER BY date_debut_campagne");	
+$req = new requete($site->db, "SELECT * FROM `cpg_campagne` ORDER BY date_debut_campagne");  
 
 $site->add_contents(new sqltable(
-	"listcampagnes", 
-	"Archives", $req, "../campagne.php", 
-	"id_campagne", 
-	array(
-		"nom_campagne"=>"Campagne",
-		"date_debut_campagne"=>"Du",
-		"date_fin_campagne"=>"Au"
-		), 
-	array(), 
-	array(),
-	array( )
-	));
+                                 "listcampagnes", 
+                                 "Archives", $req, "../campagne.php", 
+                                 "id_campagne",
+                                 array(
+                                       "nom_campagne"=>"Campagne",
+                                       "date_debut_campagne"=>"Du",
+                                       "date_fin_campagne"=>"Au"
+                                      ),
+                                 array(),
+                                 array(),
+                                 array()
+                                ));
 
 $site->end_page();
 
