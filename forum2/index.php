8e8ff55f434472fff988f9c5f44f03d44d865d65
Feu <feu@troll>
2007-06-21 21:22:22 +0000

    wip

--- forum2/index.php
+++ forum2/index.php
@@ -542,7 +542,7 @@ if ( $sujet->is_valid() )
   if ( $site->user->is_valid() )
   {
     $row = $sujet->get_user_infos($site->user->id);
-    
+    print_r($row);
     if ( is_null($row) && $row['etoile_sujet'] != 1 )
       $cts->add_paragraph("<a href=\"?id_sujet=".$sujet->id."&amp;page=reply\"><img src=\"".$wwwtopdir."images/icons/16/message.png\" class=\"icon\" alt=\"\" />Répondre</a> <a href=\"?id_sujet=".$sujet->id."&amp;action=star\"><img src=\"".$wwwtopdir."images/icons/16/star.png\" class=\"icon\" alt=\"\" />Ajouter aux sujets favoris</a>","frmtools");
     else
