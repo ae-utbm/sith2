516eb244755f55d2dac9b6c7541c971e786a76c0
Feu <feu@troll>
2007-06-23 12:03:41 +0000

    raleur & co

--- forum2/index.php
+++ forum2/index.php
@@ -22,7 +22,7 @@ $cts->cssclass="liner";
 if ( $site->user->is_valid() )
   $cts->buffer = "<p class=\"center\">Connecté en tant que ".$site->user->alias." - <a href=\"../user.php?page=edit\">Mon profil</a></p>";
 else
-  $cts->buffer = "<p class=\"center\">Non connecté - <a href=\"../index.php\">Se connecter</p> - <a href=\"../newaccount.php\">Creer un compte</p>";
+  $cts->buffer = "<p class=\"center\">Non connecté - <a href=\"../index.php\">Se connecter</a> - <a href=\"../newaccount.php\">Creer un compte</p>";
 $site->add_contents($cts);
 unset($cts);
 
