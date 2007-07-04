d348e081ee4215db0bb4201e2d3ad63639568702
Feu <feu@troll>
2007-07-04 17:00:02 +0000

    wip

--- forum2/index.php
+++ forum2/index.php
@@ -1,5 +1,29 @@
 <?php
-
+/* 
+ * FORUM2
+ *        
+ * Copyright 2007
+ * - Julien Etelain < julien dot etelain at gmail dot com >
+ * - Pierre Mauduit <pierre POINT mauduit CHEZ utbm POINT fr>
+ *
+ * Ce fichier fait partie du site de l'Association des Étudiants de
+ * l'UTBM, http://ae.utbm.fr/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
 $topdir = "../";
 
 require_once($topdir. "include/site.inc.php");
@@ -61,6 +85,12 @@ elseif ( isset($_REQUEST["react"]) )
   
   if ( isset($_REQUEST["id_nouvelle"]) )
     $conds[]= "(`frm_sujet`.`id_nouvelle`='" . mysql_escape_string($_REQUEST["id_nouvelle"]) . "')";
+    
+  if ( isset($_REQUEST["id_catph"]) )
+    $conds[]= "(`frm_sujet`.`id_catph`='" . mysql_escape_string($_REQUEST["id_catph"]) . "')";
+  
+  if ( isset($_REQUEST["id_sondage"]) )
+    $conds[]= "(`frm_sujet`.`id_sondage`='" . mysql_escape_string($_REQUEST["id_sondage"]) . "')";
   
   if ( count($conds) > 0 )
   {
