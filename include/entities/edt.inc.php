cb0398c03fc1af00459998d19159ba645b0b6053
pedrov <pedrov@troll>
2007-07-06 19:29:34 +0000

    mmmmh ?

--- include/entities/edt.inc.php
+++ include/entities/edt.inc.php
@@ -160,6 +160,19 @@ class edt extends stdentity
 
   function delete_edt($id_etu, $semestre)
   {
+    echo "SELECT 
+                                                  `id_uv_groupe`  
+                                  FROM 
+                                                  `edu_uv_groupe_etudiant`
+                                  INNER JOIN 
+                                                  `edu_uv_groupe` 
+                                  USING     
+                                                  (`id_uv_groupe`)
+                                  WHERE 
+                                                  `id_utilisateur` = ".intval($id_etu)." 
+                                  AND 
+                                                  `semestre_grp` = '".mysql_real_escape_string($semestre)."'";
+
 
     $req = new requete($this->db,"SELECT 
                                                   `id_uv_groupe`  
