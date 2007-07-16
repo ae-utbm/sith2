aebcb0a223a45ac88f0f5deca35757e6bfae7515
pedrov <pedrov@troll>
2007-07-16 13:33:08 +0000

    plouf

--- include/cts/imgcarto.inc.php
+++ include/cts/imgcarto.inc.php
@@ -63,14 +63,12 @@ class imgcarto
   
   function addcolor($def, $r,$g,$b)
   {
-    /* on ne génère pas encore 
-     * la couleur via imagecolorallocate
-     * étant donné que les dimensions de l'image ne sont 
-     * pas encore connues, et que l'image n'est pas encore
-     * créée
+    /* on ne génère pas encore la couleur via imagecolorallocate()
+     * étant donné que les dimensions de l'image ne sont pas encore
+     * connues, et que l'image n'est pas encore créée.
      */
 
-    $colors[$def] = array($r,$g,$b);
+    $this->colors[$def] = array($r,$g,$b);
   }
   
 
@@ -81,7 +79,7 @@ class imgcarto
 
   function addline($x, $y, $fx, $fy, $color)
   {
-    $this->lines[] = array($x,$y,$fx,$y,$color);
+    $this->lines[] = array($x,$y,$fx,$fy,$color);
   }
   function addpolygon($plg, $color, $filled = false)
   {
@@ -110,9 +108,6 @@ class imgcarto
     $min_x = 0;
     $max_y = 0;
     $min_y = 0;
-
-
-    $factor = &$this->factor;
 
     if (count($this->lines))
       {
@@ -175,8 +170,12 @@ class imgcarto
 	  }
       } // end parsing polygons
 
-    $this->dimx = ($max_x - $min_x) / $factor + $offset * 2;
-    $this->dimy = ($max_y - $min_y) / $factor + $offset * 2;
+    
+    $this->dimx = (($max_x - $min_x) / $this->factor) + $this->offset * 2;
+    $this->dimy = (($max_y - $min_y) / $this->factor) + $this->offset * 2;
+
+    $min_x = ($min_x / $this->factor);
+    $min_y = ($min_y / $this->factor);
 
     /* on peut maintenant recalculer les coordonnées */
    
@@ -185,8 +184,9 @@ class imgcarto
       {
 	for ($i = 0; $i < count($this->points); $i++)
 	  {
-	    $this->points[$i][0] = ($this->points[$i][0] / $factor) - $this->dimx + $offset;
-	    $this->points[$i][1] = ($this->points[$i][1] / $factor) - $this->dimy + $offset;
+	    $this->points[$i][0] = ($this->points[$i][0] / $this->factor) - $min_x + $this->offset;
+	    /* ATTENTION : inversion des ordonnées */
+	    $this->points[$i][1] = $this->dimy - ($this->points[$i][1] / $this->factor) - $min_y + $this->offset;
 	  }
       }
     // lines
@@ -194,10 +194,13 @@ class imgcarto
       {
 	for ($i = 0; $i < count($this->lines); $i++)
 	  {
-	    $this->lines[$i][0] = ($this->lines[$i][0] / $factor) - $this->dimx + $offset;
-	    $this->lines[$i][1] = ($this->lines[$i][1] / $factor) - $this->dimy + $offset;
-	    $this->lines[$i][2] = ($this->lines[$i][2] / $factor) - $this->dimx + $offset;
-	    $this->lines[$i][3] = ($this->lines[$i][3] / $factor) - $this->dimy + $offset;
+	    $this->lines[$i][0] = ($this->lines[$i][0] / $this->factor) - $min_x + $this->offset;
+	    /* attention : inversion ordonnées */
+	    $this->lines[$i][1] = $this->dimy - ($this->lines[$i][1] / $this->factor) - $min_y + $this->offset;
+	    
+	    $this->lines[$i][2] = ($this->lines[$i][2] / $this->factor) - $min_x + $this->offset;
+	    /* meme remarque ... */
+	    $this->lines[$i][3] = $this->dimy - ($this->lines[$i][3] / $this->factor) - $min_y + $this->offset;
 	  }
 
       }
@@ -208,11 +211,59 @@ class imgcarto
 	  {
 	    for ($j = 0; $j < count($this->polygons[$i][0]); $j += 2)
 	      {
-		$this->polygons[$i][0][$j] =   ($this->polygons[$i][0][$j]   / $factor) - $this->dimx + $offset;
-		$this->polygons[$i][0][$j+1] = ($this->polygons[$i][0][$j+1] / $factor) - $this->dimy + $offset;
+		$this->polygons[$i][0][$j] =   ($this->polygons[$i][0][$j] / $this->factor) - $min_x + $this->offset;
+		/* ATTENTION : invesion des ordonnées ! */
+		$this->polygons[$i][0][$j+1] = $this->dimy - ($this->polygons[$i][0][$j+1] / $this->factor) - $min_y + $this->offset;
 	      }
 	  }
       }
+
+    /* HACK ? on reparse (suite aux calculs) pour avoir la
+     * bonne composante en y pour la dimension "hauteur" de l'image */
+    $max_y = 0;
+    $min_y = 0;
+
+    if (count($this->lines))
+      {
+	foreach ($this->lines as $line)
+	  {
+	    if ($max_y < $line[1])
+	      $max_y = $line[1];
+	    if ($min_y > $line[1])
+	      $min_y = $line[1];
+	    if ($max_y < $line[3])
+	      $max_y = $line[3];
+	    if ($min_y > $line[3])
+	      $min_y = $line[3];
+	  }
+      } // end parsing lines
+
+    if (count($this->points))
+      {
+	foreach ($this->points as $point)
+	  {
+	    if ($max_y < $point[1])
+	      $max_y = $point[1];
+	    if ($min_y > $point[1])
+	      $min_y = $point[1];
+	  }
+      } // end parsing points
+
+    if (count($this->polygons))
+      {
+	foreach ($this->polygons as $polygon)
+	  {
+	    for ($i = 0; $i < count($polygon[0]); $i+= 2)
+	      {
+		if ($max_y < $polygon[0][$i+1])
+		  $max_y = $polygon[0][$i+1];
+		if ($min_y > $polygon[0][$i+1])
+		  $min_y = $polygon[0][$i+1];
+	      }
+	  }
+      } // end parsing polygons
+
+    $this->dimy = ($max_y - $min_y) + $this->offset * 2;
   }
 
   function draw()
@@ -220,15 +271,14 @@ class imgcarto
     if ($max_x == 0)
       $this->calculatedimensions();
 
-    if ($dimx > IMG_MAX_WIDTH)
+    if ($this->dimx > IMG_MAX_WIDTH)
       {
-	$this->errmsg = "Image too large !";
+	$this->errmsg = "Image too large : $this->dimx:$this->dimy\n";
 	return false;
       }
 
-    $this->imgres = imagecolorallocate($this->dimx, $this->dimy);
-
-
+    $this->imgres = imagecreatetruecolor($this->dimx, $this->dimy);
+    
     /* allocate colors */
     if (count($this->colors))
       {
@@ -241,21 +291,20 @@ class imgcarto
 	  }
       }
 
-    /* draw points */
-    if (count($this->points))
-      {
-	foreach ($this->points as $point)
-	  {
-	    imagefilledellipse ($this->imgres,
-				$point[0],
-				$point[1],
-				$point[2],
-				$point[2],
-				$this->colors[$point[3]]);
-	    
-	  }
+    imagefill($this->imgres, 0,0, $this->colors['white']['gd']);
 
+    /* draw polygons */
+    if (count($this->polygons))
+      {
+	foreach ($this->polygons as $polygon)
+	  {
+	    if ($polygon[2] == false)
+	      imagepolygon($this->imgres, $polygon[0], count($polygon[0]) / 2, $this->colors[$polygon[1]]['gd']);
+	    else
+	      imagefilledpolygon($this->imgres, $polygon[0], count($polygon[0]) / 2, $this->colors[$polygon[1]]['gd']);
+	  }
       }
+
     /* draw lines */
     if (count($this->lines))
       {
@@ -266,31 +315,36 @@ class imgcarto
 		       $line[1],
 		       $line[2],
 		       $line[3],
-		       $this->colors[$line[4]]);
+		       $this->colors[$line[4]]['gd']);
 	  }
+      }
+    /* draw points */
+    if (count($this->points))
+      {
+	foreach ($this->points as $point)
+	  {
+	    imagefilledellipse ($this->imgres,
+				$point[0],
+				$point[1],
+				$point[2],
+				$point[2],
+				$this->colors[$point[3]]['gd']);
+	    
+	  }
+
       }
 
-    /* draw polygons */
-    if (count($this->polygons))
-      {
-	foreach ($this->polygons as $polygon)
-	  {
-	    if ($polygon[2] == false)
-	      imagepolygon($this->imgres, $polygon[0], count($polygon[0]) / 2, $this->colors[$polygon[1]]);
-	    else
-	      imagefilledpolygon($this->imgres, $polygon[0], count($polygon[0]) / 2, $this->colors[$polygon[1]]);
-	  }
-      }
   }
   function saveas($path)
   {
-    if ($imgres)
-      imagepng($imgres, $path);
+    if ($this->imgres)
+      imagepng($this->imgres, $path);
   }
+
   function output()
   {
-    if ($imgres)
-      imagepng($imgres);
+    if ($this->imgres)
+      imagepng($this->imgres);
   }
 
   function destroy()
