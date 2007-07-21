4adcad6f216da2b82f72b478381fc0d20d82b93d
Ayolo <ayolo@troll>
2007-07-21 19:08:19 +0000

    fix

--- temp/carte_cotisants.php
+++ temp/carte_cotisants.php
@@ -190,9 +190,9 @@ if($req->lines!=0)
     /* on récupèré toutes les coordonnées pour les villes */
     foreach($loc AS $point)
     {
-      $pgreq = new pgrequete($pgconn, "SELECT gid, AsText(TRANSFORM(GeomFromText('POINT(".$point['long']." ".$point['lat'].")', 4030), 27582)) AS villecoords ".
+      $pgreq = new pgrequete($pgconn, "SELECT gid".
                                       "FROM deptfr ".
-                                      "WHERE CONTAINS(the_geom, TRANSFORM(GeomFromText('POINT(".$point['long']." ".$point['lat'].")', 4030), 27582)) ".
+                                      "WHERE CONTAINS(the_geom, TRANSFORM(GeomFromText('POINT(".$point['lat']." ".$point['long'].")', 4030), 27582)) ".
                                       "LIMIT 1");
       $rs = $pgreq->get_all_rows();
       foreach($rs as $result)
