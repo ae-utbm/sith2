4fe857b02697950d3daae0b3d53724585f91e226
Ayolo <ayolo@troll>
2007-07-21 14:14:29 +0000

    huhuhu, est-ce que ça va marcher ? :D

--- temp/carte_cotisants.php
+++ temp/carte_cotisants.php
@@ -22,6 +22,9 @@
  * 02111-1307, USA.
  */
 exit();
+
+define("RATIO", 1600);
+define("MODE", 0); // 0 => points, 1 => nb/departement
 $topdir = "../";
 include($topdir. "include/site.inc.php");
 
@@ -32,98 +35,231 @@ require_once($topdir. "include/entities/ville.inc.php");
 
 $site = new site ();
 
-$req = new requete($site->db, "SELECT `L1`.`lat_ville`, `L1`.`long_ville`, `L2`.`lat_ville`, `L2`.`long_ville`
-                               FROM `utilisateurs`
-                               INNER JOIN `utl_etu` ON `utl_etu`.`id_utilisateur`=`utilisateurs`.`id_utilisateur`
-                               LEFT JOIN `loc_ville` AS L1 ON `utl_etu`.`id_ville` = `L1`.`id_ville`
-                               LEFT JOIN `loc_ville` AS L2 ON CAST( `utl_etu`.`cpostal_parents` AS UNSIGNED ) = CAST( `L2`.`cpostal_ville` AS UNSIGNED )
-                               WHERE (`L1`.`lat_ville` IS NOT NULL OR `L2`.`lat_ville` IS NOT NULL)
-                               GROUP BY `L1`.`id_ville`, `L2`.`id_ville`");
+if (MODE == 0)
+{
+  /*
+  $req = new requete($site->db, "SELECT `L1`.`lat_ville`, `L1`.`long_ville`, `L2`.`lat_ville`, `L2`.`long_ville`
+                                 FROM `utl_etu`
+                                 LEFT JOIN `loc_ville` AS L1 ON `utl_etu`.`id_ville` = `L1`.`id_ville`
+                                 LEFT JOIN `loc_ville` AS L2 ON CAST( `utl_etu`.`cpostal_parents` AS UNSIGNED ) = CAST( `L2`.`cpostal_ville` AS UNSIGNED )
+                                 WHERE (`L1`.`lat_ville` IS NOT NULL OR `L2`.`lat_ville` IS NOT NULL)
+                                 GROUP BY `L1`.`id_ville`, `L2`.`id_ville`");
+   */
+  $req = new requete($site->db, "SELECT `loc_ville`.`lat_ville`, `loc_ville`.`long_ville`
+                                 FROM `loc_ville`
+                                 LEFT JOIN `utl_etu` AS E1 ON (`loc_ville`.`id_ville` = `E1`.`id_ville`
+                                 OR CAST( `E2`.`cpostal_parents` AS UNSIGNED )
+                                 = CAST( `loc_ville`.`cpostal_ville` AS UNSIGNED ))
+                                 GROUP BY `loc_ville`.`id_ville`");
+}
+
+elseif (MODE == 1)
+{
+  // on ne veux que les codes postaux et on va essayer un truc : faire en 2 requetes
+  // la première : jointure sur les id_ville
+  $req = new requete($site->db, "SELECT  `lat_ville`, `long_ville`, COUNT(*) AS `nb` FROM `loc_ville`
+    LEFT JOIN `utl_etu` ON  `utl_etu`.`id_ville` = `loc_ville`.`id_ville`
+    GROUP BY `loc_ville`.`id_ville`");
+}
+else
+{
+  echo "what the fuck ?!";
+  exit();
+}
 
 if($req->lines!=0)
 {
   $img = new imgcarto();
   $img->addcolor('pblue', 222, 235, 245);
   $img->addcolor('pblue_dark', 51, 102, 153);
+  if(MODE==1)
+  {
+    $img->addcolor('p10', 172, 213, 245); // 0 < hts < 10
+    $img->addcolor('p20', 172, 194, 245); // 10 <= hts < 20
+    $img->addcolor('p30', 74, 170, 245); // 20 <= hts < 30
+    $img->addcolor('p40', 39, 123, 227); // 30 <= hts < 40
+    $img->addcolor('p50', 39, 52, 227); // 40 <= hts < 50
+    $img->addcolor('p60', 99, 0, 227); // 50 <= hts < 60
+    $img->addcolor('p70', 168, 0, 185); // 60 <= hts < 70
+    $img->addcolor('p80', 167, 0, 71); // 70 <= hts < 80
+    $img->addcolor('p90', 190, 59, 68); // 80 <= hts < 90
+    $img->addcolor('p100', 255, 120, 0); // 90 <= hts < 100
+    $img->addcolor('pmax', 255, 0, 0); // 100 <= hts
+  }
 
   $i=0;
   $loc = array();
-  while(list($_lat, $_long, $_lat2, $_long2 ) = $req->get_row())
+  if(MODE == 0)
   {
-    if(!is_null($_lat))
+    while(list($_lat, $_long) = $req->get_row())
     {
       $lat  = rad2deg($_lat);
       $long = rad2deg($_long);
+      $lat = str_replace(",", ".", $lat);
+      $long = str_replace(",", ".", $long);
+      $loc[$i]['lat']=$lat;
+      $loc[$i]['long']=$long;
+      $i++;
     }
-    else
+  }
+  elseif(MODE==1)
+  {
+    while(list($_lat, $_long, $nb) = $req->get_row())
     {
-      $lat  = rad2deg($_lat2);
-      $long = rad2deg($_long2);
+      $lat  = rad2deg($_lat);
+      $long = rad2deg($_long);
+      $lat = str_replace(",", ".", $lat);
+      $long = str_replace(",", ".", $long);
+      $loc[$i]['lat']=$lat;
+      $loc[$i]['long']=$long;
+      $loc[$i]['nb']=$nb;
+      $i++;
     }
-    $lat = str_replace(",", ".", $lat);
-    $long = str_replace(",", ".", $long);
-    $loc[$i]['lat']=$lat;
-    $loc[$i]['long']=$long;
-    $i++;
   }
 
   $pgconn = new pgsqlae();
-  $pgreq = new pgrequete($pgconn, "SELECT asText(the_geom) AS points".
-                                  " FROM deptfr");
-  $rs = $pgreq->get_all_rows();
-  $numdept = 0;
-  foreach($rs as $result)
+  if(MODE==0)
   {
-    $astext = $result['points'];
-    $matched = array();
-    preg_match_all("/\(([^)]*)\)/", $astext, $matched);
-    $i = 0;
-    foreach ($matched[1] as $polygon)
-    {
-      $polygon = str_replace("(", "", $polygon);
-      $points = explode(",", $polygon);
-      foreach ($points as $point)
-      {
-        $coord = explode(" ", $point);
-        $dept[$numdept]['plgs'][$i][] = $coord[0];
-        $dept[$numdept]['plgs'][$i][] = $coord[1];
-      }
-      $i++;
-    }
-    $numdept++;
-  }
-
-  $viles = array();
-  /* on récupèré toutes les coordonnées pour les villes */
-  foreach($loc AS $point)
-  {
-    $pgreq = new pgrequete($pgconn, "SELECT AsText(TRANSFORM(GeomFromText('POINT(".$point['long']." ".$point['lat'].")', 4030), 27582)) AS villecoords ".
-                                    "FROM deptfr LIMIT 1");
+    $pgreq = new pgrequete($pgconn, "SELECT asText(the_geom) AS points".
+                                    " FROM deptfr");
     $rs = $pgreq->get_all_rows();
+    $numdept = 0;
     foreach($rs as $result)
     {
-      $villes[] = $result['villecoords'];
-      break;
+      $astext = $result['points'];
+      $matched = array();
+      preg_match_all("/\(([^)]*)\)/", $astext, $matched);
+      $i = 0;
+      foreach ($matched[1] as $polygon)
+      {
+        $polygon = str_replace("(", "", $polygon);
+        $points = explode(",", $polygon);
+        foreach ($points as $point)
+        {
+          $coord = explode(" ", $point);
+          $dept[$numdept]['plgs'][$i][] = $coord[0];
+          $dept[$numdept]['plgs'][$i][] = $coord[1];
+        }
+        $i++;
+      }
+      $numdept++;
     }
   }
-  foreach($dept as $departement)
+  elseif(MODE==1)
   {
-    foreach($departement['plgs'] as $plg)
+    $pgreq = new pgrequete($pgconn, "SELECT gid, asText(the_geom) AS points".
+                                    " FROM deptfr");
+    $rs = $pgreq->get_all_rows();
+    $numdept=0;
+    foreach($rs as $result)
     {
-      $img->addpolygon($plg, 'pblue', true);
-      $img->addpolygon($plg, 'pblue_dark', false);
+      $astext = $result['points'];
+      $matched = array();
+      preg_match_all("/\(([^)]*)\)/", $astext, $matched);
+      $i = 0;
+      foreach ($matched[1] as $polygon)
+      {
+        $polygon = str_replace("(", "", $polygon);
+        $points = explode(",", $polygon);
+        foreach ($points as $point)
+        {
+          $coord = explode(" ", $point);
+          $dept[$numdept]['plgs'][$i][] = $coord[0];
+          $dept[$numdept]['plgs'][$i][] = $coord[1];
+        }
+        $i++;
+      }
+      $dept[$numdept]['id']=$result['gid'];
+      $numdept++;
     }
   }
 
-  foreach($villes as $ville)
+  if (MODE==0)
   {
-    $villecoords = str_replace("POINT(", "", $ville);
-    $villecoords = str_replace(")", "", $villecoords);
-    $villecoords = explode(" ", $villecoords);
-    $img->addpoint($villecoords[0], $villecoords[1], 5, "black");
+    $villes = array();
+    /* on récupèré toutes les coordonnées pour les villes */
+    foreach($loc AS $point)
+    {
+      $pgreq = new pgrequete($pgconn, "SELECT AsText(TRANSFORM(GeomFromText('POINT(".$point['long']." ".$point['lat'].")', 4030), 27582)) AS villecoords ".
+                                      "FROM deptfr LIMIT 1");
+      $rs = $pgreq->get_all_rows();
+      foreach($rs as $result)
+      {
+        $villes[] = $result['villecoords'];
+        break;
+      }
+    }
+  }
+  if(MODE==1)
+  {
+    $villes = array();
+    /* on récupèré toutes les coordonnées pour les villes */
+    foreach($loc AS $point)
+    {
+      $pgreq = new pgrequete($pgconn, "SELECT gid, AsText(TRANSFORM(GeomFromText('POINT(".$point['long']." ".$point['lat'].")', 4030), 27582)) AS villecoords ".
+                                      "FROM deptfr LIMIT 1"
+                                      "WHERE CONTAINS(the_geom, TRANSFORM(GeomFromText('POINT(".$point['long']." ".$point['lat'].")', 4030), 27582))");
+      $rs = $pgreq->get_all_rows();
+      foreach($rs as $result)
+      {
+        $d=$result['gid'];
+        if(!isset($villes[$d]))
+          $villes[$d] = $point['nb'];
+        else
+          $villes[$d] = $villes[$d]+$point['nb'];
+        break;
+      }
+    }
   }
 
-  $img->setfactor(1600);
+  if(MODE==0)
+  {
+    foreach($dept as $departement)
+    {
+      foreach($departement['plgs'] as $plg)
+      {
+        $img->addpolygon($plg, 'pblue', true);
+        $img->addpolygon($plg, 'pblue_dark', false);
+      }
+    }
+  }
+  elseif(MODE==1)
+  {
+    foreach($dept as $departement)
+    {
+      $d=$departement['gid'];
+      if(!isset($villes[$d]))
+        $color="pblue";
+      elseif($villes[$d]>=100)
+        $color="pmax";
+      elseif(10 <= $villes[$d] && $villes[$d]<100)
+      {
+        $n=int($villes[$d] / 10)
+        $color="p".$n;
+      }
+      else
+        $color="pblue";
+
+      foreach($departement['plgs'] as $plg)
+      {
+        $img->addpolygon($plg, $color, true);
+        $img->addpolygon($plg, 'pblue_dark', false);
+      }
+    }
+  }
+
+  if(MODE==0)
+  {
+    foreach($villes as $ville)
+    {
+      $villecoords = str_replace("POINT(", "", $ville);
+      $villecoords = str_replace(")", "", $villecoords);
+      $villecoords = explode(" ", $villecoords);
+      $img->addpoint($villecoords[0], $villecoords[1], 5, "black");
+    }
+  }
+
+  $img->setfactor(RATIO);
   $img->draw();
   $wm_img = new img_watermark (&$img->imgres);
   $wm_img->output();
