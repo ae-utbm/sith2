1a1e8a1059ef3be84eeda582e81db7abf7b3e925
pedrov <pedrov@troll>
2007-07-24 09:31:04 +0000

    uvs

--- uvs/uvs.php
+++ uvs/uvs.php
@@ -38,6 +38,76 @@ $site->start_page("services", "Informations UV");
 
 $depts = array('Humas', 'GESC', 'GI', 'IMAP', 'GMC');
 
+if (isset($_REQUEST['id_uv']))
+{
+  $iduv = intval($_REQUEST['id_uv']);
+
+  $req = new requete($site->db,
+		     "SELECT 
+                             `edu_uv`.`code_uv`
+                             , `edu_uv`.`intitule_uv`
+                             , `edu_uv`.`cours_uv`
+                             , `edu_uv`.`td_uv`
+                             , `edu_uv`.`tp_uv`
+
+                      FROM
+                             `edu_uv`
+                      WHERE
+                             `edu_uv`.`id_uv` = $iduv");
+
+  $cts = new contents('');
+
+  $rs = $req->get_row();
+  
+  /* Code + intitulé */
+  $cts->add_title(2, $rs['code_uv']);
+  $cts->add_paragraph("<b>".$rs['intitule_uv']."</b>");
+
+  /* format horaire */
+  $cts->add_title(2, "Formats horaires");
+  
+  $parag = "<ul>";
+
+  if ($rs['cours_uv'] == 1)
+    $parag .= "<li>Cours</li>\n";
+  if ($rs['td_uv'] == 1)
+    $parag .= "<li>TD</li>\n";
+  if ($rs['tp_uv'] == 1)
+    $parag .= "<li>TP</li>\n";
+  $parag .= "</ul>\n";
+
+  if (($rs['cours_uv']== 0) && ($rs['cours_uv']== 0) && ($rs['cours_uv']== 0)) 
+    $parag = "<b>UV Hors Emploi du Temps (HET)</b>";
+
+  
+  $cts->add_paragraph($parag);
+  
+  /* départements concernés */
+  $cts->add_title(2, "Départements dans lequel l'UV est enseignée");
+
+  $req = new requete($site->db,
+		     "SELECT 
+                             `id_dept`
+                      FROM
+                             `edu_uv_dept`
+                      WHERE
+                              `id_uv` = $iduv");
+
+
+  while ($rs = $req->get_row())
+    {
+      $myuvdpts[] = "<a href=\"./uvs.php?iddept=".$rs['id_dept']."\">".$rs['id_dept']."</a>\n";
+    }
+  $lst = new itemlist("Départements",
+		      false,
+		      $myuvdpts);
+  $cts->add($lst);
+
+  exit();
+}
+
+
+
 if (isset($_REQUEST['iddept']))
 {
   if (in_array($_REQUEST['iddept'], $depts))
