b7f478c1e37208564d3d699ff9bbb74cdf88e5c0
Feu <feu@troll>
2007-07-25 11:36:01 +0000

    un autre test

--- temp/galaxy.php
+++ temp/galaxy.php
@@ -11,9 +11,8 @@ if ( isset($_REQUEST["init"]) )
   echo "INITIALISATION : ";
   $st = microtime(true);
   
-  new requete($dbrw, 
-  "TRUNCATE `galaxy_link`;\n".  "TRUNCATE `galaxy_star`;"
-  );
+  new requete($dbrw,"TRUNCATE `galaxy_link`");  new requete($dbrw,"TRUNCATE `galaxy_star`");
+  
   
   $req1 = new requete($dbrw, "SELECT p1.id_utilisateur, alias_utl
   FROM `sas_personnes_photos` AS `p1`
@@ -81,29 +80,22 @@ for($i=0;$i<$cycles;$i++)
   // CYCLE
   echo "CYCLE : ";
   $st = microtime(true);
-  new requete($dbrw, 
-  /*"-- MAJ longueur liens --\n".*/
-  "UPDATE galaxy_link, galaxy_star AS a, galaxy_star AS b SET ".
+  new requete($dbrw,"UPDATE galaxy_link, galaxy_star AS a, galaxy_star AS b SET ".
   "vx_link = b.x_star-a.x_star, ".
   "vy_link = b.y_star-a.y_star  ".
-  "WHERE a.id_star = galaxy_link.id_star_a AND b.id_star = galaxy_link.id_star_b;\n ".
-  "UPDATE galaxy_link SET length_link = SQRT(POW(vx_link,2)+POW(vy_link,2));\n ".
-  "UPDATE galaxy_link SET dx_link=vx_link/length_link, dy_link=vy_link/length_link WHERE length_link != 0;\n ".
-  "UPDATE galaxy_link SET dx_link=0, dy_link=0 WHERE length_link = 0;\n ".
-  /*"-- MAJ delta lien --\n".*/
-  "UPDATE galaxy_link, galaxy_star AS a, galaxy_star AS b SET  ".
+  "WHERE a.id_star = galaxy_link.id_star_a AND b.id_star = galaxy_link.id_star_b");
+  new requete($dbrw,"UPDATE galaxy_link SET length_link = SQRT(POW(vx_link,2)+POW(vy_link,2))");
+  new requete($dbrw,"UPDATE galaxy_link SET dx_link=vx_link/length_link, dy_link=vy_link/length_link WHERE length_link != 0");
+  new requete($dbrw,"UPDATE galaxy_link SET dx_link=0, dy_link=0 WHERE length_link = 0");
+  new requete($dbrw,"UPDATE galaxy_link, galaxy_star AS a, galaxy_star AS b SET  ".
   "delta_link_a=(length_link-ideal_length_link)*tense_link/a.sum_tense_star/2, ".
   "delta_link_b=(length_link-ideal_length_link)*tense_link/b.sum_tense_star*-1/2 ".
-  "WHERE a.id_star = galaxy_link.id_star_a AND b.id_star = galaxy_link.id_star_b;\n ".
-  /*"-- MAJ delta éléments --\n".*/
-  "UPDATE galaxy_star ".
+  "WHERE a.id_star = galaxy_link.id_star_a AND b.id_star = galaxy_link.id_star_b");
+  new requete($dbrw,"UPDATE galaxy_star ".
   "SET dx_star = ( SELECT SUM(IF(id_star_a=id_star,delta_link_a,delta_link_b)*dx_link) FROM galaxy_link WHERE id_star_a=id_star OR id_star_b=id_star ), ".
-  "dy_star = ( SELECT SUM(IF(id_star_a=id_star,delta_link_a,delta_link_b)*dy_link) FROM galaxy_link WHERE id_star_a=id_star OR id_star_b=id_star );\n ".
-  /*"-- AntiCollision -- (la dernière condition devrai éviter les blocages perpetuels, en raison d'une collision effective)\n".*/
-  "UPDATE galaxy_star AS a, galaxy_star AS b SET a.dx_star=0, a.dy_star=0 WHERE POW((a.x_star+a.dx_star)-(b.x_star+b.dx_star),2)+POW((a.y_star+a.dy_star)-(b.y_star+b.dy_star),2) < 0.1 AND POW(a.x_star-b.x_star,2)+POW(a.y_star-b.y_star,2) > 0.1;\n ".
-  /*"-- MAJ Coordonnées éléments --\n".*/
-  "UPDATE galaxy_star SET x_star = x_star + dx_star, y_star = y_star + dy_star WHERE dx_star != 0 OR dy_star != 0;"
-  );
+  "dy_star = ( SELECT SUM(IF(id_star_a=id_star,delta_link_a,delta_link_b)*dy_link) FROM galaxy_link WHERE id_star_a=id_star OR id_star_b=id_star )");
+  new requete($dbrw,"UPDATE galaxy_star AS a, galaxy_star AS b SET a.dx_star=0, a.dy_star=0 WHERE POW((a.x_star+a.dx_star)-(b.x_star+b.dx_star),2)+POW((a.y_star+a.dy_star)-(b.y_star+b.dy_star),2) < 0.1 AND POW(a.x_star-b.x_star,2)+POW(a.y_star-b.y_star,2) > 0.1");
+  new requete($dbrw,"UPDATE galaxy_star SET x_star = x_star + dx_star, y_star = y_star + dy_star WHERE dx_star != 0 OR dy_star != 0");
   echo "done in ".(microtime(true)-$st)." sec<br/>\n";
 }
 
