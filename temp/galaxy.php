e04eee24b63c545c35423cebcee30d751b5f8275
Feu <feu@troll>
2007-07-26 15:39:34 +0000

    test

--- temp/galaxy.php
+++ temp/galaxy.php
@@ -204,13 +204,11 @@ if ( isset($_REQUEST["render"]) )
     // Rouge foncé   | Rouge              | Jaune              | Bleu  | Blanc
     
     
-    imagefilledellipse ($img, $row['rx_star'], $row['ry_star'], 20, 20, $bullcolor | ((0x40) << 24) );
-    //imagefilledellipse ($img, $row['rx_star'], $row['ry_star'], 8, 8, $bullcolor | (0x60) << 24 );
-    
-    
+    imageellipse ($img, $row['rx_star'], $row['ry_star'], 9, 9, $bullcolor | ((0x20) << 24) );
+    imageellipse ($img, $row['rx_star'], $row['ry_star'], 8, 8, $bullcolor | ((0x40) << 24) );
+    imageellipse ($img, $row['rx_star'], $row['ry_star'], 7, 7, $bullcolor | ((0x50) << 24) );
+    imageellipse ($img, $row['rx_star'], $row['ry_star'], 6, 6, $bullcolor | (0x60) << 24 );
     imagefilledellipse ($img, $row['rx_star'], $row['ry_star'], 5, 5, $bullcolor );
-    
-    
     
     
   }
