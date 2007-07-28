9ad5ee0f6f4c3ed4c31268a3265cf0bc2f1265dd
Ayolo <ayolo@troll>
2007-07-28 02:27:59 +0000

    test

--- ae/stats_site.php
+++ ae/stats_site.php
@@ -29,28 +29,6 @@ $site = new site ();
 if (!$site->user->is_in_group ("gestion_ae"))
   error_403();
 
-$color=array(0 => "FFF200",
-//             1 => "FFE700",
-//             2 => "FFCD00",
-//             3 => "FFC600",
-             4 => "FFB000",
-//             5 => "FFA500",
-//             6 => "FF9A00",
-//             7 => "FF8F00",
-             8 => "FF8400",
-//             9 => "FF7900",
-//             10=> "FF7200",
-//             11=> "FF6500",
-             12=> "FF5A00",
-//             13=> "FF5000",
-//             14=> "FF4400",
-//             15=> "FF3A00",
-             16=> "FF2F00",
-//             17=> "FF2400",
-//             18=> "FF1A00",
-//             19=> "FF1000",
-             20=> "FF0000");
-
 function dec2hex($val)
 {
   $hex="";
@@ -71,7 +49,8 @@ if ( $_REQUEST["action"] == "os" )
   $_color="#ffffff";
   $inc=50;
   $req = new requete($site->db,"SELECT * FROM `stats_os`  ORDER BY `visites` DESC");
-  $cam=new camembert(600,500,array(),2,20,0,10,0.25,10,10,10,150);
+	$cam=new camembert(600,500,array(),2,20,0,10,0.25,10,10,10,150);
+  $i=1;
   while($row=$req->get_row())
   {
     $cam->data($row['visites'], $_color, $row['os']);
@@ -137,7 +116,7 @@ if ( $_REQUEST["action"] == "browser" )
   $inc=50;
   $req = new requete($site->db,"SELECT * FROM `stats_browser`  ORDER BY `visites` DESC");
   $cam=new camembert(600,500,array(),2,20,0,10,0.25,10,10,10,150);
-  $i=20;
+  $i=1;
   while($row=$req->get_row())
   {
     $cam->data($row['visites'], $_color, $row['os']);
