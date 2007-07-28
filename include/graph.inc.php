08d8fe52d5dac1ac8c2caf77fff67915bff2b829
Ayolo <ayolo@troll>
2007-07-28 03:34:32 +0000

    debug

--- include/graph.inc.php
+++ include/graph.inc.php
@@ -523,12 +523,12 @@ class camembert
     $tab[]=$this->centreXCam - ($this->largeurLegend / 2) - $this->padding - $this->padding;
     $tab[]=$this->centreYCam - $this->padding;
 
-    //if ($tab[0]>=$tab[1])$tab[3]=$tab[1];
-		//if ($tab[0]<$tab[1])$tab[3]=$tab[0];
+    if ($tab[0]>=$tab[1])$tab[3]=$tab[1];
+		if ($tab[0]<$tab[1])$tab[3]=$tab[0];
     $tab[3]=$tab[1];
 
     $this->largeurCam=($tab[3] * 2);
-    $this->hauteurCam=(($tab[3]) - ($tab[3] * $this->inclinaison / 100)) *2);
+    $this->hauteurCam=(($tab[3]) - (($tab[3] * $this->inclinaison / 100)) *2);
   }
 
 
