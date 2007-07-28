4c78d2f0e0e4b4994e5e0971dad8d8240f5d8256
Ayolo <ayolo@troll>
2007-07-28 00:59:10 +0000

    le camembert, ça pue mais c'est bon

--- include/graph.inc.php
+++ include/graph.inc.php
@@ -8,8 +8,9 @@
  */
 /* Copyright 2005,2006
  * - Pierre Mauduit <pierre POINT mauduit CHEZ utbm POINT fr>
+ * - Simon Lopez < simon DOT lopez AT ayolo DOT org >
  *
- * Ce fichier fait partie du site de l'Association des Ã‰tudiants de
+ * Ce fichier fait partie du site de l'Association des étudiants de
  * l'UTBM, http://ae.utbm.fr.
  *
  * This program is free software; you can redistribute it and/or
@@ -91,11 +92,11 @@ class graphic
 
   /* constructeur */
   function graphic ($title,
-		    $func_legend,
-		    $coords = false,
-		    $xaxis_time = false,
-		    $x_tics = false,
-		    $y_tics = false)
+                    $func_legend,
+                    $coords = false,
+                    $xaxis_time = false,
+                    $x_tics = false,
+                    $y_tics = false)
   {
     /* identifiant de graphique */
     $this->graph_id = substr(md5(microtime(true)), 0, 6);
@@ -124,26 +125,26 @@ class graphic
   function generate_database ()
   {
     if ((!$this->coords) ||
-	(!is_array($this->coords)))
+        (!is_array($this->coords)))
       return false;
 
     sort ($this->coords);
 
     foreach ($this->coords as $plot)
+    {
+      /* une seule ordonnee */
+      if (!is_array($plot['y']))
+        $datas .= $plot['x'] ." ".$plot['y'] . "\n";
+      /* plusieurs ordonnees */
+      else
       {
-	/* une seule ordonnee */
-	if (!is_array($plot['y']))
-	  $datas .= $plot['x'] ." ".$plot['y'] . "\n";
-	/* plusieurs ordonnees */
-	else
-	  {
-	    $datas .= $plot['x'];
-	    foreach ($plot['y'] as $ordonnees)
-	      $datas .= (" " .$ordonnees);
+        $datas .= $plot['x'];
+        foreach ($plot['y'] as $ordonnees)
+          $datas .= (" " .$ordonnees);
 
-	    $datas .= "\n";
-	  }
+        $datas .= "\n";
       }
+    }
     file_put_contents($this->data_file, $datas);
     return true;
   }
@@ -164,15 +165,15 @@ class graphic
 
     /* ranges en x - si abscisses non temporelles */
     if ($this->xaxis_time == false)
-      {
-	$min = $this->coords[0]['x'];
-	$max = $this->coords[count($this->coords) - 1]['x'];
-	$datas .= "set xrange [";
-	$datas .= $min;
-	$datas .= ":";
-	$datas .= $max;
-	$datas .= "]\n";
-      }
+    {
+      $min = $this->coords[0]['x'];
+      $max = $this->coords[count($this->coords) - 1]['x'];
+      $datas .= "set xrange [";
+      $datas .= $min;
+      $datas .= ":";
+      $datas .= $max;
+      $datas .= "]\n";
+    }
 
     /* ranges en y */
     $ymin = 0;
@@ -180,27 +181,27 @@ class graphic
     /* determination min / max de la(des) courbe(s) */
     if ( count($this->coords) > 0 )
     foreach ($this->coords as $coord)
+    {
+      /* une seule courbe en y */
+      if (!is_array($coord['y']))
       {
-	/* une seule courbe en y */
-	if (!is_array($coord['y']))
-	  {
-	    if ($coord['y'] < $ymin)
-	      $ymin = $coord['y'];
-	    if ($coord['y'] > $ymax)
-	      $ymax = $coord['y'];
-	  }
-	/* plusieurs :'( */
-	else
-	  {
-	    foreach ($coord['y'] as $value)
-	      {
-		if ($value < $ymin)
-		  $ymin = $value;
-		if ($value > $ymax)
-		  $ymax = $value;
-	      }
-	  } // fin plusieurs courbes
-      } // fin determination max
+        if ($coord['y'] < $ymin)
+          $ymin = $coord['y'];
+        if ($coord['y'] > $ymax)
+          $ymax = $coord['y'];
+      }
+      /* plusieurs :'( */
+      else
+      {
+        foreach ($coord['y'] as $value)
+        {
+          if ($value < $ymin)
+            $ymin = $value;
+          if ($value > $ymax)
+            $ymax = $value;
+        }
+      } // fin plusieurs courbes
+    } // fin determination max
     $round = abs($ymin - $ymax) * 5 / 100;
 
     $ymin -= $round;
@@ -214,44 +215,44 @@ class graphic
 
     /* abscisses temporelles */
     if ($this->xaxis_time != false)
-      {
-	$datas .= "set xdata time\n";
-	$datas .= "set timefmt \"".$this->xaxis_time[0]. "\"\n";
-	$datas .= "set format x \"".$this->xaxis_time[1]."\"\n";
-      }
+    {
+      $datas .= "set xdata time\n";
+      $datas .= "set timefmt \"".$this->xaxis_time[0]. "\"\n";
+      $datas .= "set format x \"".$this->xaxis_time[1]."\"\n";
+    }
 
     /* abscisses speciales */
     if ($this->x_tics != false)
-      {
-	$datas .= "set xtics (";
+    {
+      $datas .= "set xtics (";
 
-	$total = count ($this->x_tics) - 1;
-	foreach ($this->x_tics as $xtics => $legend)
-	  {
-	    /* si format temporel en abscisses */
-	    if ($this->xaxis_time != false)
-	      $xtics = "\"" . $xtics . "\"";
-	    $datas .= ("\"" . $legend . "\" " . $xtics . ", ");
-	  }
-	/* on enleve la virgule finale */
-	$datas = substr ($datas, 0, strlen ($datas) - 2);
-	$datas .= ")\n";
+      $total = count ($this->x_tics) - 1;
+      foreach ($this->x_tics as $xtics => $legend)
+      {
+        /* si format temporel en abscisses */
+        if ($this->xaxis_time != false)
+          $xtics = "\"" . $xtics . "\"";
+        $datas .= ("\"" . $legend . "\" " . $xtics . ", ");
       }
+      /* on enleve la virgule finale */
+      $datas = substr ($datas, 0, strlen ($datas) - 2);
+      $datas .= ")\n";
+    }
 
     /* ordonnees speciales */
     if ($this->y_tics != false)
-      {
-	$datas .= "set ytics (";
+    {
+      $datas .= "set ytics (";
 
-	$total = count ($this->y_tics) - 1;
-	foreach ($this->y_tics as $ytics => $legend)
-	  {
-	    $datas .= ("\"" . $legend . "\" " . $ytics . ", ");
-	  }
-	/* on enleve la virgule finale */
-	$datas = substr ($datas, 0, strlen ($datas) - 2);
-	$datas .= ")\n";
+      $total = count ($this->y_tics) - 1;
+      foreach ($this->y_tics as $ytics => $legend)
+      {
+        $datas .= ("\"" . $legend . "\" " . $ytics . ", ");
       }
+      /* on enleve la virgule finale */
+      $datas = substr ($datas, 0, strlen ($datas) - 2);
+      $datas .= ")\n";
+    }
 
     $datas .= "set datafile missing \"-1\"\n";
 
@@ -263,20 +264,20 @@ class graphic
 
     /* plusieurs */
     else
-      {
-	$nb_courbes = count($this->func_legend);
-	$datas .= "plot ";
+    {
+      $nb_courbes = count($this->func_legend);
+      $datas .= "plot ";
 
-	for ($i=0; $i < ($nb_courbes - 1); $i++)
-	  $datas .= ("\"".$this->data_file."\" \
-		      using 1:".sprintf($i + 2)." with lines title \"".
-		     $this->func_legend[$i] ."\",   \\" ."\n");
+      for ($i=0; $i < ($nb_courbes - 1); $i++)
+        $datas .= ("\"".$this->data_file."\" \
+          using 1:".sprintf($i + 2)." with lines title \"".
+         $this->func_legend[$i] ."\",   \\" ."\n");
 
-	$datas .= ("\"". $this->data_file ."\" \
-		    using 1:".sprintf($nb_courbes + 1).
-		   " with lines title \"".
-		   $this->func_legend[$nb_courbes - 1] ."\"\n");
-      }
+      $datas .= ("\"". $this->data_file ."\" \
+        using 1:".sprintf($nb_courbes + 1).
+       " with lines title \"".
+       $this->func_legend[$nb_courbes - 1] ."\"\n");
+    }
 
     $datas .= "set style fill solid 0.25 border
                replot\n";
@@ -320,7 +321,7 @@ class graphic
     header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
     header("Content-Type: image/png");
     header("Content-Disposition: inline; filename=".
-	   basename($this->img_file));
+    basename($this->img_file));
     /*on envoie le fichier */
     readfile($this->img_file);
 
@@ -364,14 +365,14 @@ class histogram
 
 
     $out_gplot = "set terminal png nocrop enhanced\n".
-      "set output '". $this->img_file."'\n".
-      "set style data histogram\n".
-      "set style histogram cluster gap 1\n".
-      "set style fill solid border -1\n".
-      "set boxwidth 0.9\n".
-      "set title \"".$title."\"\n".
-      "set xtics border nomirror offset character 0,0,0\n".
-      "plot '".$this->data_file."' using 2:xtic(1) title col";
+                 "set output '". $this->img_file."'\n".
+                 "set style data histogram\n".
+                 "set style histogram cluster gap 1\n".
+                 "set style fill solid border -1\n".
+                 "set boxwidth 0.9\n".
+                 "set title \"".$title."\"\n".
+                 "set xtics border nomirror offset character 0,0,0\n".
+                 "plot '".$this->data_file."' using 2:xtic(1) title col";
 
     $minvalue = 0;
     $maxvalue = 0;
@@ -379,13 +380,13 @@ class histogram
     
 
     foreach ($plots as $key => $value)
-      {
-	$out_data .= $key . "\t". $value."\n";	
-	if ($value >= $maxvalue)
-	  $maxvalue = $value;
-	if ($value <= $minvalue)
-	  $minvalue = $value;
-      }
+    {
+      $out_data .= $key . "\t". $value."\n";  
+      if ($value >= $maxvalue)
+        $maxvalue = $value;
+      if ($value <= $minvalue)
+        $minvalue = $value;
+    }
 
     file_put_contents($this->data_file, $out_data);
     file_put_contents($this->conf_file, $out_gplot);
@@ -407,7 +408,7 @@ class histogram
     header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
     header("Content-Type: image/png");
     header("Content-Disposition: inline; filename=".
-	   basename($this->img_file));
+    basename($this->img_file));
     /*on envoie le fichier */
     readfile($this->img_file);
 
@@ -420,4 +421,291 @@ class histogram
     @unlink($this->data_file);
     @unlink($this->img_file);
   }
+}
+
+
+
+/*
+ * une classe pour faire des camemberts :)
+ */
+
+class camembert
+{
+  var $img;
+  var $centreCam;
+  var $hauteurCam;
+  var $largeurCam;
+  var $tabValeurColor;
+  var $tabComment;
+  var $nbrdata;
+  var $imgTemp='';
+
+  //couleurs par defaut
+  var $fond="#ffffff";
+  var $colorArette = "#685309";
+  var $texte = "#000000";
+  var $colorLegende = "#E2E2E2";
+
+  function camembert($width=100,
+                     $height=80,
+                     $color =array(),
+                     $decimal=2,
+                     $padding=10,
+                     $spacing=15,
+                     $profondeur=0,
+                     $ombre = 0,
+                     $inclinaison=0,
+                     $ombrage=0,
+                     $largeurArette=10,
+                     $largeurLegend=0;
+  {
+    $this->largeurImg           = $width;
+    $this->hauteurImg           = $height;
+    $this->padding              = $padding;
+    $this->decimal              = $decimal;
+    $this->spacing              = $spacing;
+    $this->prodondeurGraph      = $profondeur; //Profondeur du graph en pixel (0 pour 2D)
+    $this->ombre                = $ombre;
+    $this->inclinaison          = $inclinaison;
+    $this->ombrage              = $ombrage;
+    $this->largeurLegend        = $largeurLegend;
+    $this->largeurAretteExterne = $largeurArette;
+
+    if(!empty($color) && count($color)>0)
+    {
+      if(isset($color[0]))
+        $this->fond=$color[0];
+      if(isset($color[1]))
+        $this->colorArette=$color[1];
+      if(isset($color[2]))
+        $this->texte=$color[2];
+      if(isset($color[3]))
+        $this->colorLegende=$color[3];
+    }
+
+    $this->drawEnviro();
+  }
+
+  function drawEnviro()
+  {
+    $this->img = imagecreate($this->largeurImg,$this->hauteurImg);
+    $hexa = hexa2rvb($this->fond);
+    imagecolorallocate($this->img, $hexa[0],$hexa[1],$hexa[2]);
+  }
+
+  function hexa2rvb($hexa)
+  {
+    $hexa = str_replace('#', '', $hexa);
+    for ($i=0; $i<3; $i++){
+      $tab[$i]=hexdec(substr($hexa, (2 * $i),2));
+    }
+    return $tab;
+  }
+
+  function initColor()
+  {
+    $tabColor=$this->hexa2rvb($this->colorArette);
+    $this->colorArette= imagecolorallocate($this->img, $tabColor[0],$tabColor[1],$tabColor[2]);
+    $tabColor=$this->hexa2rvb($this->texte);
+    $this->texte= imagecolorallocate($this->img, $tabColor[0],$tabColor[1],$tabColor[2]);
+    $tabColor=$this->hexa2rvb($this->colorLegende);
+    $this->colorLegende= imagecolorallocate($this->img, $tabColor[0],$tabColor[1],$tabColor[2]);
+  }
+
+  function drawGraph()
+  {
+    $this->centreXCam=($this->largeurImg - $this->largeurLegend - $this->padding - $this->padding) / 2 + $this->padding;
+    $this->centreYCam=($this->hauteurImg - $this->padding - $this->padding) / 2  + $this->padding;
+
+    if ($this->ombrage>0)
+      $this->centreYCam=($this->centreYCam-20);
+
+    $tab[]=$this->centreXCam - ($this->largeurLegend / 2) - $this->padding - $this->padding;
+    $tab[]=$this->centreYCam - $this->padding - $this->padding;
+
+    if ($tab[0]>$tab[1])$tab[3]=$tab[1];
+    if ($tab[0]<$tab[1])$tab[3]=$tab[0];
+
+    $this->largeurCam=($tab[3] * 2);
+    $this->hauteurCam=(($tab[3] - ($tab[3] * $this->inclinaison / 100)) *2);
+  }
+
+
+  function data($v, $c, $com)
+  {
+    $tabColor=$this->hexa2rvb($c);
+    $this->tabColor[]= imagecolorallocate($this->img, $tabColor[0],$tabColor[1],$tabColor[2]);
+
+    $tabColor1[0]=round($tabColor[0]* (1.05 - $this->ombre));
+    $tabColor1[1]=round($tabColor[1]* (1.05 - $this->ombre));
+    $tabColor1[2]=round($tabColor[2]* (1.05 - $this->ombre));
+    $this->tabColorOmbre[]= imagecolorallocate($this->img, $tabColor1[0],$tabColor1[1],$tabColor1[2]);
+
+    $this->tabValue[]= $v;
+    $this->tabComment[]= $com;
+  }
+
+
+  function dataTreat()
+  {
+    $this->nbrdata=sizeof($this->tabValue);
+    $total='';
+    for ($i=0; $i<$this->nbrdata; $i++)
+      $total+=$this->tabValue[$i];
+
+    for ($i=0; $i<$this->nbrdata; $i++)
+      $this->tabPourc[$i]=($this->tabValue[$i] * 100 / $total);
+
+    for ($i=0; $i<$this->nbrdata; $i++)
+      $this->tabAngle[$i]=($this->tabPourc[$i] * 360 / 100);
+  }
+
+
+  function traceArc($img, $ombre=0)
+  {
+    if ($ombre==0)
+    {
+      for ($j=$this->prodondeurGraph; $j>0; $j--)
+      {
+        $sommeAngle=0;
+        for ($i=0; $i<$this->nbrdata; $i++)
+        {
+          $aStart=((isset($this->tabAngle[$i-1]))? $aStart+$this->tabAngle[$i-1] : 0);
+          imagefilledarc( $img,
+                          ($this->centreXCam + ($this->spacing * cos(deg2rad($sommeAngle + ($this->tabAngle[$i] / 2))))),
+                          (($this->centreYCam +  $j ) + ($this->spacing * sin(deg2rad((($sommeAngle + ($this->tabAngle[$i] / 2)>90 AND $sommeAngle + ($this->tabAngle[$i] / 2)<270)? ($sommeAngle + ($this->tabAngle[$i] / 2)) : ($sommeAngle + ($this->tabAngle[$i] / 2))))))),
+                          $this->largeurCam,
+                          $this->hauteurCam,
+                          $aStart,
+                          ($aStart + $this->tabAngle[$i]),
+                          $this->tabColorOmbre[$i],
+                          IMG_ARC_EDGED);
+
+          $sommeAngle+=$this->tabAngle[$i];
+        }
+      }
+    }
+
+    $sommeAngle=0;
+    for ($i=0; $i<$this->nbrdata; $i++)
+    {
+      $aStart=((isset($this->tabAngle[$i-1]))? $aStart+$this->tabAngle[$i-1] : 0);
+      imagefilledarc($img,
+                     ($this->centreXCam + ($this->spacing * cos(deg2rad($sommeAngle + ($this->tabAngle[$i] / 2))))),
+                     (($this->centreYCam +  0) + ($this->spacing * sin(deg2rad((($sommeAngle + ($this->tabAngle[$i] / 2)>90 AND $sommeAngle + ($this->tabAngle[$i] / 2)<270)? ($sommeAngle + ($this->tabAngle[$i] / 2)) : ($sommeAngle + ($this->tabAngle[$i] / 2))))))),
+                     $this->largeurCam,
+                     $this->hauteurCam,
+                     $aStart,
+                     ($aStart + $this->tabAngle[$i]),
+                     $this->tabColor[$i],
+                     IMG_ARC_EDGED);
+      $sommeAngle+=$this->tabAngle[$i];
+    }
+  }
+
+  function markValeur()
+  {
+    $sommeAngle=0;
+    for ($i=0; $i<$this->nbrdata; $i++)
+    {
+      if (round($this->tabPourc[$i], $this->decimal)>0)
+      {
+        $px1=($this->centreXCam + ((($this->largeurCam / 4) * cos(deg2rad($sommeAngle + ($this->tabAngle[$i] / 2))))));
+        $py1=(($this->centreYCam ) + (($this->spacing + ($this->hauteurCam / 4)) * sin(deg2rad((($sommeAngle + ($this->tabAngle[$i] / 2)>90 AND $sommeAngle + ($this->tabAngle[$i] / 2)<270)? ($sommeAngle + ($this->tabAngle[$i] / 2)) : ($sommeAngle + ($this->tabAngle[$i] / 2)))))));
+
+        $px2=($this->centreXCam + (($this->spacing + $this->largeurCam /2 + $this->largeurAretteExterne) * cos(deg2rad($sommeAngle + ($this->tabAngle[$i] / 2)))));
+        $py2=(($this->centreYCam ) + (($this->spacing + $this->hauteurCam / 2 + $this->largeurAretteExterne) * sin(deg2rad((($sommeAngle + ($this->tabAngle[$i] / 2)>90 AND $sommeAngle + ($this->tabAngle[$i] / 2)<270)? ($sommeAngle + ($this->tabAngle[$i] / 2)) : ($sommeAngle + ($this->tabAngle[$i] / 2)))))));
+
+        imageline($this->img, $px1, $py1, $px2, $py2, $this->colorArette);
+
+        $px3=($px2<$this->centreXCam)? $px2 - $this->largeurAretteExterne : $px2 + $this->largeurAretteExterne;
+        imageline($this->img, $px2, $py2, $px3, $py2, $this->colorArette);
+
+        $px4=($px2<$this->centreXCam)? $px3 : $px2 ;
+        imagestring($this->img, 2, $px4, (($py2 < ($this->centreYCam + 10))? ($py2-12) : $py2) , round($this->tabPourc[$i], $this->decimal).'%', $this->texte);
+        $sommeAngle+=$this->tabAngle[$i];
+      }
+    }
+  }
+
+
+  function ombrage()
+  {
+    for ($i=$this->ombrage; $i>0; $i=($i - 3))
+    {
+      $this->imgTemp = imagecreatetruecolor($this->largeurImg, $this->hauteurImg);
+
+      $indexColorTransparente = imagecolorexact($this->imgTemp,0,0,0);
+        ImageColorTransparent($this->imgTemp,$indexColorTransparente);
+
+      $this->traceArc($this->imgTemp, $i);
+
+      imagecopymerge($this->img,
+                     $this->imgTemp,
+                     0,
+                     (20 + $i),
+                     0,
+                     0,
+                     $this->largeurImg,
+                     $this->hauteurImg,
+                     ($this->ombrage - $i));
+
+      imagedestroy($this->imgTemp);
+    }
+  }
+
+
+  function legend()
+  {
+    $x1=($this->largeurImg - $this->padding) - $this->largeurLegend;
+    imagefilledrectangle($this->img,
+                         $x1,
+                         $this->padding,
+                         ($this->largeurImg - $this->padding),
+                         ($this->hauteurImg - $this->padding),
+                         $this->colorLegende);
+
+    for ($i=0; $i<$this->nbrdata; $i++)
+    {
+      imagefilledrectangle($this->img,
+                           ($x1 +10),
+                           ($this->padding + 10 + ($i * 23)),
+                           ($x1 +20),
+                           ($this->padding +20 + ($i * 23)),
+                           $this->tabColor[$i]);
+      imagestring($this->img,
+                  2,
+                  ($x1 + 20 + 5 ),
+                  ($this->padding +20 + ($i * 23) - 12 ),
+                  $this->tabComment[$i].' ('.round($this->tabValue[$i], $this->decimal).')',
+                  $this->texte);
+    }
+  }
+
+
+  function png_render ($watermark=true)
+  {
+    $this->initColor();
+    $this->drawGraph();
+    $this->dataTreat();
+    if ($this->ombrage>0)
+      $this->ombrage();
+
+    $this->traceArc($this->img, false);
+
+    $this->markValeur();
+
+    if ($this->largeurLegend>20)
+      $this->legend();
+
+    header("Content-Type: image/png");
+    if($watermark)
+    {
+      $img_wmarked = new img_watermark (imagecreatefrompng($this->img));
+      imagepng($img_wmarked);
+    }
+    else
+      imagepng($this->img);
+  }
+
 }
