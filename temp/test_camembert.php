ba90da89aa82f0c55aeddc9fbcd05bf23f65c072
Ayolo <ayolo@troll>
2007-07-28 01:13:45 +0000

    fix

--- temp/test_camembert.php
+++ temp/test_camembert.php
@@ -30,7 +30,7 @@ $valeur=array('50',
               '65',
 							'35');
 
-$cam=new camembert();
+$cam=new camembert(500,500);
 
 for ($i=0; $i<count($valeur); $i++)
   $cam->data($valeur[$i],$color[$i], '');
