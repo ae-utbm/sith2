183349c0e0b7d2fa79b12106a33c6dee1e6f7ea3
Ayolo <ayolo@troll>
2007-07-28 13:26:58 +0000

    watermark

--- include/watermark.inc.php
+++ include/watermark.inc.php
@@ -45,8 +45,8 @@ class img_watermark
   var $opacity;
 
   function img_watermark ($image_to_mark,
-			  $watermark = "/var/www/ae/www/var/img/ae_watermark.png",
-			  $opacity = 9)
+        $watermark = "/var/www/ae/www/var/img/ae_watermark.png",
+        $opacity = 9)
   {
     $this->opacity = $opacity;
     /* watermark temporaire */
@@ -54,13 +54,13 @@ class img_watermark
     switch (exif_imagetype($watermark))
       {
       case IMAGETYPE_JPEG:
-	$temp_wm = imagecreatefromjpeg ($watermark);
-	break;
+  $temp_wm = imagecreatefromjpeg ($watermark);
+  break;
       case IMAGETYPE_PNG:
-	$temp_wm = imagecreatefrompng($watermark);
-	break;
+  $temp_wm = imagecreatefrompng($watermark);
+  break;
       default:
-	die ("Format Watermark non supporte");
+  die ("Format Watermark non supporte");
       }
     /* taille du watermark temporaire */
     $temp_wm_dim = getimagesize($watermark);
@@ -77,35 +77,36 @@ class img_watermark
 
     /* creation du watermark a la taille de l'image a marquer */
     $this->res_wm = imagecreatetruecolor($this->size[0],
-					 $this->size[1]);
-    imagecolorallocate($this->res_wm, 255,255,255);
+           $this->size[1]);
+    $fond = imagecolorallocate($this->rw_wm, 255, 255, 255);
+    imagefilledrectangle($this->rw_wm,0,0,$this->size[0],$this->size[1],$fond);
     /* calcul du positionnement dans l'image */
     $dest['x'] = ($this->size[0] - $size) / 2;
     $dest['y'] = ($this->size[1] - $size) / 2;
 
     /* copie watermark original vers watermark taille finale */
     imagecopyresized($this->res_wm,
-		     $temp_wm,
-		     $dest['x'],
-		     $dest['y'],
-		     0,
-		     0,
-		     $size,
-		     $size,
-		     $temp_wm_dim[0],
-		     $temp_wm_dim[1]);
+         $temp_wm,
+         $dest['x'],
+         $dest['y'],
+         0,
+         0,
+         $size,
+         $size,
+         $temp_wm_dim[0],
+         $temp_wm_dim[1]);
 
 
     /* merging a proprement parler */
     imagecopymerge($this->img,
-		   $this->res_wm,
-		   0,
-		   0,
-		   0,
-		   0,
-		   $this->size[0],
-		   $this->size[1],
-		   $this->opacity);
+       $this->res_wm,
+       0,
+       0,
+       0,
+       0,
+       $this->size[0],
+       $this->size[1],
+       $this->opacity);
     imagedestroy ($this->res_wm);
     imagedestroy ($temp_wm);
   }
