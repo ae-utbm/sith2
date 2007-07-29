35565838bcb3f2d273a40ff50a8f6f35ffdfa939
Ayolo <ayolo@troll>
2007-07-29 23:00:47 +0000

    stats svn

---
+++ temp/test_stats_svn.php
@@ -0,0 +1,20 @@
+<?php
+include("../include/graph.inc.php");
+
+$cam=new camembert(600,500,array(),2,0,0,0,0,0,0,10,150);
+
+$svn=exec ("/usr/share/php5/exec/rev_info.sh");
+
+$svn=explode("|",$svn);
+$stast_svn=array();
+for($i=0;$i<count($svn);$i++)
+{
+  if($empty($svn[$i]))
+  {
+    $tmp=explode(" ",$svn[$i]));
+    $cam->data($tmp[1],$tmp[0]);
+  }
+}
+$cam->png_render();
+exit();
+?>
