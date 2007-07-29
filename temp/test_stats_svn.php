40439421d8396c00ec74ba5fbacc29152c3441d2
Ayolo <ayolo@troll>
2007-07-29 23:07:52 +0000

    debug

--- temp/test_stats_svn.php
+++ temp/test_stats_svn.php
@@ -6,14 +6,12 @@ $cam=new camembert(600,500,array(),2,0,0,0,0,0,0,10,150);
 
 $svn=exec ("/usr/share/php5/exec/svn_stats.sh");
 
-$svn=explode("|",$svn);
-$stast_svn=array();
+$svn=explode("|",$svn,-1);
 for($i=0;$i<count($svn);$i++)
 {
   if(!empty($svn[$i]))
   {
 		$tmp=explode(" ",$svn[$i]);
-    print_r($tmp);
     $cam->data($tmp[1],$tmp[0]);
   }
 }
