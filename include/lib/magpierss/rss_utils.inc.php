e304095d453a91b7a65ef82c5dd3c6c8e06fc212
Ayolo <ayolo@troll>
2007-07-31 19:03:59 +0000

    grrr

--- include/lib/magpierss/rss_utils.inc.php
+++ include/lib/magpierss/rss_utils.inc.php
@@ -26,7 +26,7 @@ function parse_w3cdtf ( $date_str ) {
     # regex to match wc3dtf
     $pat = "/(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2})(:(\d{2}))?(?:([-+])(\d{2}):?(\d{2})|(Z))?/";
     if ( preg_match( $pat, $date_str, $match ) ) {
-			list( $year, $month, $day, $hours, $minutes, $seconds) = array( $match[1], $match[2], $match[3], $match[4], $match[5], $match[6]);
+      list( $year, $month, $day, $hours, $minutes, $seconds) = array( $match[1], $match[2], $match[3], $match[4], $match[5], $match[6]);
         $seconds=str_replace(":","",$seconds);
         # calc epoch for current date assuming GMT
         $epoch = gmmktime( $hours, $minutes, $seconds, $month, $day, $year);
@@ -53,7 +53,7 @@ function parse_w3cdtf ( $date_str ) {
             
             $offset = $offset_secs; 
         }
-				$epoch = $epoch + $offset;
+        $epoch = $epoch + $offset;
         return $epoch;
     }
     else {
