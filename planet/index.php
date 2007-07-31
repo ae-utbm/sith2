e57130b06a4ba104c31e619a1e6f1aa7540a6b70
Ayolo <ayolo@troll>
2007-07-31 19:26:35 +0000

    mouick

--- planet/index.php
+++ planet/index.php
@@ -153,7 +153,7 @@ else
             //$cts->add_title(2, $item['title']." (le ".date("d/m/Y h:i:s", $date).")");
             $_cts->puts($item['content']);
             $_cts->add_paragraph('<p align="right"><a href="'.$item['link'].'">Version complète</a></p><hr />');
-            $cts->add($cts);
+            $cts->add($_cts);
           }
         }
       }
