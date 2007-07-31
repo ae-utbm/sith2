a3d63fbcd4f6ef78d9def70bf2548504993ae02d
Ayolo <ayolo@troll>
2007-07-31 01:34:55 +0000

    fix

--- include/site.inc.php
+++ include/site.inc.php
@@ -626,7 +626,7 @@ class site extends interfaceweb
 		$sublist->add("<a href=\"".$topdir."user.php?id_utilisateur=".$this->user->id."\">Informations personnelles</a>");
 		$sublist->add("<a href=\"".$topdir."uvs/edt.php\">Mes emplois du temps</a>");
     if($this->user->utbm)
-      $sublist->add("<a href=\"".$topdir."trombino/index.php\">Trombinoscope</a>");
+      $sublist->add("<a href=\"".$topdir."trombi/index.php\">Trombinoscope</a>");
     $sublist->add("<a href=\"".$topdir."user.php?view=assos\">Associations et clubs</a>");
     $sublist->add("<a href=\"".$topdir."user.php?view=parrain\">Parrains et fillots</a>");
     $sublist->add("<a href=\"".$topdir."user/compteae.php\">Compte AE</a>");
