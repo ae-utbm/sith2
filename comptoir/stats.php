8585c3ba3447854772faabf7bf46660452102a08
BenC <benc@troll>
2007-08-01 13:01:03 +0000

    Bleh.

--- comptoir/stats.php
+++ comptoir/stats.php
@@ -86,14 +86,14 @@ if ( $_REQUEST["action"] == "view" )
     "SELECT `cpt_produits`.`nom_prod`, `cpt_produits`.`id_produit`," .
     "`asso`.`nom_asso`,`asso`.`id_asso`, " .
     "`cpt_type_produit`.`id_typeprod`,`cpt_type_produit`.`nom_typeprod`, " .
+    "`cpt_mise_en_vente`.`stock_local_prod`, " .
 		"(SELECT SUM(`cpt_vendu`.`quantite`) AS `ventes` FROM `cpt_vendu` WHERE `cpt_vendu`.`id_produit`=`cpt_produits`.`id_produit`) AS `ventes` ".
-    "FROM `cpt_vendu` " .
-		"INNER JOIN `cpt_produits` ON `cpt_produits`.`id_produit`=`cpt_vendu`.`id_produit` " .
-		"INNER JOIN `asso` ON `asso`.`id_asso`=`cpt_produits`.`id_assocpt` " .
-		"INNER JOIN `cpt_type_produit` ON `cpt_type_produit`.`id_typeprod`=`cpt_produits`.`id_typeprod` " .
-		"INNER JOIN `cpt_mise_en_vente` ON `cpt_mise_en_vente`.`id_produit`=`cpt_produits`.`id_produit` ".
-		"INNER JOIN `cpt_debitfacture` ON `cpt_debitfacture`.`id_facture`=`cpt_vendu`.`id_facture` " .
-    "WHERE " . implode(" AND ",$conds).
+    "FROM `cpt_produits` " .
+    "INNER JOIN `cpt_type_produit` ON `cpt_type_produit`.`id_typeprod`=`cpt_produits`.`id_typeprod` " .
+		"INNER JOIN `cpt_vendu` ON `cpt_vendu`.`id_produit` =`cpt_produits`.`id_produit` " .
+    "INNER JOIN `asso` ON `asso`.`id_asso`=`cpt_produits`.`id_assocpt` " .
+    "INNER JOIN cpt_mise_en_vente ON `cpt_mise_en_vente`.`id_produit`=`cpt_produits`.`id_produit` " .
+    "WHERE `cpt_mise_en_vente`.`id_comptoir`='".$_REQUEST["id_comptoir"]."'" .
 		"ORDER BY `ventes` DESC,`cpt_type_produit`.`nom_typeprod`,`cpt_produits`.`nom_prod`");
 
   $tbl = new sqltable(
