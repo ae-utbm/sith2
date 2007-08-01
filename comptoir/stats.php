fc547aee6efa662ef93dd70580a95e62adeb8119
BenC <benc@troll>
2007-08-01 11:08:00 +0000

    Bleh.

--- comptoir/stats.php
+++ comptoir/stats.php
@@ -89,7 +89,9 @@ if ( $_REQUEST["action"] == "view" )
     "`cpt_mise_en_vente`.`stock_local_prod`, " .
 		"(SELECT SUM(`cpt_vendu`.`quantite`) AS `ventes` FROM `cpt_vendu` WHERE `cpt_vendu`.`id_produit`=`cpt_produits`.`id_produit` ) AS `ventes` ".
     "FROM `cpt_produits` " .
+		"INNER JOIN `cpt_produits` ON `cpt_produits`.`id_produit` =`cpt_vendu`.`id_produit` " .
     "INNER JOIN `cpt_type_produit` ON `cpt_type_produit`.`id_typeprod`=`cpt_produits`.`id_typeprod` " .
+    "INNER JOIN `cpt_debitfacture` ON `cpt_debitfacture`.`id_facture` =`cpt_vendu`.`id_facture` " .
     "INNER JOIN `asso` ON `asso`.`id_asso`=`cpt_produits`.`id_assocpt` " .
     "INNER JOIN cpt_mise_en_vente ON `cpt_mise_en_vente`.`id_produit`=`cpt_produits`.`id_produit` " .
     "WHERE `cpt_mise_en_vente`.`id_comptoir`='".$_REQUEST["id_comptoir"]."' AND " . implode(" AND ",$conds) .
