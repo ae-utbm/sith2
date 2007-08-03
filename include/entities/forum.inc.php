78e9142c69715ab9c69f812db5b0b48af2433c02
BenC <benc@troll>
2007-08-03 10:28:44 +0000

    Bouleeeeeeeeeeeet

--- include/entities/forum.inc.php
+++ include/entities/forum.inc.php
@@ -331,16 +331,6 @@ class forum extends basedb
     }
     
   }
-
-  /**
-	* Permet de faire des remplacements au moment du commit
-	*/
-  function commit_replace($text,$alias)
-	{
-	  $text = preg_replace("/(\n|^)\/me\s/","\n* ".$alias." ",$text);
-
-		return $text;
-	}
 }
 
 ?>
