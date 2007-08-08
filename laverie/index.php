344aa74835b5b671c2b21023a61a3d420f31c872
BenC <benc@troll>
2007-08-08 22:48:48 +0000

    Et la première pière fut posée, sans moufles

---
+++ laverie/index.php
@@ -0,0 +1,39 @@
+<?php
+
+/* Copyright 2007
+ * - Benjamin Collet < bcollet AT oxynux DOT org >
+ *
+ * Ce fichier fait partie du site de l'Association des Étudiants de
+ * l'UTBM, http://ae.utbm.fr.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+ 
+$topdir = "../";
+require_once($topdir. "include/site.inc.php");
+
+$site = new site ();
+
+$site->start_page("none","Machines");
+$cts = new contents("Machines à laver de l'AE");
+
+$cts->add_paragraph("<br />Ici trônera joyeusement l'interface cotisant-machine à laver.");
+$cts->add_paragraph("Le tout via un système révolutionnaire de responsables machines nourris au Bob AE et à la cancoyottte afin de vous assurer une productivité parfaite dans le nettoyage de vos chaussettes sales et de vos caleçons dégueus (non rien pour les filles, elle sont juste un mythe à l'UTBM).");
+
+$site->add_contents($cts);
+$site->end_page();  
+
+?>
