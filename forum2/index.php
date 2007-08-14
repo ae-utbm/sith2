d43598943b22b3a1f887850c2d0924bfacb17f44
Sam <sam@troll>
2007-08-14 08:07:04 +0000

    Ajout d'un lien vers les messages non lus dans le liner

--- forum2/index.php
+++ forum2/index.php
@@ -44,7 +44,7 @@ $site->add_css("css/forum.css");
 $cts = new contents();
 $cts->cssclass="liner";
 if ( $site->user->is_valid() )
-  $cts->buffer = "<p class=\"center\">Connecté en tant que ".$site->user->alias." - <a href=\"../user.php?page=edit\">Mon profil</a></p>";
+  $cts->buffer = "<p class=\"center\">Connecté en tant que ".$site->user->alias." - <a href=\"../user.php?page=edit\">Mon profil</a> - <a href=\"search.php?page=unread\">Messages non lus</a></p>";
 else
   $cts->buffer = "<p class=\"center\">Non connecté - <a href=\"../index.php\">Se connecter</a> - <a href=\"../newaccount.php\">Creer un compte</a></p>";
 $site->add_contents($cts);
