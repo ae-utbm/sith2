7e874df0c8cb7ada59f6380b90fe492928dbede3
Ayolo <ayolo@troll>
2007-08-16 00:53:17 +0000

    debug

--- include/graph.inc.php
+++ include/graph.inc.php
@@ -372,7 +372,7 @@ class histogram
                  "set boxwidth 0.9\n".
 								 "set title \"".$title."\"\n".
 								 "tics out\n".
-                 "set xtics border nomirror rotate 45 offset character 0,0,0\n".
+                 "set xtics border nomirror rotate by 45 offset character 0,0,0\n".
                  "plot '".$this->data_file."' using 2:xtic(1) title col";
 
     $minvalue = 0;
