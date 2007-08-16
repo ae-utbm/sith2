9b371dacb634f82009d0a0f4a17fe3ea8b1de2b7
Ayolo <ayolo@troll>
2007-08-16 02:16:34 +0000

    debug

--- include/lib/dokusyntax.inc.php
+++ include/lib/dokusyntax.inc.php
@@ -694,10 +694,21 @@ function tableformat($block)
     {
       $GLOBALS['js']=true;
       $_js = "<script language=\"JavaScript\">\n";
-      $_js.= "function switchid(obj,id,p){\n";
-      $_js.= "document.getElementById(id+'Table').style.display = 'none';\n";
-      $_js.= "document.getElementById(id+'Graph').style.display = 'none';\n";
-      $_js.= "document.getElementById(id+p).style.display = 'block';\n";
+			$_js.= "function switchid(obj,id,p){\n";
+			$_js.= "if(p==1)\n";
+			$_js.= "{\n";
+			$_js.= "table = document.getElementById(id+1);\n";
+      $_js.= "table.style.display = 'none';\n";
+      $_js.= "graph = document.getElementById(id+2);\n";
+			$_js.= "graph.style.display = 'block';\n";
+			$_js.= "}\n";
+			$_js.= "else\n";
+			$_js.= "{\n";
+			$_js.= "graph = document.getElementById(id+2);\n";
+			$_js.= "graph.style.display = 'none';\n";
+			$_js.= "table = document.getElementById(id+1);\n";
+			$_js.= "table.style.display = 'block';\n";
+			$_js.= "}\n";
       $_js.= "}\n";
       $_js.="</script>\n";
     }
@@ -721,11 +732,11 @@ function tableformat($block)
       $total=round($total,0);
       $id=substr(md5(microtime(true)), 0, 6);
       $_ret ="<div id=\"".$id."\" class=\"tabs\">\n";
-      $_ret.="<span id=\"".$id."T\" class=\"selected\"><a href=\"javascript:switchid(this,$id,Graph);\" id=\"".$id."TA\" class=\"selected\" title=\"Tableau\">Tableau</a></span>\n";
-      $_ret.="<span id=\"".$id."G\"><a href=\"javascript:switchid(this,$id,Table);\" id=\"".$id."GA\" title=\"Graph\">Graph</a></span>\n";
+      $_ret.="<span id=\"".$id."T\" class=\"selected\"><a href=\"javascript:switchid(this,$id,1);\" id=\"".$id."TA\" class=\"selected\" title=\"Tableau\">Tableau</a></span>\n";
+      $_ret.="<span id=\"".$id."G\"><a href=\"javascript:switchid(this,$id,2);\" id=\"".$id."GA\" title=\"Graph\">Graph</a></span>\n";
       $_ret.="</div>\n";
-      $_ret.="<div id=\"".$id."Table\" style=\"display:block;\">".$ret."</div>\n";
-      $_ret.="<div id=\"".$id."Graph\" style=\"display:none;\">";
+      $_ret.="<div id=\"".$id."1\" style=\"display:block;\">".$ret."</div>\n";
+      $_ret.="<div id=\"".$id."2\" style=\"display:none;\">";
       if($total==100)
         $_ret.= "<img src=\"".$topdir."gen_graph.php?action=cam&values=".$data."\" /></div>\n";
       else
