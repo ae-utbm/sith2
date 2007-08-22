34ccf6e43255d44462d242c19914be8b373af35b
Feu <feu@troll>
2007-08-22 01:39:13 +0000

    streamons

--- stream.php
+++ stream.php
@@ -44,41 +44,68 @@ if ( $_REQUEST["get"] == "qt.pls" )
 }
 
 
-if ( !$GLOBALS["streaminfo"]["mp3"] )
+
+if ( $_REQUEST["get"] == "popup" )
 {
-  echo "<p>Indisponible actuellement</p>";
-  exit();  
-}
-
-$plug = "quicktime";
-
-if (isset($_REQUEST["plug"]))
-  $plug = $_REQUEST["plug"];
-
-
-if ( $plug == "wmp" )
-{
-  echo "<object classid=\"CLSID:22d6f312-b0f6-11d0-94ab-0080c74c7e95\" 
-  codebase=\"http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=5,1,52,701\" 
-  type=\"application/x-oleobject\">
-  							<param name=\"filename\" value=\"".$GLOBALS["streaminfo"]["mp3"]."\">
-  							<param name=\"autostart\" value=\"1\">
-  							<embed src=\"".$GLOBALS["streaminfo"]["mp3"]."\"
-  							autostart=\"1\" type=\"application/x-mplayer2\"
-  							pluginspage=\"http://www.microsoft.com/Windows/MediaPlayer/download/default.asp\">
-  							</embed></object>";
-}
-else
-{
-  echo "<object classid=\"clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B\"
-   codebase=\"http://www.apple.com/qtactivex/qtplugin.cab#version=6,0,2,0\">
-  <param name=\"src\" value=\"http://".$_SERVER["HTTP_HOST"].$_SERVER["SCRIPT_NAME"]."?get=qt.pls\" />
-  <param name=\"autoplay\" value=\"true\" />
-  <embed type=\"image/x-quicktime\" src=\"http://".$_SERVER["HTTP_HOST"].$_SERVER["SCRIPT_NAME"]."?get=qt.pls\"
-    pluginspage=\"http://www.apple.com/quicktime/download/\"
-    autoplay=\"true\" qtsrc=\"http://".$_SERVER["HTTP_HOST"].$_SERVER["SCRIPT_NAME"]."?get=qt.pls\"></embed></object>";  
+  if ( !$GLOBALS["streaminfo"]["mp3"] )
+  {
+    echo "<p>Indisponible actuellement</p>";
+    exit();  
+  }
   
+  echo "<h1>Superflux</h1>";
+  
+  $plug = "quicktime";
+  
+  if (isset($_REQUEST["plug"]))
+    $plug = $_REQUEST["plug"];
+  
+  $plugins = array("quicktime"=>"QuickTime","wmp"=>"Windows Media Player");
+  
+  if ( $plug == "wmp" )
+  {
+    echo "<object classid=\"CLSID:22d6f312-b0f6-11d0-94ab-0080c74c7e95\" 
+    codebase=\"http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=5,1,52,701\" 
+    type=\"application/x-oleobject\">
+    							<param name=\"filename\" value=\"".$GLOBALS["streaminfo"]["mp3"]."\">
+    							<param name=\"autostart\" value=\"1\">
+    							<embed src=\"".$GLOBALS["streaminfo"]["mp3"]."\"
+    							autostart=\"1\" type=\"application/x-mplayer2\"
+    							pluginspage=\"http://www.microsoft.com/Windows/MediaPlayer/download/default.asp\">
+    							</embed></object>";
+  }
+  else
+  {
+    echo "<object classid=\"clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B\"
+     codebase=\"http://www.apple.com/qtactivex/qtplugin.cab#version=6,0,2,0\">
+    <param name=\"src\" value=\"http://".$_SERVER["HTTP_HOST"].$_SERVER["SCRIPT_NAME"]."?get=qt.pls\" />
+    <param name=\"autoplay\" value=\"true\" />
+    <embed type=\"image/x-quicktime\" src=\"http://".$_SERVER["HTTP_HOST"].$_SERVER["SCRIPT_NAME"]."?get=qt.pls\"
+      pluginspage=\"http://www.apple.com/quicktime/download/\"
+      autoplay=\"true\" qtsrc=\"http://".$_SERVER["HTTP_HOST"].$_SERVER["SCRIPT_NAME"]."?get=qt.pls\"></embed></object>";  
+  }
+  
+  echo "<p>Lecteur audio :</p>";
+  echo "<ul>";
+  foreach ( $plugins as $key => $desc)
+  {
+    echo "<li><a href=\"?get=popup&amp;plug=$key\">$desc</a></li>";
+  }
+  echo "</ul>";
+  exit();
 }
+
+
+$site->start_page("stream","Streaming");
+
+$cts = new contents("Superflux, la webradio");
+
+$cts->add_paragraph("<a href=\"stream.php?get=popup\" onclick=\"return popUpStream();\">Ecouter avec le lecteur web</a>");
+
+$site->add_contents($cts);
+
+$site->end_page();
+
 
 
 ?>
