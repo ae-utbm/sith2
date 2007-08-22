9d6b5caa93506f1f2b3a83a3624c69ba7f67d0af
BenC <benc@troll>
2007-08-22 13:28:33 +0000

    Ebauche jetons

--- laverie/index.php
+++ laverie/index.php
@@ -38,8 +38,26 @@ if ( $site->is_admin )
 $site->start_page("none","Machines");
 $cts = new contents("Machines à laver de l'AE");
 
-$cts->add_paragraph("<br />Ici trônera joyeusement l'interface cotisant-machine à laver.");
-$cts->add_paragraph("Le tout via un système révolutionnaire de responsables machines nourris au Bob AE et à la cancoillote afin de vous assurer une productivité parfaite dans le nettoyage de vos chaussettes sales et de vos caleçons dégueus (non rien pour les filles, elle sont juste un mythe à l'UTBM).");
+if ( $_REQUEST['action'] == "inventaire" )
+{
+	$cts->add_title(2,"Nombre de jetons");
+
+	$req = new requete($site->db,"SELECT COUNT(*) FROM `mc_jeton`");
+	list($total) = $req->get_row();
+	$cts->add_paragraph("Total : $total");
+	$req = new requete($site->db,"SELECT COUNT(*) FROM `mc_jeton_utilisateur` WHERE `retour_jeton` IS NULL");
+	list($utilises) = $req->get_row();
+	$cts->add_paragraph("En circulation : $utilises");
+	$disponibles = $total - $utilises;
+	$cts->add_paragraph("En caisse : $disponibles");
+
+
+}
+else
+{
+  $cts->add_paragraph("<br />Ici trônera joyeusement l'interface cotisant-machine à laver.");
+  $cts->add_paragraph("Le tout via un système révolutionnaire de responsables machines nourris au Bob AE et à la cancoillote afin de vous assurer une productivité parfaite dans le nettoyage de vos chaussettes sales et de vos caleçons dégueus (non rien pour les filles, elle sont juste un mythe à l'UTBM).");
+}
 
 $site->add_contents($cts);
 $site->end_page();  
