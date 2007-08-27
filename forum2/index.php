23b410d977e365739a1db6f5612efe83e5f01ef8
gliss <gliss@troll>
2007-08-27 08:53:13 +0000

    demande de Pintos

--- forum2/index.php
+++ forum2/index.php
@@ -152,7 +152,7 @@ if ( !$forum->is_right($site->user,DROIT_LECTURE) )
 if ( $_REQUEST["action"] == "setallread" )
 {
   $site->allow_only_logged_users("forum");
-  $site->user->set_all_read();
+  $site->user->set_all_read( $wwwtopdir."forum2/index.php" );
 }
 
 
--- include/entities/utilisateur.inc.php
+++ include/entities/utilisateur.inc.php
@@ -1757,7 +1757,7 @@ L'équipe info AE";
     return $user->is_valid();
   }
   
-  function set_all_read ( )
+  function set_all_read ( $page = NULL )
   {
     
     // supprime les frm_sujet_utilisateur qui ne servirons plus à rien
@@ -1771,6 +1771,9 @@ L'équipe info AE";
     $req = new update($this->dbrw,"utilisateurs",
                       array("tout_lu_avant_utl"=>date("Y-m-d H:i:s")),
                       array("id_utilisateur"=>$this->id));
+                      
+    if( $page )
+    	header("Location: $page");
   }
   
   
