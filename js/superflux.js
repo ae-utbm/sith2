634ee0faaf857df2eedf293edbfa8e8cfb5cef6c
Ayolo <ayolo@troll>
2007-08-27 14:29:40 +0000

    bleh le tiemer

--- js/superflux.js
+++ js/superflux.js
@@ -3,7 +3,7 @@ function updateStreamInfo()
 {
   if ( document.getElementById("streaminfo") )
     openInContents( "streaminfo", "stream.php", "get=info" );
-  setTimeout("updateStreamInfo()",10);
+  setTimeout("updateStreamInfo()",10000);
 }
 
 updateStreamInfo();
