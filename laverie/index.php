328b131590ccb7fa767185edd07206222354051d
BenC <benc@troll>
2007-09-02 10:40:11 +0000

    Bleh

--- laverie/index.php
+++ laverie/index.php
@@ -366,7 +366,7 @@ if ( !$site->user->is_in_group("blacklist_machines") )
 		/* Liste des machines */
 		$sql = new requete($site->db, "SELECT * FROM mc_machines
 			INNER JOIN loc_lieu ON mc_machines.loc = loc_lieu.id_lieu
-			WHERE mc_machines.hs = 0";
+			WHERE mc_machines.hs = 0");
 
 		$table = new sqltable("listmachinesok",
 			"Liste des machines en service",
