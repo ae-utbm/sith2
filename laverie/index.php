0993d95684d4821d5207491ed0ef1c0a8027b195
BenC <benc@troll>
2007-09-02 11:15:29 +0000

    Bleh

--- laverie/index.php
+++ laverie/index.php
@@ -394,6 +394,13 @@ if ( !$site->user->is_in_group("blacklist_machines") )
 				$lst->add("La machine $id  a bien été supprimée","ok");
 			}
 		}
+
+		if (!empty($_REQUEST["lettre_machine"]) )
+		{
+			$sql = new requete($site-dbrw, "INSERT INTO mc_machines
+				('lettre','type','loc')
+				VALUES ('$_REQUEST["lettre_machine"]', '$_REQUEST["typemachine"]', '$_REQUEST["locmachine"]')");
+		}
 		
 		$frm = new form("ajoutmachine", "index.php?view=machines", false, "POST", "Ajouter une machine");
 
