9f4f1c06bafd53e44cd57e116aee0bf306e404df
BenC <benc@troll>
2007-09-08 15:34:23 +0000

    Test

--- laverie/index.php
+++ laverie/index.php
@@ -555,10 +555,9 @@ if ( !$site->user->is_in_group("blacklist_machines") )
 		$sql = new requete($site->db, "SELECT * FROM mc_machines
 			LEFT JOIN pl_planning ON mc_machines.id = pl_planning.name_planning
 			INNER JOIN loc_lieu ON mc_machines.loc = loc_lieu.id_lieu
-			WHERE mc_machines.hs = 0
-			AND pl_planning.start_date_planning > '".$now."'
-			AND ( (pl_planning.start_date_planning < '".$now."' AND pl_planning.end_date_planning >= '".$now."')
-			OR (pl_planning.end_date_planning IS NULL AND pl_planning.start_date_planning IS NULL) )
+			WHERE ( mc_machines.hs = 0
+			AND NOT (pl_planning.start_date_planning < '".$now."' AND pl_planning.end_date_planning > '".$now."') )
+			OR (pl_planning.end_date_planning IS NULL AND pl_planning.start_date_planning IS NULL)
 			ORDER BY mc_machines.lettre,mc_machines.type");
 
 		$table = new sqltable("listmachinesencours",
