bf1afa2d1fc0a859f1075bca677f86f35d4b402f
BenC <benc@troll>
2007-09-13 15:31:04 +0000

    Test

--- laverie/index.php
+++ laverie/index.php
@@ -560,9 +560,7 @@ if ( !$site->user->is_in_group("blacklist_machines") )
 			LEFT JOIN pl_planning ON mc_machines.id = pl_planning.name_planning
 			INNER JOIN loc_lieu ON mc_machines.loc = loc_lieu.id_lieu
 			WHERE ( mc_machines.hs = 0
-			AND (NOT (pl_planning.start_date_planning < '".$now."' AND pl_planning.end_date_planning > '".$now."')
-			AND NOT pl_planning.start_date_planning > '".$now."'
-			AND NOT pl_planning.end_date_planning < '".$now."' )
+			AND NOT (pl_planning.start_date_planning < '".$now."' AND pl_planning.end_date_planning > '".$now."')
 			OR (pl_planning.end_date_planning IS NULL AND pl_planning.start_date_planning IS NULL) )
 			ORDER BY mc_machines.lettre,mc_machines.type");
 
