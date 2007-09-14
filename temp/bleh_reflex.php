e335ec485ff17f2b742739856410130dae0351e6
Ayolo <ayolo@troll>
2007-09-14 14:50:51 +0000

    gnia

---
+++ temp/bleh_reflex.php
@@ -0,0 +1,39 @@
+<?php
+/*
+  vive le taff à l'arrache !!!!!
+  licence agréée la-rache.com
+ */
+
+$topdir = "../";
+
+
+require_once($topdir. "include/site.inc.php");
+
+$site = new site ();
+
+if ($site->user->id > 0 )
+  exit();
+
+$user = new utilisateur($site->db,$site->dbrw);
+$user->load_by_id(3538);
+
+
+$req = new requete($site->db,"SELECT `id_photo` FROM `sas_photos` WHERE `id_asso_photographe`='43' AND `incomplet`='0'");
+
+$photo = new photo($site->db,$site->dbrw);
+
+while ( list($id) = $req_>gt_row() )
+{
+  $photo->load_by_id($id);
+  if ($photo->is_valid())
+  {
+    if ($photo->is_right($user,DROIT_LECTURE))
+    {
+      echo "ça poutre";
+    }
+  }
+}
+
+
+
+?>
