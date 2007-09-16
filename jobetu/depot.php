d8c0ad0d10e82f20a9f36566a59d6d1bbf64eaf8
gliss <gliss@troll>
2007-09-16 16:28:23 +0000

    wip

--- jobetu/depot.php
+++ jobetu/depot.php
@@ -64,9 +64,10 @@ if(!empty($_REQUEST['action']) && $_REQUEST['action']=="annonce")
 	$frm->add_text_area("profil", "Profil recherche", false, 60, 3, true);
 	$frm->add_date_field("date_debut", "Date de debut (facultatif)");
 	$frm->add_text_field("duree", "Duree (facultatif)");
-	$frm->add_text_field("remuneration", "Rémuneration en € (facultatif)");
+	$frm->add_text_field("remuneration", "Rémuneration (facultatif)");
+	$frm->add_text_field("nb_postes", "Nombre de postes disponibles", "1", true, 4);
 	$frm->add_text_area("divers", "Autres informations", false, 60, 3);
-	$frm->add_checkbox("allow_diff", "Diffuser aux candidats mon numéro de téléphone afin qu'ils puissent me contacter");
+	$frm->add_checkbox("allow_diff", "Diffuser mon numéro de téléphone aux candidats afin qu'ils puissent me contacter");
 	$frm->add_submit("go", "Enregistrer mon annonce");
 	
 	$cts->add($frm, true);
