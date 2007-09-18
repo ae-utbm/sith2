b3587fafb928277c359bff7997d7a36bcfae4fbd
pedrov <pedrov@troll>
2007-09-18 15:53:42 +0000

    vivement chez moi ...

---
+++ uvs/edit.php
@@ -0,0 +1,68 @@
+<?php
+/** @file
+ *
+ * @brief Page d'édition des emplois du temps.
+ *
+ */
+
+/* Copyright 2007
+ * - Pierre Mauduit <pierre POINT mauduit CHEZ utbm POINT fr>
+ *
+ * Ce fichier fait partie du site de l'Association des Étudiants de
+ * l'UTBM, http://ae.utbm.fr.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Sofware
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+$topdir = "../";
+
+include($topdir. "include/site.inc.php");
+
+require_once ($topdir . "include/entities/edt.inc.php");
+require_once ($topdir . "include/cts/edt_img.inc.php");
+
+
+$site = new site();
+
+$site->start_page("services", "Emploi du temps");
+
+/* protection d'usage */
+if (!$site->user->utbm)
+{
+	error_403("reservedutbm");
+}
+if (!$site->user->is_valid())
+{
+	error_403();
+}
+
+
+
+$edt = new edt($site->db, $site->dbrw);
+
+$semestre = mysql_real_escape_string($_REQUEST['semestre']);
+
+$edt->load($site->user->db, $semestre);
+
+
+$cts = new contents("Edition de l'emploi du temps", print_r($edt, true));
+
+$site->add($cts);
+
+$site->end_page();
+
+?>
+
