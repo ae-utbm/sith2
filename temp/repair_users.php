b6c86fb5759a24086bcb94f5f5304595f2438a8d
Ayolo <ayolo@troll>
2007-09-22 15:33:51 +0000

    on fait un audit vite fait

--- temp/repair_users.php
+++ temp/repair_users.php
@@ -5,19 +5,14 @@ $topdir="../";
 require_once($topdir. "include/site.inc.php");
 $site = new site();
 
-header("Content-Type: text/html; charset=utf-8");
+$site->start_page("accueil","Réparation de la base utilisateur");
 
 /**
  * Script de vérification des catégories des doublons utilisateurs
  */
 
-echo "<h1>AE2/USER: Auto repair</h1>\n";
 
 $sql = new requete($site->db,"SELECT * FROM `utilisateurs`");
-
-echo "<h2>Vérification des potentiels doublons</h2>\n";
-
-echo "<ul>\n";
 
 while ( $row = $sql->get_row() )
 {
@@ -29,20 +24,41 @@ while ( $row = $sql->get_row() )
   else
   {
     if(!isset($names[$row['nom_utl']][$row['prenom_utl']]))
-      $names[$row['nom_utl']][$row['prenom_utl']]=1;
+    {
+      $names[$row['nom_utl']][$row['prenom_utl']]=array();
+      $names[$row['nom_utl']][$row['prenom_utl']][]=$row['id_utilisateur'];
+    }
     else
-      $names[$row['nom_utl']][$row['prenom_utl']]++;
+      $names[$row['nom_utl']][$row['prenom_utl']][]=$row['id_utilisateur'];
   }
 }
 
+$cts = new contents("Gestion des doublons");
 foreach($names as $name => $firstnames)
 {
   foreach($firstnames as $firstname => $num)
-    if($num>1)
-      echo "<li>".$name." ".$firstname." : ".$num."</li>\n";
+    if(count($num)>1)
+    {
+      if(count($num)==2)
+      {
+        $frm = new form("discard","repair_users.php",true,"POST",$name." ".$firstname);
+        $frm->add_info("Faut il merger les fiches suivantes :<br />");
+        $frm->add_info("<ul>");
+        $frm->add_info("<li><a href='../user.php?id_utilisateur=".$num[0].">".$num[0]."</a></li>");
+        $frm->add_info("<li><a href='../user.php?id_utilisateur=".$num[1].">".$num[1]."</a></li>");
+        $frm->add_info("</ul>");
+        $frm->add_info("");
+        $frm->add_hidden("action","merge");
+        $frm->add_hidden("id_1",$num[0]);
+        $frm->add_hidden("id_2",$num[1]);
+        $frm->add_submit("save","Merger");
+        $cts->add($frm,true);
+
+      }
+      else
+      {
+      }
+    }
 }
-
-echo "</ul>\n";
-
 
 ?>
