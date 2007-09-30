d408de81eff01e2b2d91ff84e8610e053c24992d
vazlin <vazlin@troll>
2007-09-30 08:45:52 +0000

    debug

--- temp/existing_comments.php
+++ temp/existing_comments.php
@@ -11,10 +11,12 @@ require_once($topdir. "trombi/include/entities/commentaire.inc.php");
 $site = new site();
 $site->start_page("none", "Commentaires existants");
 
+$cts = new contents("Commentaires existant");
+
 $req = new requete($site->db,"SELECT * FROM trombi_commentaire WHERE 1");
 
 $tbl = new sqltable("listcomment",
-                      "Commentaires existants", $req, "",
+                      "Commentaires", $req, "",
                       "id_commentaire",
                       array("commentaire"=>"Commentaire"),
                       array(), array(), array( )
