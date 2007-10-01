513985a8b86b9145de55f4de3104ed78d47a6f2f
pedrov <pedrov@troll>
2007-10-01 17:12:18 +0000

    entitization uvs

--- uvs/uvs.php
+++ uvs/uvs.php
@@ -30,7 +30,7 @@
 $topdir = "../";
 
 include($topdir. "include/site.inc.php");
-require_once($topdir . "include/entities/uv.php");
+require_once($topdir . "include/entities/uv.inc.php");
 
 
 $site = new site();
