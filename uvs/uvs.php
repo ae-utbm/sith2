e075402ebe1996f57d57b024098b6f6bfa853c34
pedrov <pedrov@troll>
2007-10-04 09:45:47 +0000

    Pas 2 commentaires pour la meme uv

--- uvs/uvs.php
+++ uvs/uvs.php
@@ -217,6 +217,7 @@ if (isset($_REQUEST['id_uv']) || (isset($_REQUEST['code_uv'])))
   if (count($uv->comments) > 0)
     {
       $commented = false;
+
       foreach ($uv->comments as $comm)
 	{
 	  if ($site->user->id == $comm->id_commentateur)
@@ -234,7 +235,7 @@ if (isset($_REQUEST['id_uv']) || (isset($_REQUEST['code_uv'])))
 
 
   /* l'utilisateur est étudiant UTBM */
-  if ($site->user->is_in_group_id(10004) && $commented)
+  if (($site->user->is_in_group_id(10004)) && ($commented))
     {
       $commcts = new contents("Commentaires sur les UVs");
       $commform = new form('commform',
