a416f25439e6b82ee9eadc0dee07a0efe428b047
Feu <feu@troll>
2007-10-08 20:45:26 +0000

    wip

--- matmatronch/index2.php
+++ matmatronch/index2.php
@@ -30,9 +30,15 @@ $is_admin = ( $site->user->is_in_group("gestion_ae") || $site->user->is_asso_rol
 
 $info = new contents("Mat'Matronch");
 $info->add_paragraph("Le Mat'Matronch donne enfin un nom a un visage, le numero de portable du binome fantome, l'adresse de ce confrere que l'on recherche tant depuis cette fameuse soiree ...");
-$info->add_paragraph("<a href=\"".$topdir."e-boutic/?id_typeprod=25\">Acheter la version papier</a>");
-$info->add_paragraph("Extension Firefox : <a href=\"".$topdir."matmatronch/matmatronch.src\">ici</a>");
-$info->add_paragraph("La version mobile : <a href=\"http://ae.utbm.fr/i/\">http://ae.utbm.fr/i/</a> <a href=\"".$topdir."iinfo.php\">Informations</a>","center");
+
+$list = new itemlist("Voir aussi");
+
+$list->add("<a href=\"".$topdir."e-boutic/?id_typeprod=25\">Acheter la version papier</a>");
+$list->add("<a href=\"".$topdir."matmatronch/matmatronch.src\">Extension Firefox</a>");
+$list->add("La version mobile : <a href=\"http://ae.utbm.fr/i/\">http://ae.utbm.fr/i/</a> <a href=\"".$topdir."iinfo.php\">Informations</a>");
+
+$info->add($list,true);
+
 $site->add_box("mmtinfo",$info);
 
 $site->set_side_boxes("right",array("mmtinfo"),"mmt2_right");
