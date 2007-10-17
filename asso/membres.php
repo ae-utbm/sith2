80b83c51a4ae3ae76c83175d26b1f44605384577
Feu <feu@troll>
2007-10-17 12:14:25 +0000

    selfenroll sur la page de la liste des membres

--- asso/membres.php
+++ asso/membres.php
@@ -327,6 +327,9 @@ elseif ( $_REQUEST["view"] == "anciens" )
 else
 {
 
+  if ( !is_null($asso->id_parent) && (!$site->user->is_valid() || !$asso->is_member($site->user->id)) )
+    $cts->add_paragraph("Inscrivez vous pour recevoir les nouvelles de ".$asso->nom." par e-mail et participer aux discussions, c'est simple et rapide : <a href=\"../asso.php?id_asso=".$asso->id."&amp;action=selfenroll\">cliquez ici</a>");
+
 	$req = new requete($site->db,
 		"SELECT `utilisateurs`.`id_utilisateur`, " .
 		"CONCAT(`utilisateurs`.`prenom_utl`,' ',`utilisateurs`.`nom_utl`) as `nom_utilisateur`, " .
