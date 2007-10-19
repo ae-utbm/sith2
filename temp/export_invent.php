6a5bd7505295833b1746d731282b430ec7afdecf
Ayolo <ayolo@troll>
2007-10-19 15:57:22 +0000

    je test un truc

--- temp/export_invent.php
+++ temp/export_invent.php
@@ -24,7 +24,7 @@ else
 $emp = new emprunt($site->db,$site->dbrw);
 $obj->add_emprunt('.$id_utl.','.$id_asso.','.$ext.', '.mktime(0,0,0,9,22,2007).','.mktime(0,0,0,9,23,2007).');';
 $req2 = new requete($site->db,"SELECT `id_objet` FROM `inv_emprunt_objet` WHERE `id_emprunt`='".$id_emp."'");
-while($list($id)=$req2->get_row())
+while(list($id)=$req2->get_row())
 echo '$emp->add_object('.$id.');
 $emp->retrait()
 $emp->full_back();
