5508dbb052e0b7863d5ebe020751017ffa79ce20
pedrov <pedrov@troll>
2007-10-25 19:02:36 +0000

    pourissage rss forum caracteres a la con

--- include/rssforum.inc.php
+++ include/rssforum.inc.php
@@ -86,9 +86,9 @@ class rssfeedforum extends rssfeed
 	echo "\t<link>".$this->pubUrl."?id_message=".$row["id_message"]."#msg".$row['id_message']."</link>\n";
 	
 
-	$row['contenu_message'] = htmlspecialchars($row['contenu_message'],
-						   ENT_COMPAT,
-						   "UTF-8");
+	$row['contenu_message'] = htmlentities($row['contenu_message'],
+					       ENT_COMPAT,
+					       "UTF-8");
 
 	if ($row['syntaxengine_message'] == 'doku')
 	  $content = doku2xhtml($row['contenu_message']);
