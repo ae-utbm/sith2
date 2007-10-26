0193cf108cbc6aeaf39308b9b4f7122d3ff1f475
Ayolo <ayolo@troll>
2007-10-26 09:19:57 +0000

    bleh

---
+++ temp/update_user_utbm.php
@@ -0,0 +1,60 @@
+<?php
+// ce fichier permet de mettre à jour les fiches matmatronch
+// tout d'abord, tout ceux qui ne sont pas dans le xml sont
+// ancien étudiant s'ils sont étudiants
+//
+
+$topdir = "../";
+include($topdir. "include/site.inc.php");
+
+
+$site = new site ();
+
+$cts = new contents();
+
+if( isset($_REQUEST["action"]) )
+{
+  if ( $_REQUEST["action"]=="upload" )
+    if ( is_uploaded_file($_FILES['xmlfile']['tmp_name']) )
+      $src=$_FILES['xmlfile']['tmp_name'];
+
+  if ( $_REQUEST["action"]=="frompath" )
+    if (file_exists($_REQUEST["path"]))
+      $src=$_REQUEST["path"];
+
+
+  if (isset($src))
+  {
+    $fp = fopen($src, "r");
+    if (!$fp)
+      die("Impossible d'ouvrir le fichier XML");
+    else
+    {
+      fclose($fp);
+      $xml = simplexml_load_file($src);
+      recursivite($xml);
+      var_dump($xml);
+      exit();
+    }
+  }
+}
+
+
+
+$frm = new form("upload","update_user_utbm.php"."#upload",true,"POST","Changer mes photos persos");
+$frm->add_hidden("action","upload");
+$frm->add_file_field ( "xmlfile", "Fichier" );
+$frm->add_submit("save","UPLOAD");
+
+$cts->add($frm,true);
+
+$frm = new form("frompath","update_user_utbm.php"."#frompath",true,"POST","Changer mes photos persos");
+$frm->add_hidden("action","frompath");
+$frm->add_text_field("path","Path : (ex : /tmp/truc.xml)");
+$frm->add_submit("save","Executer");
+$cts->add($frm,true);
+
+$site->add_contents($cts);
+$site->end_page();
+
+?>
