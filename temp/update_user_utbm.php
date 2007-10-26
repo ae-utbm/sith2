e1ab04e6a8e9249dc356a14d69f731f1802c4486
pedrov <pedrov@troll>
2007-10-26 10:21:56 +0000

    comparaisons infos

--- temp/update_user_utbm.php
+++ temp/update_user_utbm.php
@@ -42,7 +42,7 @@ if( isset($_REQUEST["action"]) )
         if($user->load_by_email($student->email))
         {
 
-	  $user->load_all_extras();
+	  $user->load_all_extra();
 
           $cts = new contents($user->prenom." ".$user->nom);
           $cts->add_paragraph("<a href='".$topdir."user.php?id_utilisateur=".
