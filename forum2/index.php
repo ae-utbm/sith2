602a3ec2f743a2b86a3b734f5da2042d45b14d35
gliss <gliss@troll>
2007-10-29 21:33:21 +0000

    comme ca j aurai commité quelque chose ce soir

--- forum2/index.php
+++ forum2/index.php
@@ -427,9 +427,23 @@ if ( $sujet->is_valid() )
     $frm->add_dokuwiki_toolbar('text');
     $frm->add_text_area("text", "Texte du message : ",$message->contenu,80,20);
     $frm->add_submit("submit", "Modifier");
+    $frm->puts("<div class=\"formrow\"><div class=\"formlabel\"></div><div class=\"formfield\"><input type=\"button\" id=\"preview\" name=\"preview\" value=\"Prévisualiser\" class=\"isubmit\" onClick=\"javascript:make_preview();\" /></div></div>\n");
     $frm->allow_only_one_usage();
-
+		
+    $cts->add_paragraph("<script language=\"javascript\">
+      function make_preview()
+      {
+        title = document.frmedit.title.value;
+        content = document.frmedit.text.value;
+        user = ".$site->user->id.";
+        syntaxengine = document.frmedit.synengine.value;
+        
+        openInContents('msg_preview', './index.php', 'get_preview&title='+title+'&content='+content+'&user='+user+'&syntaxengine='+syntaxengine);
+      }
+      </script>\n");
+    
     $cts->add($frm);
+    $cts->puts("<div id=\"msg_preview\"></div>");
   
     /**@todo*/
     $site->add_contents($cts);
