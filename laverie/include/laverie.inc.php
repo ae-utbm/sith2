5881b7d8260af02834a065be013e694527a25a24
BenC <benc@troll>
2007-10-30 00:45:41 +0000

    Broyons tout gaiement

--- laverie/include/laverie.inc.php
+++ laverie/include/laverie.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-/* Copyright 2006
+/* Copyright 2007
  * - Benjamin Collet < bcollet at oxynux dot org >
  *
  * Ce fichier fait partie du site de l'Association des Étudiants de
@@ -44,25 +44,24 @@ class sitelaverie extends site
 		parent::start_page("services",$title);
 	}
 	
-	function user_is_admin()
+	function is_user_admin()
 	{
 		$this->gestion_laverie = array();
 		
 		if ( $this->user->is_in_group("gestion_ae") OR $this->user->is_in_group("gestion_machines" ) )
-		  $this->is_admin = true;
+		  $this->user_is_admin = true;
 	}
 
-	function set_admin_mode()
+	function get_rights($public=false)
 	{
-		if ( !isset($this->is_admin) )
-			$this->user_is_admin();
-		
-		$admcts = new contents("Laverie");
-		
-		$admcts->add_paragraph("<a href=\"index.php\">Laverie</a>");
-		
-		if ( $this->is_admin )
+		$site->is_user_admin()
+
+		if ( $site->user_is_admin )
 		{
+			$admcts = new contents("Laverie");
+		
+			$admcts->add_paragraph("<a href=\"index.php\">Laverie</a>");
+		
 			$lst_taches = new itemlist("Tâches usuelles","boxlist");
 			$lst_taches->add("<a href=\"index.php?view=reserver\">Réserver un créneau</a>");
 			$lst_taches->add("<a href=\"index.php?view=vente\">Retirer des jeton</a>");
@@ -70,7 +69,7 @@ class sitelaverie extends site
 			$lst_admin = new itemlist("Administration","boxlist");
 			$lst_admin->add("<a href=\"index.php?view=machines\">Gestion des machines</a>");
 			$lst_admin->add("<a href=\"index.php?view=plannings\">Gestion des plannings</a>");
-			$lst_admin->add("<a href=\"index.php?view=inventaire\">Inventaire des jetons</a>");
+			$lst_admin->add("<a href=\"inventaire.php\">Inventaire des jetons</a>");
 			$lst_util = new itemlist("Utilisateurs","boxlist");
 			$lst_util->add("<a href=\"index.php?view=mauvais\">Mauvais clients</a>");
 			$lst_util->add("<a href=\"index.php?view=recharger\">Rechargement</a>");
@@ -80,9 +79,39 @@ class sitelaverie extends site
 			$admcts->add($lst_taches,true, true, "tachesbox", "boxlist", true, true);
 			$admcts->add($lst_admin,true, true, "adminbox", "boxlist", true, true);
 			$admcts->add($lst_util,true, true, "utilbox", "boxlist", true, true);
-		}
 		
-		$this->add_box("laverie",$admcts);	
+			$this->add_box("laverie",$admcts);
+		}
+		elseif ( $site->user->is_in_group("blacklist_machines") )
+		{
+		  $cts->add_paragraph("Vous n'avez pas le droit d'utiliser les machines de l'AE, car vous n'avez pas respecté les conditions d'utilisation.");
+			/* Remplacer cette "chose" par une fonction dans la classe jeton */
+		  $sql = new requete($site->db, "SELECT 
+				    `mc_jeton_utilisateur`.`id_jeton`
+				    , `mc_jeton`.`nom_jeton`
+				    , DATEDIFF(CURDATE(), `mc_jeton_utilisateur`.`prise_jeton`) AS `duree` 
+				    FROM `mc_jeton` 
+				    INNER JOIN `mc_jeton_utilisateur` ON `mc_jeton`.`id_jeton` = `mc_jeton_utilisateur`.`id_jeton` 
+				    WHERE `id_utilisateur` = ".$site->user->id." AND mc_jeton_utilisateur.retour_jeton IS NULL");
+
+			if ($sql->lines >= 1)
+			{
+				$jetons = array();
+																				  
+			  while ($row = $sql->get_row())
+			    array_push($jetons,"Jeton ".$row['nom_jeton'].", emprunté depuis ".$row['duree']." jours.");
+																										  
+			  $list = new itemlist("Vous devez rendre les jetons suivants :",false,$jetons);
+
+				$cts->add($list,true); 
+			}
+			$site->page_end();
+			exit();
+		}
+		elseif ( !$public )
+		{
+			$site->error_forbidden("none","group",30);
+		}
 	}
 }
 
