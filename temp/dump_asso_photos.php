585d76938e2798edfd128edf7b8da91b965b35bf
Ayolo <ayolo@troll>
2007-11-10 13:48:22 +0000

    bleh

--- temp/dump_asso_photos.php
+++ temp/dump_asso_photos.php
@@ -12,28 +12,81 @@ if( !isset($_REQUEST["id_asso"]) || empty($_REQUEST["id_asso"]) )
 require_once($topdir. "include/site.inc.php");
 require_once($topdir. "sas2/include/photo.inc.php");
 require_once($topdir. "include/entities/asso.inc.php");
+require_once($topdir. "include/cts/sas.inc.php");
 $site = new site ();
-
-$asso = new asso($site->db,$site->dbrw);
-$asso->load_by_id($_REQUEST["id_asso"]);
-if ( $asso->id < 1 )
-{
-  exit();
-}
 
 $user = new utilisateur($site->db,$site->dbrw);
 /* le lion, c'est notre utilisater de référence */
 $user->load_by_id(3538);
 
+function get_catphoto($id_catph)
+{
+  global $user;
+  $photos = array();
+  $cat = new catphoto($site->db,$site->dbrw);
+  $cat->load_by_id($id_catph);
+  if ( !$cat->is_valid() )
+    return $photos;
 
-$req = new requete($site->db,"SELECT `id_photo` FROM `sas_photos` WHERE (`id_asso_photographe`='".$asso->id."' OR `meta_id_asso_ph`='".$asso->id."') AND `incomplet`='0'");
+  /* on récupère les photos de la catégorie */
+  $sqlph = $cat->get_photos ( $cat->id, $site->user, $grps, "sas_photos.id_photo");
+  while ( list($id) = $sqlph->get_row() )
+    $photos[] = $id;
+
+  /* on récupère les sous catégories */
+  $cats = $cat->get_all_categories($user);
+  foreach ( $cats as $row )
+  {
+    $subcatph = get_catphoto($row["id_catph"]);
+    if(!empty($subcatph))
+    {
+      foreach($subcatph as $id)
+      {
+        if(!in_array($id,$photos)
+          $photos[] = $id;
+      }
+    }
+  }
+  return $photos;
+}
+
+if(isset($_REQUEST["id_asso"]))
+{
+  $asso = new asso($site->db,$site->dbrw);
+  $asso->load_by_id($_REQUEST["id_asso"]);
+  if ( $asso->id < 1 )
+    exit();
+
+  $photos = array();
+  $req = new requete($site->db,"SELECT `id_photo` FROM `sas_photos` WHERE (`id_asso_photographe`='".$asso->id."' OR `meta_id_asso_ph`='".$asso->id."') AND `incomplet`='0'");
+  while ( list($id) = $req->get_row() )
+  {
+    $photos[]=$id;
+  }
+}
+elseif(isset($_REQUEST["id_catph"]))
+{
+  $cat = new catphoto($site->db,$site->dbrw);
+  $cat->load_by_id($_REQUEST["id_catph"]);
+  if ( !$cat->is_valid() )
+    exit();
+
+  $photos = array();
+  $photos = get_catphoto($_REQUEST["id_catph"]);
+}
+else
+  exit();
 
 $photo = new photo($site->db,$site->dbrw);
 
 $bouh = false;
 
 exec("/bin/mkdir /tmp/".$asso->nom_unix);
-while ( list($id) = $req->get_row() )
+
+if(empty($photos))
+  exit();
+
+foreach($photos as $id)
 {
   $photo->load_by_id($id);
   if ($photo->is_valid() && $photo->type_media != MEDIA_VIDEOFLV)
@@ -49,7 +102,7 @@ while ( list($id) = $req->get_row() )
 if($bouh)
 {
   exec("/bin/tar czf /tmp/".$asso->nom_unix.".tar.gz /tmp/".$asso->nom_unix);
-  exec("/bin/mv /tmp/".$asso->nom_unix.".tar.gz /var/www/ae/accounts/equipeinfo/");
+  exec("/bin/mv /tmp/".$asso->nom_unix.".tar.gz /var/www/ae/www/equipeinfo/");
   exec("/bin/rm -Rf /tmp/".$asso->nom_unix);
 }
 
