95d76b97dd9de10f9d8cf905fd46df85a3679038
gliss <gliss@troll>
2007-11-15 23:28:28 +0000

    fix

--- include/entities/utilisateur.inc.php
+++ include/entities/utilisateur.inc.php
@@ -1873,7 +1873,7 @@ L'équipe info AE";
                   array("parrain","user.php?view=parrain&id_utilisateur=".$this->id, "Parrains"),
                   array("assos","user.php?view=assos&id_utilisateur=".$this->id, "Associations"),
                   array("photos","user/photos.php?id_utilisateur=".$this->id, "Photos"),
-		              array("pedagogie","user.php?view=edt&id_utilisateur=".$this->id, "Pédagogie") );
+		              array("pedagogie","user.php?view=pedagogie&id_utilisateur=".$this->id, "Pédagogie") );
 
     if (  $this->id==$user->id || $user->is_in_group("gestion_ae") )
     {
