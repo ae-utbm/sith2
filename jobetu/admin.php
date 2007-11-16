44137c2d6d2f1b36c1d34a6c787444e986bd6a35
gliss <gliss@troll>
2007-11-16 02:43:04 +0000

    fixip

--- jobetu/admin.php
+++ jobetu/admin.php
@@ -168,12 +168,14 @@ if(isset($_REQUEST['action']) && $_REQUEST['action'] == "delete")
       {
         $annonce = new annonce($site->db, $site->dbrw);
         $annonce->load_by_id($tmp);
-       if( $annonce->destroy() )
+        print_r($annonce);
+        echo $annonce->destroy() ."\n";
+/*       if( $annonce->destroy() )
         $msg = "Opération effectuée";
        else
         $msg = "La suppression n'a pu être réalisée. Peut-être un étudiant a-t-il déjà été sélectionnée. Dans ce cas veuillez prendre contact avec les différentes personnes pour clôre l'annonce";
       }
-      $header->add(new itemlist(false, false, array( $msg )));
+      $header->add(new itemlist(false, false, array( $msg ))); */
     }
     else //on demande confirmation (boolay proofing)
     {
