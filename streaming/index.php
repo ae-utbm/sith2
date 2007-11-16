ee286455bd906077c3be83b37bf19b5dc582a969
Ayolo <ayolo@troll>
2007-11-16 23:47:21 +0000

    bleh

--- streaming/index.php
+++ streaming/index.php
@@ -58,7 +58,7 @@ if(isset($event["bandeau"]))
   $cts->add_paragraph(" ");
 }
 $cts->add_paragraph($event["intro"]);
-$cts->add_paragraph("La cérémonie a lieu le ".$event["date"]." de ".$event["h-debut"]."h à ".$event["h-fin"]."h ".$event["lieu"].".");
+$cts->add_paragraph("<b>La cérémonie a lieu le ".$event["date"]." de ".$event["h-debut"]."h à ".$event["h-fin"]."h ".$event["lieu"]."</b>.");
 $site->add_contents($cts);
 
 $cts = new contents("Regarder la cérémonie en direct");
