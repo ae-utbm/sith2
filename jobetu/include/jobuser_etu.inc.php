6d75af3226cfb26a43b75d02d5de04816fe8c499
gliss <gliss@troll>
2007-11-16 00:43:05 +0000

    fixip

--- jobetu/include/jobuser_etu.inc.php
+++ jobetu/include/jobuser_etu.inc.php
@@ -154,7 +154,9 @@ class jobuser_etu extends utilisateur
 			$sql = new insert($this->dbrw, "job_prefs", array("id_utilisateur" => $this->id, "pub_cv" => $new_pub_cv, "mail_prefs" => $new_mail_prefs));
 		else
 			$sql = new update($this->dbrw, "job_prefs", array("pub_cv" => $new_pub_cv, "mail_prefs" => $new_mail_prefs), array("id_utilisateur" => $this->id));
-		
+			
+		echo $new_pub_cv."\n". $new_mail_prefs."\n";
+		print_r($sql);
 		$this->load_prefs();
 		
 		if($sql)
