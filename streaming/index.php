737f7b4add6f76af81f480d3d0b7cc46e7f620f2
Ayolo <ayolo@troll>
2007-11-16 22:38:26 +0000

    bleh

--- streaming/index.php
+++ streaming/index.php
@@ -28,11 +28,11 @@ require_once($topdir. "include/site.inc.php");
 /* on défini des variables simples et efficaces */
 $rdd=array("title"=>"remise des diplômes",
            "intro"=>"Cette année, afin de faire participer les personnes non-conviées à la cérémonie de la Remise des Diplômes de l'<a href='http://www.utbm.fr'>Université de Technologie de Belfort-Montbéliard</a>, nous avons mis en place un service expérimental de diffusion de la cérémonie en direct.",
-           "date"=>"samedi 17 novembre 2006",
+           "date"=>"samedi 17 novembre 2007",
            "h-debut"=>"10",
            "h-fin"=>"12",
            "lieu"=>"au centre des expositions AirExpo d'Andelnans (entre Belfort et Sevenans)",
-           "remerciements"=>"Grâce au travail d'UTBM-Productions pendant la cérémonie, nous vous offrons en direct les images montées afin de profiter, chez vous, de la cérémonie."
+           "remerciements"=>"Grâce au travail d'UTBM-Productions pendant la cérémonie, nous vous offrons en direct les images montées afin de profiter, chez vous, de la cérémonie.<br /> <img src=\"utprod.png\" alt=\"UTBM Production\" title=\"UTBM Production\" />"
           );
 
 $event=$rdd;
@@ -57,6 +57,10 @@ $cts->add_paragraph("Pour profiter au mieux et de manière plus fiable de cette 
 $cts->add_paragraph("Pour les utilisateurs de <b>VideoLan</b>, lancez VLC puis dans le menu <i>Fichier</i> choisissez <i>Ouvrir un flux réseau ...</i>. Sélectionnez <i>HTTP/HTTPS/FTP/MMS</i> et rentrez l'URL <tt>mmsh://ae.utbm.fr:8080/</tt>");
 $cts->add_paragraph("Pour les utilisateurs de <b>Windows Media Player</b>, lancez Windows Media Player puis dans le menu <i>Fichier</i> choisissez <i>Ouvrir un flux video ...</i> et rentrez l'adresse suivante : <tt>http://ae.utbm.fr:8080/</tt>");
 $site->add_contents($cts);
+
+$cts = new contents("Merci à");
+$cts->add_paragraph($event["remerciements"]);
+$site->add_contents($cts)
 
 $cts = new contents("Propulsé par ...");
 $cts->add_paragraph("<img src=\"linux.png\" alt=\"GNU/Linux\" title=\"GNU/Linux\" /> <img src=\"vlc.jpg\" alt=\"VideoLan\" title=\"VideoLan\" /> <img src=\"debian.png\" alt=\"Debian\" title=\"Debian\" />");
