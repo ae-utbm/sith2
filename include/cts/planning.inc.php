045444f9955e9a8099dc904c3ac024aefe8fffe4
Feu <feu@troll>
2007-11-21 15:34:01 +0000

    fix

--- include/cts/planning.inc.php
+++ include/cts/planning.inc.php
@@ -138,7 +138,7 @@ class weekplanning extends stdcontents
 				    $this->buffer .= "<div style=\"height:".($st-$last)."px; overflow:hidden;\">&nbsp;</div>\n";
 				
 				  $this->buffer .= "<div style=\"border: 1px solid black; padding:1px; height:".($ln-4)."px; overflow:hidden;\"><a href=\"".$infopage.$idf."=".$row[2]."\"><i>".date("H:i",$row[0])."</i> ".$row[3]."</a></div>\n";
-				  $last=$st+$ln+1;
+				  $last=$st+$ln;
 			  }
 		  }
 			
