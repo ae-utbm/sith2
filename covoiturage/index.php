8c073354284a88d359abfb083b8c860674f1c59e
pedrov <pedrov@troll>
2007-11-21 23:50:18 +0000

    pas de trajets sur la page d'accueil

--- covoiturage/index.php
+++ covoiturage/index.php
@@ -44,69 +44,6 @@ $accueil = new contents("Accueil - Covoiturage",
 			"Bienvenue sur le système de covoiturage de l'AE.<br/><br/>");
 
 
-/* 5 derniers trajets TRJ_PCT proposés */
-
-$sql = new requete($site->db, "SELECT 
-                                      `id_trajet`
-                               FROM
-                                      `cv_trajet`
-                               INNER JOIN
-                                      `cv_trajet_date`
-                               USING (`id_trajet`)
-                               WHERE 
-                                      `trajet_date` >= DATE_FORMAT(NOW(), '%Y-%m-%d')
-                               AND
-                                       `type_trajet` = 0
-                               GROUP BY 
-                                       `id_trajet`
-                               ORDER BY
-                                       `id_trajet` DESC
-                               LIMIT 5");
-
-if ($sql->lines > 0)
-{
-  
-  $trajet = new trajet($site->db);
-  $usrtrj = new utilisateur($site->db);
-
-  while ($res = $sql->get_row())
-    {
-      $trajet->load_by_id($res['id_trajet']);
-      if (!$trajet->has_expired())
-	{
-	  if (!$firsttrj)
-	    {
-
-	      $accueil->add_title(2, "Derniers trajets ponctuels proposés");
-
-	      $accueil->add_paragraph("Dans la liste ci-dessous, cliquez sur une date spécifique pour avoir ".
-				      "le détail du trajet, et éventuellement faire une demande de covoiturage");
-	      $firsttrj = true;
-	    }
-
-	  $usrtrj->load_by_id($trajet->id_utilisateur);
-	  $trj = "Trajet ". $trajet->ville_depart->nom . " / " . $trajet->ville_arrivee->nom . 
-	    " proposé par ". $usrtrj->get_html_link();
-	  
-	  $accueil->add_title(3, $trj);
-
-	  $dates = array();
-
-	  foreach ($trajet->dates as $date)
-	    {
-	      if (strtotime($date) > time())
-		$dates[] = "<a href=\"./details.php?id_trajet=".$trajet->id
-		  ."&amp;date=".$date."\">Le " .HumanReadableDate($date, "", false, true) . "</a>";
-	    }
-	  if (count($dates))
-	    $accueil->add(new itemlist(false, false, $dates));
-	  
-	}  
-    }
-}
-
-
-
 /* "Mes trajets proposés" */
 
 $sql = new requete($site->db, "SELECT 
