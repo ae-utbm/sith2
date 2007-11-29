2cba7a7d4253740e2e565525f7b7e0c3e1e00e71
Ayolo <ayolo@troll>
2007-11-29 16:44:04 +0000

    diff

---
+++ temp/test_diff.php
@@ -0,0 +1,34 @@
+<?php
+
+$topdir = "../";
+
+require_once($topdir. "include/site.inc.php");
+require_once($topdir. "include/entities/diff.inc.php");
+
+$site = new site ();
+
+$site->start_page("services","AE - Recherche et Développement");
+
+$intro = new contents("Edition liste au format pdf");
+
+
+$_old = "Feu est un génie, mais faut pa le dire
+sinon il va se faire exploiter
+par les vilains luttins de la foret";
+$_new = "Feu est un génie, mais faut pas le dire
+sinon il va se faire exploiter
+comme ça devrait être interdit
+par les vilains luttins de la foret";
+
+$df  = new Diff($_old,$_new);
+$tdf = new TableDiffFormatter();
+
+$intro->add_paragraph("<table class=\"diff\">\n".$tdf->format($df)."</table>");
+
+
+$site->add_contents($intro);
+$site->add_contents($cts);
+$site->end_page();
+
+
+?>
