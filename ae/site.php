921b5db46fc5506fe3eb655b8c06d28c765e9cb1
Ayolo <ayolo@troll>
2007-12-05 06:58:25 +0000

    bleh

--- ae/site.php
+++ ae/site.php
@@ -37,7 +37,7 @@ require_once($topdir. "include/cts/sqltable.inc.php");
 $site = new site ();
 
 if (!$site->user->is_in_group ("moderateur_site"))
-	error_403();
+  error_403();
 
 /* else */
 $site->start_page ("none", "Gestion du site");
@@ -45,28 +45,28 @@ $site->start_page ("none", "Gestion du site");
 /* objets graphiques de base */
 
 $cts_base = new contents ("Gestion du site","<p>Cette page vous permet "
-			  ."de gérer le site de l'AE. Ceci implique la ".
-			  "modification des textes dans les boites, etc".
-			  "...</p>");
+        ."de gérer le site de l'AE. Ceci implique la ".
+        "modification des textes dans les boites, etc".
+        "...</p>");
 
 $site->add_contents ($cts_base);
 
 
 $req = new requete ($site->db,
-		    "SELECT * FROM `site_boites` WHERE `nom_boite` != 'news_help'");
+        "SELECT * FROM `site_boites` WHERE `nom_boite` != 'news_help'");
 
 /* formulaire */
 if (!isset($_REQUEST['action']))
 {
   $cts_mod_box = new sqltable ("frm_mod_box",
-			       "Modification des boites",
-			       $req,
-			       "site.php",
-			       "nom_boite",
-			       array ("nom_boite" => "nom de la boite",
-				      "description_boite" => "description"),
-			       array("edit" => "Editer la boite"),
-			       array("edit" => "Editer la / les boite(s)"));
+             "Modification des boites",
+             $req,
+             "site.php",
+             "nom_boite",
+             array ("nom_boite" => "nom de la boite",
+              "description_boite" => "description"),
+             array("edit" => "Editer la boite"),
+             array("edit" => "Editer la / les boite(s)"));
   $site->add_contents ($cts_mod_box);
 }
 
@@ -76,19 +76,19 @@ if (!isset($_REQUEST['action']))
 if (isset($_REQUEST['frm_edit_box_ct']))
 {
   foreach ($_REQUEST['frm_edit_box_ct'] as $nom_boite => $contenu_boite)
-    {
-      $req = new update ($site->dbrw,
-			 "site_boites",
-			 array ("contenu_boite" => $contenu_boite),
-			 array ("nom_boite" => $nom_boite));
-      if ($req != false)
-	$site->add_contents(new contents("Modification boite ". $nom_boite,
-					 "La boite a &eacute;t&eacute; modifi&eacute;e avec succ&egrave;s"));
-      else
-	$site->add_contents(new error("Erreur Modification",
-				      "Une erreur est survenue lors de la modification "
-				      ."de la boite " . $nom_boite));
-    }
+  {
+    $req = new update ($site->dbrw,
+       "site_boites",
+       array ("contenu_boite" => $contenu_boite),
+       array ("nom_boite" => $nom_boite));
+    if ($req != false)
+      $site->add_contents(new contents("Modification boite ". $nom_boite,
+           "La boite a &eacute;t&eacute; modifi&eacute;e avec succ&egrave;s"));
+    else
+      $site->add_contents(new error("Erreur Modification",
+              "Une erreur est survenue lors de la modification "
+              ."de la boite " . $nom_boite));
+  }
 }
 
 
@@ -97,59 +97,60 @@ if (isset($_REQUEST['frm_edit_box_ct']))
 if (isset($_REQUEST['action']))
 {
   $frm_edit_box = new form ("frm_edit_box",
-			    "site.php",
-			    true,
-			    "post",
-			    "Edition des boites");
+          "site.php",
+          true,
+          "post",
+          "Edition des boites");
 
   /* modification d'une seule boite */
   if ((isset($_REQUEST['nom_boite'])) &&
       (!isset($_REQUEST['nom_boites'])))
-    {
-      $req = new requete ($site->db,
-			  "SELECT `contenu_boite`, `nom_boite` FROM `site_boites` ".
-			  "WHERE `nom_boite` = '".
-			  mysql_real_escape_string($_REQUEST['nom_boite'])
-			  ."'");
-      $ct = $req->get_row ();
-      /* un seul resultat */
-      $frm_edit_box->add_text_area ("frm_edit_box_ct[".$ct['nom_boite']."]",
-				    "Contenu de la boite " .
-				    $ct['nom_boite'],
-				    $ct['contenu_boite']);
-    }
+  {
+    if( $_REQUEST['nom_boite']= "" )
+      $frm_edit_box->add_dokuwiki_toolbar("frm_edit_box_ct[".$ct['nom_boite']."]");
+    $req = new requete ($site->db,
+      "SELECT `contenu_boite`, `nom_boite` FROM `site_boites` ".
+      "WHERE `nom_boite` = '".
+      mysql_real_escape_string($_REQUEST['nom_boite'])
+      ."'");
+    $ct = $req->get_row ();
+    /* un seul resultat */
+    $frm_edit_box->add_text_area ("frm_edit_box_ct[".$ct['nom_boite']."]",
+        "Contenu de la boite " .
+        $ct['nom_boite'],
+        $ct['contenu_boite']);
+  }
 
   /* modification de plusieurs boites */
   if (isset($_REQUEST['nom_boites']))
+  {
+    /* mysql protection (meme si envoye en post) */
+    for ($i = 0; $i < count ($_REQUEST['nom_boites']); $i++)
+      $boxes_t[$i] = "'".
+      mysql_real_escape_string($_REQUEST['nom_boites'][$i]).
+      "'";
+
+    $boxes = implode(",", $boxes_t);
+
+
+    $req = new requete ($site->db,
+      "SELECT `contenu_boite`, `nom_boite` ".
+      "FROM `site_boites` ".
+      "WHERE `nom_boite` IN (".
+      $boxes .")");
+    for ($i = 0; $i < $req->lines; $i++)
     {
-      /* mysql protection (meme si envoye en post) */
-      for ($i = 0; $i < count ($_REQUEST['nom_boites']); $i++)
-	$boxes_t[$i] = "'".
-	  mysql_real_escape_string($_REQUEST['nom_boites'][$i]).
-	  "'";
-
-      $boxes = implode(",", $boxes_t);
-
-
-      $req = new requete ($site->db,
-			  "SELECT `contenu_boite`, `nom_boite` ".
-			  "FROM `site_boites` ".
-			  "WHERE `nom_boite` IN (".
-			  $boxes .")");
-      for ($i = 0; $i < $req->lines; $i++)
-	{
-	  $ct[] = $req->get_row ();
-	  $frm_edit_box->add_text_area ("frm_edit_box_ct[".
-					$ct[$i]['nom_boite']."]",
-					"Contenu de la boite " .
-					$ct[$i]['nom_boite'],
-					$ct[$i]['contenu_boite'],
-					80,
-					10);
-	}
+      $ct[] = $req->get_row ();
+      $frm_edit_box->add_text_area ("frm_edit_box_ct[".
+            $ct[$i]['nom_boite']."]",
+            "Contenu de la boite " .
+            $ct[$i]['nom_boite'],
+            $ct[$i]['contenu_boite'],
+            80,
+            10);
     }
-  $frm_edit_box->add_submit("frm_edit_box_submit",
-			    "Modifier");
+  }
+  $frm_edit_box->add_submit("frm_edit_box_submit", "Modifier");
 
   $site->add_contents($frm_edit_box);
 
