9a143f8eb7bbc3a89d9de2c034165d6d24e2ac34
gliss <gliss@troll>
2007-12-08 14:15:57 +0000

    wip

--- temp/calendar.js
+++ temp/calendar.js
@@ -1,38 +1,18 @@
 
-function findPos(obj)
-{
-	var curleft = curtop = 0;
-	if (obj.offsetParent)
-	{
-		curleft = obj.offsetLeft
-		curtop = obj.offsetTop
-		while (obj = obj.offsetParent)
-		{
-			curleft += obj.offsetLeft
-			curtop += obj.offsetTop
-		}
-	}
-	return [curleft,curtop];
-}
-
 function opencal(_ref)
 {
 	var ref = document.getElementById(_ref);
-//	var elem = document.getElementById('calendar'); 
-
+	var pos = findPos(ref);
+		
 	var elem = document.createElement('div');
 	elem.id = 'calendar';
 	elem.className = 'container';
 	document.body.appendChild(elem);
-	alert(elem);
-	var pos = findPos(ref);
-	/*
-	elem.style.display = "block";
-	elem.style.left = event.clientX;
-	elem.style.top = event.clientY;
-/*	elem.style.left = pos[0] + 20;
-	elem.style.top = pos[1];*/
-	
+
+	elem.style.display = 'block';
+	elem.style.left = pos[0] + 20;
+	elem.style.top = pos[1] - 10;
+	openInContents('calendar', './little_calendar2.php', 'get_cal'); 
 }
 
 function closecal()
