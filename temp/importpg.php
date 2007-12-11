8f90d229c9f5a5be2099c0fa9108f62bfd981055
Feu <feu@troll>
2007-12-11 19:10:31 +0000

    pg2: import categories et affichage

--- temp/importpg.php
+++ temp/importpg.php
@@ -16,14 +16,14 @@ $cat1_to_cat=array();
 $cat2_to_cat=array();
 $cat3_to_cat=array();
 
-$rootcat = pgcategory($site->db,$site->dbrw);
+$rootcat = new pgcategory($site->db,$site->dbrw);
 $rootcat->create ( null, "Le Guide", "", 1, null, null, null, null, null, null );
 
 $req = new requete($dbpg,"SELECT * FROM pg_cat1");
 
 while ( $row = $req->get_row() )
 {
-  $cat = pgcategory($site->db,$site->dbrw);
+  $cat = new pgcategory($site->db,$site->dbrw);
   $cat->create ( $rootcat->id, utf8_encode($row['nom']), "", $row['ordre'], 
     sprintf("%02x%02x%02x",$row['r1'],$row['v1'],$row['b1']), 
     sprintf("%02x%02x%02x",$row['r2'],$row['v2'],$row['b2']), 
@@ -41,7 +41,7 @@ while ( $row = $req->get_row() )
 {
   $parent = $cat1_to_cat[$row['id_cat1']];
   
-  $cat = pgcategory($site->db,$site->dbrw);
+  $cat = new pgcategory($site->db,$site->dbrw);
   $cat->create ( $parent->id, utf8_encode($row['nom']), "", 1, 
     $parent->couleur_bordure_web,
     $parent->couleur_titre_web,
@@ -58,7 +58,7 @@ while ( $row = $req->get_row() )
 {
   $parent = $cat2_to_cat[$row['id_cat2']];
   
-  $cat = pgcategory($site->db,$site->dbrw);
+  $cat = new pgcategory($site->db,$site->dbrw);
   $cat->create ( $parent->id, utf8_encode($row['nom']), "", 1, 
     $parent->couleur_bordure_web,
     $parent->couleur_titre_web,
