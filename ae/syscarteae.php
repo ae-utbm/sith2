e7b7cd8e227797ace58e2a2218f7b1d2d9c67221
Ayolo <ayolo@troll>
2007-12-13 23:05:19 +0000

    bleh

--- ae/syscarteae.php
+++ ae/syscarteae.php
@@ -76,7 +76,7 @@ if ( $_REQUEST["action"] == "genfact" )
   {
     $asso->load_by_id($row['id_asso']);print_r($row['id_asso']);
     if ( !$asso->is_valid() )
-      continue;
+      print_r("bleh");
     $facturing_infos = array ('name' => $asso->nom,
        'addr' => explode("\n",utf8_decode($asso->adresse_postale)),
        'logo' => "/var/www/ae/www/ae2/var/img/logos/".$asso->nom_unix.".jpg");
