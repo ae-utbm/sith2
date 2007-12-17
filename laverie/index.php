5e21a983f84d81788c170777972d689a2ddd53e6
BenC <benc@troll>
2007-12-17 12:52:58 +0000

    Rajout d'une indication sur les lieux de retrait des jetons

--- laverie/index.php
+++ laverie/index.php
@@ -239,6 +239,9 @@ elseif ( $_REQUEST["action"] == "searchmc" )
 $site->start_page("services","Laverie");
 $cts = new contents("<a href=\"index.php\">Laverie</a>");
 
+$cts->itemlist("Vous pouvez venir récupérer vos jetons",false,array("Belfort : tous les soirs (sauf le weekend) de 20h à 20h15","Sevenans : <a href=\"/asso/membres.php?view=trombino&id_asso=84\">contacter les responsables</a>.");
+
+
 $frm = new form("searchmc","index.php",false,"POST","Reserver un creneau");
 $frm->add_hidden("action","searchmc");
 $frm->add_select_field("id_salle","Lieu",$salles, $_REQUEST["id_salle"]);
