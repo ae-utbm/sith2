ac9509ebb0136c60ab3dfbd4693e195702cdac81
gliss <gliss@troll>
2007-12-23 00:37:50 +0000

    path pour les uvs sur plusieurs dpt

--- uvs/uvs.php
+++ uvs/uvs.php
@@ -404,7 +404,12 @@ if (isset($_REQUEST['id_uv']) || (isset($_REQUEST['code_uv']))
 
 	$uv->load_depts();
   $path = "<a href=\"".$topdir."uvs/\"><img src=\"".$topdir."images/icons/16/lieu.png\" class=\"icon\" />  Pédagogie </a>";
-  $path .= " / "."<a href=\"".$topdir."uvs/uvs.php?iddept=".$uv->depts[0]."\"><img src=\"".$topdir."images/icons/16/forum.png\" class=\"icon\" /> ".$uv->depts[0]."</a>";
+  $path .= " / "."<img src=\"".$topdir."images/icons/16/forum.png\" class=\"icon\" /> ";
+  	$stop = count($uv->depts);
+  	for($i=0; $i<$stop; $i++){
+  		$path .= "<a href=\"".$topdir."uvs/uvs.php?iddept=".$uv->depts[$i]."\"> ".$uv->depts[$i]."</a>";
+  		if($i+1 < $stop) $path .= " ,";
+  	}
   $path .= " / "."<a href=\"".$topdir."uvs/uvs.php?id_uv=$uv->id\"><img src=\"".$topdir."images/icons/16/emprunt.png\" class=\"icon\" /> $uv->code</a>";
 
   $cts = new contents($path);
