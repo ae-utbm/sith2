72c81f7e0cdd18c5d880c417eaf41225e8ae7aa9
BenC <benc@troll>
2007-12-26 17:59:13 +0000

    bleh

--- sas2/search.php
+++ sas2/search.php
@@ -197,22 +197,25 @@ if ( $_REQUEST["action"] == "search" )
     
     if ( !empty($presents) )
     {
+      $count_presents = array_count_values( $presents );
       foreach ( $presents as $present )
       {
-        $count_presents = array_count_values( $presents );
-        
         if ( $count_presents[$present] != 1 )
-          $present = -1;
-        
-        $user_present = new utilisateur($site->db);
-        $user_present->load_by_id($present);
-        
-        if ( $user_present->is_valid() )
         {
-          echo $user_present->nom;
-        
-          $joins[] = "INNER JOIN sas_personnes_photos AS `p".mysql_escape_string($user_present->id)."` ON ( sas_photos.id_photo=p".mysql_escape_string($user_present->id).".id_photo AND p".mysql_escape_string($user_present->id).".id_utilisateur='".mysql_escape_string($user_present->id)."') ";
-          $params.="&presents[]=".$user_present->id;
+          $count_presents[$present]--;
+        }
+        else
+        {       
+          $user_present = new utilisateur($site->db);
+          $user_present->load_by_id($present);
+          
+          if ( $user_present->is_valid() )
+          {
+            echo $user_present->nom;
+          
+            $joins[] = "INNER JOIN sas_personnes_photos AS `p".mysql_escape_string($user_present->id)."` ON ( sas_photos.id_photo=p".mysql_escape_string($user_present->id).".id_photo AND p".mysql_escape_string($user_present->id).".id_utilisateur='".mysql_escape_string($user_present->id)."') ";
+            $params.="&presents[]=".$user_present->id;
+          }
         }
       }
     }
