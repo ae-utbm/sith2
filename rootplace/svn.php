83501cc5a55e909fca1ded3c57546d79d1e4701b
Ayolo <ayolo@troll>
2007-12-26 21:53:18 +0000

    bleh

--- rootplace/svn.php
+++ rootplace/svn.php
@@ -153,7 +153,7 @@ if(isset($_REQUEST["id_depot"]))
     
     $cts->add_title(2,"Information sur le dépot");
     $cts->add_paragraph("Nom : ".$svn->nom."<br />type : ".$svn->type);
-    $req2 = new requete($site->db,"SELECT `id_utilisateur`, CONCAT(`utilisateurs`.`prenom_utl`,' ',`utilisateurs`.`nom_utl`) as `nom_utilisateur2` ".
+    $req2 = new requete($site->db,"SELECT `id_utilisateur`, CONCAT(`utilisateurs`.`prenom_utl`,' ',`utilisateurs`.`nom_utl`) as `nom_utilisateur` ".
                                   "FROM `svn_member_depot` ".
                                   "INNER JOIN `utilisateurs` USING(`id_utilisateur`) " .
                                   "WHERE `id_depot`='".$svn->id."'");
