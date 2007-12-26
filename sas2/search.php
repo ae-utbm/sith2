4d74a7a00544e9000efa756e6c6a71f721092c54
BenC <benc@troll>
2007-12-26 17:47:39 +0000

    caca

--- sas2/search.php
+++ sas2/search.php
@@ -203,7 +203,7 @@ if ( $_REQUEST["action"] == "search" )
         $user_present->load_by_id($present);
         echo $user_present->nom;
         
-        $joins[] = "INNER JOIN sas_personnes_photos AS `p2` ON ( sas_photos.id_photo=p2.id_photo AND p2.id_utilisateur='".mysql_escape_string($user_present->id)."') ";
+        $joins[] = "INNER JOIN sas_personnes_photos AS `p".mysql_escape_string($user_present->id)."` ON ( sas_photos.id_photo=p".mysql_escape_string($user_present->id).".id_photo AND p".mysql_escape_string($user_present->id).".id_utilisateur='".mysql_escape_string($user_present->id)."') ";
         $params.="&presents[]=".$user_present->id;
       }
     }
