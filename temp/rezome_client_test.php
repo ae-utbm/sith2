309a3db6a8af87757fdaec70c6fdee8c9e73ac09
BenC <benc@troll>
2008-02-18 17:29:29 +0000

    Test de client pour le webservice de Rezome

---
+++ temp/rezome_client_test.php
@@ -0,0 +1,21 @@
+<?php
+  /* Premier test, on inscrit un utilisateur */
+  $request = <<<XML
+<apikey>ML2ee7549d391c2fcd451758ed6cb6dcaa22d8ff80</apikey>
+<utbm>false</utbm>
+<nom>Crétin</nom>
+<prenom>Lapin</prenom>
+<email>benc@oxynux.org</email>
+<password>bleh</password>
+<naissance>2005-04-02</naissance>
+<droitimage>true</droitimage>
+<alias>LapinCretin</alias>
+<sexe>1</sexe>
+XML;
+
+  $client = new WSClient(array("to" => "http://ae.utbm.fr/rezomesync.php"));
+  $response = $client->request($reqest);
+
+  echo "Service replied asking: '".$response->str."'\n";
+
+?>
