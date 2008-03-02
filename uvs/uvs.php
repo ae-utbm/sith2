542821543cb3239d9a8b72100f1f6b44386c6e4e
pedrov <pedrov@troll>
2008-03-02 11:40:05 +0000

    partie fichiers / paths - dbg

--- uvs/uvs.php
+++ uvs/uvs.php
@@ -413,6 +413,9 @@ if (isset($_REQUEST['id_uv']) || (isset($_REQUEST['code_uv']))
   $path .= " / "."<a href=\"".$topdir."uvs/uvs.php?id_uv=$uv->id\"><img src=\"".$topdir."images/icons/16/emprunt.png\" class=\"icon\" /> $uv->code</a>";
 
   /* path partie fichiers */
+  echo $uv->get_path($_REQUEST['id_folder']);
+  
+
   if ((isset ($uv)) && (isset($_REQUEST['id_folder']))
       && ($uv->check_folder($_REQUEST['$id_folder'])))
     {
