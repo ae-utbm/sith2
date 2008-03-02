3bfa3e3a0213713d22bc93bc860266842d90c7c0
pedrov <pedrov@troll>
2008-03-02 10:58:13 +0000

    abstract :)

--- include/entities/edt.inc.php
+++ include/entities/edt.inc.php
@@ -39,7 +39,8 @@ class edt extends stdentity
   var $edt_arr;
 
   function _load($row) {}
-  function load_by_id($id) { $this->load($id); } ;
+  
+  function load_by_id($id) { $this->load($id); }
   
 
   function load ($id, $semestre = null)
