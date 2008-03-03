d6b3e2d38ef761db95237ac524b4fb7cab36b213
furtif <furtif@troll>
2008-03-03 20:59:40 +0000

    Fix bouton news.php

--- js/site.js
+++ js/site.js
@@ -189,12 +189,16 @@ function on_off_icon_store (id,topdir,key)
 function on_off_options (name,val,oldval)
 {
 	var obj = document.getElementById(name + '_' + val + '_contents');
+
+	if ( ! obj.checked ){
+
 	obj.style.display = 'block';
 
 	if ( oldval )
 	{
 		var oobj = document.getElementById(name + '_' + oldval + '_contents');
 		oobj.style.display = 'none';
+	}
 	}
 }
 
