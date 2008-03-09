6ba597212ace501508a7dc3d7037519275f42e90
pedrov <pedrov@troll>
2008-03-09 17:34:35 +0000

    tests classe requete

--- temp/sqltest.php
+++ temp/sqltest.php
@@ -5,7 +5,7 @@ require_once($topdir . "include/site.inc.php");
 
 $base = new mysqlae();
 
-$req = new requete($base, "SELECT * FROM utilisateurs WHERE `nom_utl` = 'SFSMLVKDNFLBKM'");
+$req = new requete($base, "SELECT `id_comment` FROM `edu_uv_comments` WHERE `state_comment` = 1");
 
 print_r($req);
 
