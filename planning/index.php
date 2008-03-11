63cb137855c671ab8567b311171cea20c319f01b
Sarah <sarah@troll>
2008-03-11 20:33:03 +0000

    kwain

--- planning/index.php
+++ planning/index.php
@@ -22,7 +22,9 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
  * 02111-1307, USA.
  */
- 
+
+define("BUREAU_BELFORT", 6);
+
 $topdir = "../";
 require_once($topdir. "include/site.inc.php");
 
@@ -39,12 +41,21 @@ if ( $_REQUEST["action"] == "searchpl" )
   $site->start_page("services","Planning");
   $cts = new contents("<a href=\"index.php\">Planning</a> / ".$lieux[$_REQUEST["id_salle"]]." / Affichage");
   
+  //planning
+  
+  if( $_REQUEST["id_salle"]==BUREAU_BELFORT)
+  {
+  	$is_admin = $site->user->is_in_group("gestion_ae");
+  	if ( $is_admin )
+  		$cts->add_paragraph("<a href=\"admin.php\">Administration</a>");
+  }
+  
   $frm = new form("searchpl","index.php",false,"POST","Nouvelle recherche");
   $frm->add_hidden("action","searchpl");
   //if ( isset($_REQUEST["fallback"]) )
     //$frm->add_hidden("fallback",$_REQUEST["fallback"]);
   $frm->add_select_field("id_salle","Lieu",$lieux, $_REQUEST["id_salle"]);
-  $frm->add_submit("afficher","Afficher un planning");
+  $frm->add_submit("afficher","Afficher le planning");
   $cts->add($frm,true);
   
   $site->add_contents($cts);
@@ -60,7 +71,7 @@ $cts = new contents("<a href=\"index.php\">Planning</a>");
 $frm = new form("searchpl","index.php",false,"POST","Consulter un planning");
 $frm->add_hidden("action","searchpl");
 $frm->add_select_field("id_salle","Lieu",$lieux, $_REQUEST["id_salle"]);
-$frm->add_submit("afficher","Afficher un planning");
+$frm->add_submit("afficher","Afficher le planning");
 $cts->add($frm,true);
 
 $site->add_contents($cts);
