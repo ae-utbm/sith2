d04ab336a6734d91383fc38adfb8bc02897861cc
Feu <feu@troll>
2008-03-11 20:03:57 +0000

    test

--- include/mysql.inc.php
+++ include/mysql.inc.php
@@ -115,7 +115,7 @@ class requete {
   function requete ($base, $req_sql, $debug = 0) {
     global $timing;
     $timing["mysql"] -= microtime(true);
-    $st = microtime(true);
+    //$st = microtime(true);
     $this->base = $base;
     $this->sql = $req_sql;
     $esql = explode(" ", $req_sql);
@@ -129,9 +129,9 @@ class requete {
     $res = mysql_query($req_sql, $base->dbh);
     $timing["mysql"] += microtime(true);
     $timing["mysql.counter"]++;
-    $fn = microtime(true);
+    //$fn = microtime(true);
     //if ( $fn-$st > 0.001 )
-    $timing["req"][] = array($fn-$st,$req_sql);
+    //$timing["req"][] = array($fn-$st,$req_sql);
     
     $this->errno = mysql_errno($base->dbh);
     if ($this->errno != 0) {
--- temp/timing.php
+++ temp/timing.php
@@ -4,14 +4,15 @@ $topdir="../";
 include($topdir. "include/site.inc.php");
  
 $site = new site ();
-for($m=0;$m<4;$m++)
-{
-$timing["method$m"] -= microtime(true);
+
 for($i=0;$i<1000;$i++)
 {
+  for($m=0;$m<4;$m++)
+{
+$timing["method$m"] -= microtime(true);
   $site->load_session($_COOKIE['AE2_SESS_ID'],$m);
-}
 $timing["method$m"] += microtime(true);
+}
 }
 
 echo "<pre>";
