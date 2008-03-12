203455608568eeb0be35ec2b2f3f3cea413f71e6
furtif <furtif@troll>
2008-03-12 11:40:17 +0000

    [aecsm] test requete

--- aecms/newsliste.php
+++ aecms/newsliste.php
@@ -90,14 +90,24 @@ else{
 
   $req = new requete($site->db,
 		     "SELECT `nvl_nouvelles`.*,
+                      FROM `nvl_nouvelles`
+                      WHERE `nvl_nouvelles`.`modere_nvl`='1' 
+                      AND `nvl_nouvelles`.`id_canal`='".NEWS_CANAL_AECMS."' 
+                      ORDER BY `nvl_nouvelles`.`date_nvl` 
+                      DESC");
+  /*
+ $req = new requete($site->db,
+		     "SELECT `nvl_nouvelles`.*,
                              CONCAT(`utilisateurs`.`prenom_utl`,
                                     ' ',
                                     `utilisateurs`.`nom_utl`) AS `nom_prenom`
                       FROM `nvl_nouvelles`, `utilisateurs` 
                       WHERE `nvl_nouvelles`.`modere_nvl`='1' 
                       AND `nvl_nouvelles`.`id_utilisateur` = `utilisateurs`.`id_utilisateur`
+                      AND `nvl_nouvelles`.`id_canal`='".NEWS_CANAL_AECMS."' 
                       ORDER BY `nvl_nouvelles`.`date_nvl` 
                       DESC");
+ */
 
   $texte = new contents("Liste des nouvelles",
 			  "<p>Sur cette page, vous pouvez mod&eacute;rer ".
@@ -110,7 +120,7 @@ else{
 			"newsliste.php",
 			"id_nouvelle",
 			array ("titre_nvl" => "Titre",
-			       "nom_prenom" => "auteur",
+			       "id_utilisateur" => "auteur",
 			       "date_nvl" => "Date"),
 			array ("edit" => "modifier",
 			       "delete" => "supprimer"),
