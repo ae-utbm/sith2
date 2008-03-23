fe75af39740e786259a7a71ae813deda4651b183
Sarah <sarah@troll>
2008-03-23 18:30:40 +0000

    blah

--- planning/index.php
+++ planning/index.php
@@ -279,9 +279,11 @@ if($site->user->is_in_group("gestion_ae"))
 else
 {
   $sql = 
-    "SELECT pl_gap.id_gap, start_gap, end_gap, pl_gap.id_planning
-     FROM pl_gap, pl_gap_user
-     WHERE pl_gap.id_planning='".PERM_AE_BELFORT."'";
+    "SELECT id_gap, start_gap, end_gap, pl_gap.id_planning, id_utilisateur
+     FROM pl_gap
+     LEFT JOIN pl_gap_user USING(id_gap)
+     LEFT JOIN utilisateurs USING(id_utilisateur)
+     WHERE pl_gap.id_planning=164 AND id_utilisateur != NULL";
 }
      
   $pl = new weekplanning ("Planning", $site->db, $sql, "id_gap", "start_gap", "end_gap", "texte", "index.php?action=searchpl", "index.php?action=details", "", PL_LUNDI, true);
