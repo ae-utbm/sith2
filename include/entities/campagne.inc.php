766e6ef970d42bee69ebc02c8719056b9176b589
furtif <furtif@troll>
2008-03-25 10:41:12 +0000

    Ayolo il manquait un 'e'

--- include/entities/campagne.inc.php
+++ include/entities/campagne.inc.php
@@ -167,8 +167,8 @@ class campagne extends stdentity
       array("nom_question" => $question,
             "description_question" => $desc,
             "type_question" => $type,
-            "reponses_question" => $resp,
-            "limits_reponses_question" => $limit
+            "reponses_question" => $resp
+            "limites_reponses_question" => $limit
            ),
       array("id_campagne"=>$this->id,"id_question"=>$id)
       );    
