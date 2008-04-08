5b69356c0bd0b566943b05048466786b088107a9
Ayolo <ayolo@troll>
2008-04-08 09:13:49 +0000

    bleh

--- temp/bleh.php
+++
@@ -1,9 +0,0 @@
-<?php
-$topdir="../";
-require_once($topdir. "include/site.inc.php");
-$site = new site();
-
-$req = new requete($site->dbrw, "UPDATE `site_parametres` SET valeur_param='b:1;' WHERE nom_param='closed'");
-exit();
-
-?>
