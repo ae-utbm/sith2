758d9aca525967bbc3a72e3af6d1033f42f980df
Sarah <sarah@troll>
2008-04-08 08:42:15 +0000

    truc temporaire pour un sondage

--- include/site.inc.php
+++ include/site.inc.php
@@ -317,7 +317,10 @@ class site extends interfaceweb
         $this->add_box("forum",$this->get_forum_box());
         
         $this->add_box("comptoirs",$this->get_comptoirs_box());        
-        $this->add_box("sondage",$this->get_sondage());
+        if($site->user->sexe == '2')
+        {
+        	$this->add_box("sondage",$this->get_sondage());
+        }
         $this->set_side_boxes("right",
           array("planning","photo","anniv","stream", "services",
                 "sondage","comptoirs","forum", "planning_permanences"),"accueil_c_right");
