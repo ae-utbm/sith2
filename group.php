0aaff7ad959199efbcefb760b2a12d1210aae561
BenC <benc@troll>
2008-04-08 12:00:18 +0000

    Oups le point virgule

--- group.php
+++ group.php
@@ -79,7 +79,7 @@ if (  $grp->id > 0)
       {
         $grp->remove_user_from_group($id_utilisateur);
         $user = new utilisateur($site->db);
-        $user->load_by_id($id_utilisateur)
+        $user->load_by_id($id_utilisateur);
         $site->log("Retrait d'un utilisateur du groupe ". $grp->nom,"Retrait de l'utilisateur ".$user->nom." ".$user->prenom." (id : ".$user->id.") du groupe ". $grp->nom ." (id : ".$grp->id.")","Groupes",$site->user->id);
       }
     }
