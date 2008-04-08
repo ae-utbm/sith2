d5697f9d253dfaef91a8ac7a676b6bd3f7a92536
BenC <benc@troll>
2008-04-08 18:28:35 +0000

    Doigts crochus \o/

--- rootplace/logs.php
+++ rootplace/logs.php
@@ -26,7 +26,7 @@ $topdir="../";
 
 require_once($topdir. "include/site.inc.php");
 require_once($topdir."include/cts/sqltable.inc.php");
-require_once($topdir."include/entities/utilisateur.inc/php");
+require_once($topdir."include/entities/utilisateur.inc.php");
 
 $site = new site ();
 
