4fcb8cefb11824b1b81d1bec214d8bc65952ce94
furtif <furtif@troll>
2008-04-11 20:31:01 +0000

    debug

--- forum2/liste.php
+++ forum2/liste.php
@@ -90,6 +90,8 @@ if( isset($_REQUEST["action"]) &&
       $cts->add_paragraph("Le forum ".$forum->titre.
                           " n'a pas &eacute;t&eacute; supprim&eacute; !");
 
+      print_r($rows);
+			exit;
       if( isset($rows[0]["id_forum"]) ){
         $cts->add_paragraph("Veuillez avant supprimer les forums ci-dessous");
         $tbl = new sqltable(
