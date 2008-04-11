5fd8d8066354a6a2c81768b779e365c9ce8d45bb
furtif <furtif@troll>
2008-04-11 16:32:31 +0000

    debug

--- forum2/index.php
+++ forum2/index.php
@@ -553,8 +553,11 @@ if ( $sujet->is_valid() )
       {
         $_auteur=new utilisateur($site->db,$site->dbrw);
         $_auteur->load_by_id($message->id_utilisateur);
-        if(!is_null($_auteur->id))
+        if(!is_null($_auteur->id)){
+					echo $_auteur->nom;
+				echo $_auteur->surnom;
           $_auteur="=".($_auteur->surnom!=null ? $_auteur->surnom : $_auteur->alias);
+				}
       }
       
       $rpltext = "[quote".$_auteur."]".$message->contenu . "[/quote]";
