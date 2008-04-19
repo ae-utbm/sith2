6df32f91beabb447648581c27ef6e4b2b6b68615
BenC <benc@troll>
2008-04-19 10:13:47 +0000

    Avec le bon champ ça ira mieux

--- temp/list_alias_invalides.php
+++ temp/list_alias_invalides.php
@@ -6,12 +6,12 @@ require_once($topdir. "include/site.inc.php");
 
 $site = new site ();
 
-$req = new requete($site->db, "SELECT id_utilisateur, CONCAT(prenom_utl,' ',nom_utl) AS nom_utilisateur, alias FROM utilisateurs");
+$req = new requete($site->db, "SELECT id_utilisateur, CONCAT(prenom_utl,' ',nom_utl) AS nom_utilisateur, alias_utl FROM utilisateurs");
 
 while( $row = $req->get_row() )
 {
-  if (preg_match("#^([a-z0-9][a-z0-9\.]+)$#i",strtolower($row["alias"])) || empty($row["alias"]) )
-    echo $row["id_utilisateur"]." : ".$row["nom_utilisateur"]." (".$row["alias"].")<br />";
+  if (preg_match("#^([a-z0-9][a-z0-9\.]+)$#i",strtolower($row["alias_utl"])) || empty($row["alias_utl"]) )
+    echo $row["id_utilisateur"]." : ".$row["nom_utilisateur"]." (".$row["alias_utl"].")<br />";
 }
 
 ?>
