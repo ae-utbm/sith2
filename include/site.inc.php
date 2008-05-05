a735ad63f651837e21bad100cf98fc7bd14201c2
BenC <benc@troll>
2008-05-05 19:02:27 +0000

    Thème weekair08

--- include/site.inc.php
+++ include/site.inc.php
@@ -80,7 +80,7 @@ class site extends interfaceweb
      */
     if ( $siteae )
     {
-      $this->add_css("themes/ff1j08/css/site.css");
+      $this->add_css("themes/weekair08/css/site.css");
     }
     
   }  
