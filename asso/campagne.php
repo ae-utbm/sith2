a3323ee1d97cc6c1c18536e284c696284136f943
BenC <benc@troll>
2008-05-15 16:45:57 +0000

    Et la aussi

--- asso/campagne.php
+++ asso/campagne.php
@@ -256,8 +256,8 @@ elseif(!is_null($cpg->id) && $_REQUEST["action"]=="results" && $cpg->asso==$_REQ
 
         $list = new itemlist($nom_question);
       }
-
       $list->add($valeur_reponse." : ".$nombre_reponses);
+      $id_question_precedente = $id_question;
     }
     $board->add($list,true);
     $cts->add($board);
