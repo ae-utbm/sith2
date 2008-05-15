afab724b6db2a3cccbd8b11ab317351dda01058b
BenC <benc@troll>
2008-05-15 07:02:36 +0000

    Cradiiiingue

--- temp/liste_cotisants.php
+++ temp/liste_cotisants.php
@@ -8,7 +8,13 @@ $site = new site ();
 
 $req = new requete($site->db, "SELECT CONCAT(prenom_utl,' ',nom_utl) AS nom_utilisateur FROM utilisateurs WHERE ae_utl='1' ORDER by nom_utl");
 
+$i = 0;
+echo "<table>";
 while( $row = $req->get_row() )
-  echo $row["nom_utilisateur"]."<br />";
-
+{
+  if($i == 0){echo "<tr>";}
+  echo "<td>".$row["nom_utilisateur"]."</td>";
+  if($i == 3){echo "</tr>"; $i = 0;}else{$i++;}
+}
+echo "</table>";
 ?>
