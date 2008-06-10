00fb55c13e3a7b146eaf5118fee62309050319b6
Sarah <sarah@troll>
2008-06-10 21:42:56 +0000

    mail soiree filles, encooore...

--- temp/mail_soiree_filles.php
+++ temp/mail_soiree_filles.php
@@ -9,31 +9,17 @@ $headers = 'From: Laure Guicherd <laure.guicherd@gmail.com>' . "\r\n" .
     'Reply-To: Laure Guicherd <laure.guicherd@gmail.com>' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-$message = "Salut les filles !
+$message = "Rebonjour les filles !
 
-Marre de ne cotoyer que des garçons ? Besoin d'un peu de douceur (ou pas) dans ce monde presque exclusivement masculin ?
-Nous avons la solution : Viens cotoyer 2 fois plus de chromosomes X à la soirée filles, exclusivement réservée au sexe statistiquement faible de l'école ( <- nous ) et interdite aux 88% poilus restants !
+J'espère que vous n'avez pas oublié que la soirée filles est demain :p
+Pour celles qui n'ont pas encore cotisé, je vous rappelle que la cotiz est toujours dispo sur l'e-boutic, et que demain à 10h devant l'A200 se tiendra une perm :)
+Au cas ou il y a vraiment un problème, contactez-moi (numéro sur le matmatronch)
 
-Le planning très chargé du semestre de nous ayant pas vraiment laissé le choix, la soirée aura lieu au foyer le mercredi 11 juin.
+D'autre part, je vous contacte aussi à propos des perms de bar : si l'une d'entre vous est motivée, manifestez-vous rapidement histoire qu'on essaie de se mettre d'accord sur des horaires approximatifs.
 
-Si tu veux en être, nous avons besoin de ton avis pour le choix du thème, parmi les trois suivants sortis tout droit de nos cerveaux mignons mais malades :
+A demain !
 
-- Animales : pour celles qui ont toujours rêvé de se déguiser en lapine, en chatte ou même en moule... et rugir de plaisir (ok, pas pour la moule, mais c'est vous qui voyez)
-
-- Militaire : pour les amoureuses de l'uniforme et de la discipline...
-
-- Barbie : pour celles qui ne se sont jamais remises de ce fol amour avec Ken...
-
-Viens donc voter pour ton thème préféré sur le site de l'AE !
-
-
-Nous organiserons une réunion une fois que le thème sera fixé, afin de préparer tout ça plus en détail. Si tu veux aider à l'organisation, fais-nous signe... Plus nous serons nombreuses, plus la soirée sera réussie.
-
- A bientôt
-
- Chattement vôtre,
-
- Dyna, éCoume et Locoms
+Locoms
 ";
 
 $req = new requete (new mysqlae(),
@@ -49,8 +35,8 @@ $req = new requete (new mysqlae(),
 
 while($res = $req->get_row())
 {
-  echo $res['nom'] . " &lt;" . $res['email_utl'] . "&gt; : Sent <br />";
-  //mail($res['nom']." <".$res['email_utl'].">", $subject, $message, $headers);
+  //echo $res['nom'] . " &lt;" . $res['email_utl'] . "&gt; : Sent <br />";
+  mail($res['nom']." <".$res['email_utl'].">", $subject, $message, $headers);
 }
 
 ?>
