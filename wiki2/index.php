21624857003880a6f8ff67924df9c0de566b582a
BenC <benc@troll>
2008-06-11 08:11:55 +0000

    'tain mais chuis un boulet, mettez moi au pilori

--- wiki2/index.php
+++ wiki2/index.php
@@ -299,7 +299,7 @@ if ( $req->lines == 1 )
 if ( !is_null($asso_id))
 {
   $asso = new asso();
-  $asso->load_by_id($asso);
+  $asso->load_by_id($asso_id);
 
   $cts = new contents($asso->get_html_path());
   $site->start_page("presentation","Wiki");
