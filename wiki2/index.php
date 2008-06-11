8f198a45e0c70deb12eeb13f8269621b100228e5
BenC <benc@troll>
2008-06-11 08:02:52 +0000

    En fait mon castor va bien

--- wiki2/index.php
+++ wiki2/index.php
@@ -289,14 +289,14 @@ else
 
 $castor = explode(":",$pagepath);
 
-print_r($castor);
-
 $req = new requete($site->db,"SELECT asso.id_asso FROM asso 
                               LEFT JOIN asso AS asso_parent ON asso.id_asso_parent=asso_parent.id_asso
                               WHERE CONCAT(asso_parent.nom_unix_asso,':',asso.nom_unix_asso)='".$castor[0].":".$castor[1]."'");
 
 if ( $req->lines == 0 )
   list($asso_id) = $req->get_row();
+
+echo $asso_id;
 
 if ( !is_null($asso_id))
 {
