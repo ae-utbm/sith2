72c53e92fec027bee5260bc6e3d693321f31ca58
BenC <benc@troll>
2008-06-30 10:26:05 +0000

    Moins crade

--- include/cts/cached.inc.php
+++ include/cts/cached.inc.php
@@ -53,14 +53,15 @@ class cachedcontents extends stdcontents
   
   public function set_contents ( &$contents )
   {
-    if ( !file_exists(CACHE_DIR) )
-      @mkdir(CACHE_DIR);
+    if ( !file_exists(CACHE_DIR) && is_writable(CACHE_DIR."../")
+      mkdir(CACHE_DIR);
     
     $this->title = $contents->title;
     $this->buffer = 
       "<!-- C".date ("d/m/Y H:i:s")." -->".
       $contents->html_render();
-    @file_put_contents(CACHE_DIR.$this->uid,$this->title."\n".$this->buffer);
+    if ( is_writable(CACHE_DIR) )
+      file_put_contents(CACHE_DIR.$this->uid,$this->title."\n".$this->buffer);
     return $this;
   } 
   
