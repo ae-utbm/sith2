b9e941cdf8a5609bbd00a95f70415f8951a129c2
Ayolo <ayolo@troll>
2008-07-03 16:40:36 +0000

    bleh

--- include/cts/wiki.php
+++ include/cts/wiki.php
@@ -210,7 +210,9 @@ class diff extends Text_Diff_Renderer
   {
     $this->title="Différences entre les révisions ".$old['rev']." et ".$new['rev'];
     $diff = &new Text_Diff('auto',array(split("\n",$old['cts']),split("\n",$new['cts'])));
-    $this->buffer.="<pre>".$this->render($diff)."</pre>";
+    $lines = split("\n",$this->render($diff));
+    foreach($lines as $line)
+      $this->buffer.=$line."<br />";
   }
 }
 
