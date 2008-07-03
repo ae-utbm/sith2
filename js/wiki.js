3947bf73eb63f443e6f34d4c90e022ff21253b7e
Ayolo <ayolo@troll>
2008-07-03 12:51:33 +0000

    debug

--- js/wiki.js
+++ js/wiki.js
@@ -61,88 +61,81 @@ function wiki_lock_maintain ( topdir, locktime, pagename )
 
 }
 
-function historyRadios(parent)
-{
+function historyRadios(parent) {
   var inputs = parent.getElementsByTagName('input');
   var radios = [];
-  for (var i = 0; i < inputs.length; i++)
-  {
-    if (inputs[i].name == "rev_orig" || inputs[i].name == "rev_comp")
-    {
+  for (var i = 0; i < inputs.length; i++) {
+    if (inputs[i].name == "diff" || inputs[i].name == "oldid") {
       radios[radios.length] = inputs[i];
     }
   }
   return radios;
 }
 
-function diffcheck()
-{
-  var dli = false; // li où le bouton rev_orig radio est coché
-  var oli = false; // li où le bouton radio rev_comp est coché
+// check selection and tweak visibility/class onclick
+function diffcheck() {
+  var dli = false; // the li where the diff radio is checked
+  var oli = false; // the li where the oldid radio is checked
   var hf = document.getElementById('diff');
-  if (!hf)
-  {
+  if (!hf) {
     return true;
   }
   var lis = hf.getElementsByTagName('li');
-  for (var i=0;i<lis.length;i++)
-  {
+  for (var i=0;i<lis.length;i++) {
     var inputs = historyRadios(lis[i]);
-    if (inputs[1] && inputs[0])
-    {
-      if (inputs[1].checked || inputs[0].checked) // this row has a checked radio button
-      {
-        if (inputs[1].checked && inputs[0].checked && inputs[0].value == inputs[1].value)
-	  return false;
-	if (oli)
-	{
-	  if (inputs[1].checked)
-	  {
-	    oli.className = "selected";
-	    return false;
-	  }
-	}
-	else if (inputs[0].checked)
-	  return false;
-	if (inputs[0].checked)
-	  dli = lis[i];
-	if (!oli)
-	  inputs[0].style.visibility = 'hidden';
-	if (dli)
-	  inputs[1].style.visibility = 'hidden';
-	lis[i].className = "selected";
-	oli = lis[i];
-      }
-      else // no radio is checked in this row
-      {
-        if (!oli)
-	  inputs[0].style.visibility = 'hidden';
-	else
-	  inputs[0].style.visibility = 'visible';
-	if (dli)
-	  inputs[1].style.visibility = 'hidden';
-	else
-	  inputs[1].style.visibility = 'visible';
-	lis[i].className = "";
+    if (inputs[1] && inputs[0]) {
+      if (inputs[1].checked || inputs[0].checked) { // this row has a checked radio button
+        if (inputs[1].checked && inputs[0].checked && inputs[0].value == inputs[1].value) {
+          return false;
+        }
+        if (oli) { // it's the second checked radio
+          if (inputs[1].checked) {
+            oli.className = "selected";
+            return false;
+          }
+        } else if (inputs[0].checked) {
+          return false;
+        }
+        if (inputs[0].checked) {
+          dli = lis[i];
+        }
+        if (!oli) {
+          inputs[0].style.visibility = 'hidden';
+        }
+        if (dli) {
+          inputs[1].style.visibility = 'hidden';
+        }
+        lis[i].className = "selected";
+        oli = lis[i];
+      } else { // no radio is checked in this row
+        if (!oli) {
+          inputs[0].style.visibility = 'hidden';
+        } else {
+          inputs[0].style.visibility = 'visible';
+        }
+        if (dli) {
+          inputs[1].style.visibility = 'hidden';
+        } else {
+          inputs[1].style.visibility = 'visible';
+        }
+        lis[i].className = "";
       }
     }
   }
   return true;
 }
 
-function histrowinit()
-{
+// page history stuff
+// attach event handlers to the input elements on history page
+function histrowinit() {
   var hf = document.getElementById('diff');
-  if (!hf)
+  if (!hf) {
     return;
+  }
   var lis = hf.getElementsByTagName('li');
-  for (var i = 0; i < lis.length; i++)
-  {
-  alert(lis[i]);
-  return;
+  for (var i = 0; i < lis.length; i++) {
     var inputs = historyRadios(lis[i]);
-    if (inputs[0] && inputs[1])
-    {
+    if (inputs[0] && inputs[1]) {
       inputs[0].onclick = diffcheck;
       inputs[1].onclick = diffcheck;
     }
@@ -157,6 +150,5 @@ function hookEvent(hookName, hookFunct)
   else if (window.attachEvent)
     window.attachEvent("on" + hookName, hookFunct);
 }
-
 
 hookEvent("load", histrowinit);
