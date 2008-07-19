8f7729c84b784ddf337ae5d052d07a37521bfb6c
Ayolo <ayolo@troll>
2008-07-19 19:25:09 +0000

    fix

--- include/cts/gmap.inc.php
+++ include/cts/gmap.inc.php
@@ -135,7 +135,7 @@ class gmap extends stdcontents
           $first = false;
         }        
       }
-      $this->buffer .= $path["name"]."= new google.maps.Polyline([".implode(",",$points)."], \"#".$path["color"]."\", 10);\n";
+//      $this->buffer .= $path["name"]."= new google.maps.Polyline([".implode(",",$points)."], \"#".$path["color"]."\", 10);\n";
       $this->buffer .= $path["name"]."= new google.maps.Directions(map);\n";
       $this->buffer .= $path["name"].".load(\"".implode(" to: ",$points)."\", {getSteps:true});\n";
 //      $this->buffer .= $this->name.".addOverlay(".$path["name"].");\n";
