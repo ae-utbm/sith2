803423803cf3717078ae95d492201ffc5432334e
Ayolo <ayolo@troll>
2008-07-19 19:24:26 +0000

    fix

--- include/cts/gmap.inc.php
+++ include/cts/gmap.inc.php
@@ -135,9 +135,9 @@ class gmap extends stdcontents
           $first = false;
         }        
       }
-//      $this->buffer .= $path["name"]."= new google.maps.Polyline([".implode(",",$points)."], \"#".$path["color"]."\", 10);\n";
-//      $this->buffer .= $path["name"]."= new google.maps.Directions(map);\n";
-//      $this->buffer .= $path["name"].".load(\"".implode(" to: ",$points)."\", {getSteps:true});\n";
+      $this->buffer .= $path["name"]."= new google.maps.Polyline([".implode(",",$points)."], \"#".$path["color"]."\", 10);\n";
+      $this->buffer .= $path["name"]."= new google.maps.Directions(map);\n";
+      $this->buffer .= $path["name"].".load(\"".implode(" to: ",$points)."\", {getSteps:true});\n";
 //      $this->buffer .= $this->name.".addOverlay(".$path["name"].");\n";
     }
 
