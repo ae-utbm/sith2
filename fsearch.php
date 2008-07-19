f7a41c0243014a98991d9b61b64102ff308801fa
Ayolo <ayolo@troll>
2008-07-19 15:13:21 +0000

    adieux alias

--- fsearch.php
+++ fsearch.php
@@ -30,78 +30,78 @@ $site = new site ();
 // Code barres & co
 if ( $site->user->is_in_group("gestion_ae") && $_REQUEST["pattern"] != "" )
 {
-	
-	$obj = new objet($site->db);
-	$obj->load_by_cbar($_REQUEST["pattern"]);
-	if ( $obj->id > 0 )
-	{
-		header("Location: objet.php?id_objet=".$obj->id);
-		exit();	
-	}
-	
-	if ( ereg("^([A-Za-z]{1,10})([0-9]{1,20})$",$_REQUEST["pattern"],$regs))
-	{
-		$objtype = new objtype($site->db);
-		$objtype->load_by_code($regs[1]);
-		if ( $objtype->id > 0 )
-		{
-			$obj->load_by_num($objtype->id,$regs[2]);
-			if ( $obj->id > 0 )
-			{
-				header("Location: objet.php?id_objet=".$obj->id);
-				exit();	
-			}
-		}
-	}
-	
-	$user = new utilisateur($site->db);
-	$user->load_by_carteae($_REQUEST["pattern"]);
-	if ( $user->id > 0 )
-	{
-		header("Location: user.php?id_utilisateur=".$user->id);
-		exit();	
-	}
-	
-	
+  
+  $obj = new objet($site->db);
+  $obj->load_by_cbar($_REQUEST["pattern"]);
+  if ( $obj->id > 0 )
+  {
+    header("Location: objet.php?id_objet=".$obj->id);
+    exit();  
+  }
+  
+  if ( ereg("^([A-Za-z]{1,10})([0-9]{1,20})$",$_REQUEST["pattern"],$regs))
+  {
+    $objtype = new objtype($site->db);
+    $objtype->load_by_code($regs[1]);
+    if ( $objtype->id > 0 )
+    {
+      $obj->load_by_num($objtype->id,$regs[2]);
+      if ( $obj->id > 0 )
+      {
+        header("Location: objet.php?id_objet=".$obj->id);
+        exit();  
+      }
+    }
+  }
+  
+  $user = new utilisateur($site->db);
+  $user->load_by_carteae($_REQUEST["pattern"]);
+  if ( $user->id > 0 )
+  {
+    header("Location: user.php?id_utilisateur=".$user->id);
+    exit();  
+  }
+  
+  
 }
 require_once($topdir. "include/cts/fsearch.inc.php");
-$fs	= new fsearch ( $site );
-if ( $fs->nb == 1 && $fs->redirect ) 	
+$fs  = new fsearch ( $site );
+if ( $fs->nb == 1 && $fs->redirect )   
 {
-	header("Location: ".$fs->redirect);
-	exit();	
-}	
+  header("Location: ".$fs->redirect);
+  exit();  
+}  
 
 $site->start_page("","Recherche rapide");
 $cts = new contents("Resultats");
-	
+  
 
-$cts->add($fs,false,true,"fsearchblock");	
-	
-if ( $site->user->is_in_group("gestion_ae") )	
-{	
-	$frm = new form("addobjet","objet.php",!$sucess,"POST","Ajouter un objet avec comme code barre ".$_REQUEST["pattern"]);
-	$frm->add_hidden("action","addobjet");
-	$frm->add_hidden("nb","1");
-	$frm->add_hidden("force_cbar",$_REQUEST["pattern"]);
-	$frm->add_entity_select("id_objtype", "Type", $site->db, "objtype");
-	$frm->add_text_field("nom","Nom");
-	$frm->add_text_field("num_serie","Numéro de série");
-	$frm->add_date_field("date_achat","Date d'achat");
-	$frm->add_entity_select("id_asso_prop", "Propriètaire", $site->db, "asso", false, false, array("id_asso_parent"=>NULL));
-	$frm->add_entity_select("id_asso", "Gestionnaire", $site->db, "asso");
-	$frm->add_entity_select("id_salle", "Salle", $site->db, "salle");
-	$frm->add_price_field("prix","Prix d'achat");
-	$frm->add_price_field("caution","Prix de la caution");
-	$frm->add_price_field("prix_emprunt","Prix d'un emprunt");
-	$frm->add_checkbox("empruntable","Empruntable");
-	$frm->add_checkbox("en_etat","En etat");
-	$frm->add_text_area("notes","Notes");
-	$frm->add_submit("valide","Ajouter");
-	$cts->add($frm,true);
+$cts->add($fs,false,true,"fsearchblock");  
+  
+if ( $site->user->is_in_group("gestion_ae") )  
+{  
+  $frm = new form("addobjet","objet.php",!$sucess,"POST","Ajouter un objet avec comme code barre ".$_REQUEST["pattern"]);
+  $frm->add_hidden("action","addobjet");
+  $frm->add_hidden("nb","1");
+  $frm->add_hidden("force_cbar",$_REQUEST["pattern"]);
+  $frm->add_entity_select("id_objtype", "Type", $site->db, "objtype");
+  $frm->add_text_field("nom","Nom");
+  $frm->add_text_field("num_serie","Numéro de série");
+  $frm->add_date_field("date_achat","Date d'achat");
+  $frm->add_entity_select("id_asso_prop", "Propriètaire", $site->db, "asso", false, false, array("id_asso_parent"=>NULL));
+  $frm->add_entity_select("id_asso", "Gestionnaire", $site->db, "asso");
+  $frm->add_entity_select("id_salle", "Salle", $site->db, "salle");
+  $frm->add_price_field("prix","Prix d'achat");
+  $frm->add_price_field("caution","Prix de la caution");
+  $frm->add_price_field("prix_emprunt","Prix d'un emprunt");
+  $frm->add_checkbox("empruntable","Empruntable");
+  $frm->add_checkbox("en_etat","En etat");
+  $frm->add_text_area("notes","Notes");
+  $frm->add_submit("valide","Ajouter");
+  $cts->add($frm,true);
 }
 
-	
+  
 $site->add_contents($cts);
 $site->end_page();
 
