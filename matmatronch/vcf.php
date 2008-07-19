238ad985767100e697f55aa40a0c49cc5a41ea97
Ayolo <ayolo@troll>
2008-07-19 15:56:10 +0000

    perdu

--- matmatronch/vcf.php
+++ matmatronch/vcf.php
@@ -5,22 +5,22 @@ require_once($topdir. "include/site.inc.php");
 $site = new site ();
 if ( !$site->user->is_valid() )
 {
- 	header("Location: 403.php?reason=session");
-	exit(); 	
+   header("Location: 403.php?reason=session");
+  exit();   
 }
 
 if ( !$site->user->utbm && !$site->user->ae )
 {
- 	header("Location: 403.php?reason=reservedutbm");
-	exit(); 	
+   header("Location: 403.php?reason=reservedutbm");
+  exit();   
 }
 
 $user = new utilisateur($site->db,$site->dbrw);
-$user->load_by_id($_REQUEST["id_utilisateur"]);	
+$user->load_by_id($_REQUEST["id_utilisateur"]);  
 if ( $user->id < 0 )
 {
-	$site->error_not_found();
-	exit();	
+  $site->error_not_found();
+  exit();  
 }
 $user->load_all_extra();
 header("Content-Type: text/x-vcard");
