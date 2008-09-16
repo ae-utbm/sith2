ae07581ff6cc1486ac6a820ae0ae85b150518a0f
BenC <benc@troll>
2008-09-16 06:58:51 +0000

    Maintenace

--- uvs/index.php
+++ uvs/index.php
@@ -33,6 +33,13 @@ require_once($topdir. "include/site.inc.php");
 require_once($topdir. "include/entities/uv.inc.php");
 
 $site = new site();
+
+$cts = new contents("Pédagogie : en maintenance")
+$cts->add_paragraph("La partie pédagogie est fermée pour une durée indéterminée pour une refonte complète.");
+$site->add_contents($cts);
+$site->end_page();
+exit();
+
 $site->add_box("uvsmenu", get_uvsmenu_box() );
 $site->set_side_boxes("left",array("uvsmenu", "connexion"));
 
