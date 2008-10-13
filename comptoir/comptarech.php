d672c2a61fb0264c284ef6995ddd6b3d20ca151f
Ayolo <ayolo@troll>
2008-10-13 13:40:46 +0000

    coincoin

--- comptoir/comptarech.php
+++ comptoir/comptarech.php
@@ -27,16 +27,16 @@ $site = new sitecomptoirs();
 
 if ( !$site->user->is_valid() )
 {
-	header("Location: ../403.php?reason=session");
-	exit();	
-}	
+        header("Location: ../403.php?reason=session");
+        exit();        
+}        
 
 $site->fetch_admin_comptoirs();
 $comptoirs = array_merge(array(0=>"-"),$site->admin_comptoirs);
 
 if ( !count($site->admin_comptoirs) && !$site->user->is_in_group("gestion_ae") )
-	$site->error_forbidden();	
-	
+        $site->error_forbidden();        
+        
 $TypesPaiementsFull[-1]="--";
 
 $site->set_admin_mode();
@@ -61,88 +61,93 @@ $cts->add($frm,true);
 
 if ( $_REQUEST["action"] == "view" )
 {
-	$conds = array();
-	$comptoir = false;
-	
-	if ( $_REQUEST["debut"] )
-		$conds[] = "cpt_rechargements.date_rech >= '".date("Y-m-d H:i:s",$_REQUEST["debut"])."'";
-	
-	if ( $_REQUEST["fin"] )
-		$conds[] = "cpt_rechargements.date_rech <= '".date("Y-m-d H:i:s",$_REQUEST["fin"])."'";
-	
-	if ( isset($comptoirs[$_REQUEST["id_comptoir"]]) && $_REQUEST["id_comptoir"] )
-		$conds[] = "cpt_rechargements.id_comptoir='".intval($_REQUEST["id_comptoir"])."'";
+        $conds = array();
+        $comptoir = false;
+        
+        if ( $_REQUEST["debut"] )
+                $conds[] = "cpt_rechargements.date_rech >= '".date("Y-m-d H:i:s",$_REQUEST["debut"])."'";
+        
+        if ( $_REQUEST["fin"] )
+                $conds[] = "cpt_rechargements.date_rech <= '".date("Y-m-d H:i:s",$_REQUEST["fin"])."'";
+        
+        if ( isset($comptoirs[$_REQUEST["id_comptoir"]]) && $_REQUEST["id_comptoir"] )
+        {
+          //si bureau ae, on compte aussi le comptoire machine
+          if($_REQUEST["id_comptoir"]==6)
+            $conds[] = "cpt_rechargements.id_comptoir='8'";
+          $conds[] = "cpt_rechargements.id_comptoir='".intval($_REQUEST["id_comptoir"])."'";
+        }
 
-	if ( $_REQUEST["banque_rech"] )
-		$conds[] = "cpt_rechargements.banque_rech = '".intval($_REQUEST["banque_rech"])."'";
-		
-	if ( $_REQUEST["type_paiement_rech"] != -1 )
-		$conds[] = "cpt_rechargements.type_paiement_rech = '".intval($_REQUEST["type_paiement_rech"])."'";
-		
+        if ( $_REQUEST["banque_rech"] )
+                $conds[] = "cpt_rechargements.banque_rech = '".intval($_REQUEST["banque_rech"])."'";
+                
+        if ( $_REQUEST["type_paiement_rech"] != -1 )
+                $conds[] = "cpt_rechargements.type_paiement_rech = '".intval($_REQUEST["type_paiement_rech"])."'";
+                
 
-	if ( count($conds) )
-	{
-		
-	$req = new requete($site->db, "SELECT " .
-			"COUNT(`cpt_rechargements`.`id_rechargement`), " .		
-			"SUM(`cpt_rechargements`.`montant_rech`) " .
-			"FROM `cpt_rechargements` " .
-			"INNER JOIN `asso` ON `asso`.`id_asso` =`cpt_rechargements`.`id_assocpt` " .
-			"INNER JOIN `utilisateurs` AS `vendeur` ON `cpt_rechargements`.`id_utilisateur_operateur` =`vendeur`.`id_utilisateur` " .	
-			"INNER JOIN `utilisateurs` AS `client` ON `cpt_rechargements`.`id_utilisateur` =`client`.`id_utilisateur` " .
-			"INNER JOIN `cpt_comptoir` ON `cpt_rechargements`.`id_comptoir` =`cpt_comptoir`.`id_comptoir` " .
-			"WHERE " .implode(" AND ",$conds));		
-		
-	list($ln,$sum) = $req->get_row();
-		
-		
-	$cts->add_title(2,"Somme");
-	$cts->add_paragraph("$ln rechargements pour ".($sum/100)." Euros");
-		
-	if ( $ln < 4000 )
-	{
+        if ( count($conds) )
+        {
+                
+        $req = new requete($site->db, "SELECT " .
+                        "COUNT(`cpt_rechargements`.`id_rechargement`), " .                
+                        "SUM(`cpt_rechargements`.`montant_rech`) " .
+                        "FROM `cpt_rechargements` " .
+                        "INNER JOIN `asso` ON `asso`.`id_asso` =`cpt_rechargements`.`id_assocpt` " .
+                        "INNER JOIN `utilisateurs` AS `vendeur` ON `cpt_rechargements`.`id_utilisateur_operateur` =`vendeur`.`id_utilisateur` " .        
+                        "INNER JOIN `utilisateurs` AS `client` ON `cpt_rechargements`.`id_utilisateur` =`client`.`id_utilisateur` " .
+                        "INNER JOIN `cpt_comptoir` ON `cpt_rechargements`.`id_comptoir` =`cpt_comptoir`.`id_comptoir` " .
+                        "WHERE " .implode(" AND ",$conds));                
+                
+        list($ln,$sum) = $req->get_row();
+                
+                
+        $cts->add_title(2,"Somme");
+        $cts->add_paragraph("$ln rechargements pour ".($sum/100)." Euros");
+                
+        if ( $ln < 4000 )
+        {
 
-	$req = new requete($site->db, "SELECT " .
-			"`cpt_rechargements`.`id_rechargement`, " .
-			"`cpt_rechargements`.`date_rech`, " .
-			"`cpt_rechargements`.`banque_rech`, " .
-			"`cpt_rechargements`.`type_paiement_rech`, " .
-			"`asso`.`id_asso`, " .
-			"`asso`.`nom_asso`, " .
-			"CONCAT(`client`.`prenom_utl`,' ',`client`.`nom_utl`) as `nom_utilisateur_client`, " .
-			"`client`.`id_utilisateur` AS `id_utilisateur_client`, " .
-			"CONCAT(`vendeur`.`prenom_utl`,' ',`vendeur`.`nom_utl`) as `nom_utilisateur_vendeur`, " .
-			"`vendeur`.`id_utilisateur` AS `id_utilisateur_vendeur`, " .			
-			"`cpt_rechargements`.`montant_rech`/100 AS `total`," .
-			"`cpt_comptoir`.`id_comptoir`, " .
-			"`cpt_comptoir`.`nom_cpt`" .
-			"FROM `cpt_rechargements` " .
-			"INNER JOIN `asso` ON `asso`.`id_asso` =`cpt_rechargements`.`id_assocpt` " .
-			"INNER JOIN `utilisateurs` AS `vendeur` ON `cpt_rechargements`.`id_utilisateur_operateur` =`vendeur`.`id_utilisateur` " .	
-			"INNER JOIN `utilisateurs` AS `client` ON `cpt_rechargements`.`id_utilisateur` =`client`.`id_utilisateur` " .
-			"INNER JOIN `cpt_comptoir` ON `cpt_rechargements`.`id_comptoir` =`cpt_comptoir`.`id_comptoir` " .
-			"WHERE " .implode(" AND ",$conds).
-			"ORDER BY `cpt_rechargements`.`date_rech` DESC");
-		
-	$cts->add(new sqltable(
-		"listresp", 
-		"Listing", $req, "comptarech.php", 
-		"id_facture", 
-		array(
-			"id_rechargement"=>"Rech.",
-			"date_rech"=>"Date",
-			"nom_cpt"=>"Lieu",
-			"type_paiement_rech"=>"Type",
-			"banque_rech"=>"Banque",
-			"nom_utilisateur_vendeur"=>"Operateur",
-			"nom_utilisateur_client"=>"Client",
-			"nom_asso"=>"Asso.",
-			"total"=>"Som."), 
-		/*$site->user->is_in_group("gestion_ae")?array("delete"=>"Annuler le rechargement"):*/array(), 
-		array(),
-		array( "type_paiement_rech"=>$TypesPaiementsFull,"banque_rech"=> $Banques)
-		),true);
-	}
+        $req = new requete($site->db, "SELECT " .
+                        "`cpt_rechargements`.`id_rechargement`, " .
+                        "`cpt_rechargements`.`date_rech`, " .
+                        "`cpt_rechargements`.`banque_rech`, " .
+                        "`cpt_rechargements`.`type_paiement_rech`, " .
+                        "`asso`.`id_asso`, " .
+                        "`asso`.`nom_asso`, " .
+                        "CONCAT(`client`.`prenom_utl`,' ',`client`.`nom_utl`) as `nom_utilisateur_client`, " .
+                        "`client`.`id_utilisateur` AS `id_utilisateur_client`, " .
+                        "CONCAT(`vendeur`.`prenom_utl`,' ',`vendeur`.`nom_utl`) as `nom_utilisateur_vendeur`, " .
+                        "`vendeur`.`id_utilisateur` AS `id_utilisateur_vendeur`, " .                        
+                        "`cpt_rechargements`.`montant_rech`/100 AS `total`," .
+                        "`cpt_comptoir`.`id_comptoir`, " .
+                        "`cpt_comptoir`.`nom_cpt`" .
+                        "FROM `cpt_rechargements` " .
+                        "INNER JOIN `asso` ON `asso`.`id_asso` =`cpt_rechargements`.`id_assocpt` " .
+                        "INNER JOIN `utilisateurs` AS `vendeur` ON `cpt_rechargements`.`id_utilisateur_operateur` =`vendeur`.`id_utilisateur` " .        
+                        "INNER JOIN `utilisateurs` AS `client` ON `cpt_rechargements`.`id_utilisateur` =`client`.`id_utilisateur` " .
+                        "INNER JOIN `cpt_comptoir` ON `cpt_rechargements`.`id_comptoir` =`cpt_comptoir`.`id_comptoir` " .
+                        "WHERE " .implode(" AND ",$conds).
+                        "ORDER BY `cpt_rechargements`.`date_rech` DESC");
+                
+        $cts->add(new sqltable(
+                "listresp", 
+                "Listing", $req, "comptarech.php", 
+                "id_facture", 
+                array(
+                        "id_rechargement"=>"Rech.",
+                        "date_rech"=>"Date",
+                        "nom_cpt"=>"Lieu",
+                        "type_paiement_rech"=>"Type",
+                        "banque_rech"=>"Banque",
+                        "nom_utilisateur_vendeur"=>"Operateur",
+                        "nom_utilisateur_client"=>"Client",
+                        "nom_asso"=>"Asso.",
+                        "total"=>"Som."), 
+                /*$site->user->is_in_group("gestion_ae")?array("delete"=>"Annuler le rechargement"):*/array(), 
+                array(),
+                array( "type_paiement_rech"=>$TypesPaiementsFull,"banque_rech"=> $Banques)
+                ),true);
+        }
   }
 }
 
