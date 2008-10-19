ff0e7a59dd16609f684e08354cfd2848bba91670
BenC <benc@troll>
2008-10-19 18:24:32 +0000

    debug

--- include/cts/sas.inc.php
+++ include/cts/sas.inc.php
@@ -104,8 +104,8 @@ class sascategory extends contents
       {
         $link = $page."?meta_id_catph=".$cat->id."&amp;id_catph=".$row['id_catph'];
         $scat->_load($row);
-        echo "Mode : ".$scat->meta_mode;
-        if ( $scat->is_right($user,DROIT_ECRITURE) && $scat->meta_mode != CATPH_MODE_META_ASSO )
+        echo "Mode : ".$cat->meta_mode;
+        if ( $scat->is_right($user,DROIT_ECRITURE) && $cat->meta_mode != CATPH_MODE_META_ASSO )
           $acts = array("delete","edit","cut");
       }
       else
