56c3273b34351e72365faad350459c89f22158e5
gliss <gliss@troll>
2008-11-14 11:37:06 +0000

    tip

--- temp/pedag_tri.php
+++ temp/pedag_tri.php
@@ -8,20 +8,18 @@ function c($n, $m){
   preg_match("/^([AP])([0-9]{4})$/", $n[3], $s1);
   preg_match("/^([AP])([0-9]{4})$/", $m[3], $s2);
 
-print_r($s1);
-print_r($s2);
-/*
-  $s1= $n[3][0];
-  $y1= intval(array_slice($n[3], 1));
-  $s2= $m[3][0];
-  $y2= intval(array_slice($m[3], 1));
-
-echo $n, $s1, $y1, $m, $s2, $y2, "\n";
-*/
-  if($n[1] < $m[1])
+	/* comparaisons sur les annees, puis si egalite sur les A/P */
+  if($s1[2] < $s2[2])
 		return -1;
-  else
+  else if ($s1[2] > $s2[2])
     return 1;
+	else{
+    /* si < : A pour 1 et P pour 2 */
+    if($s1[1] < $s2[1])
+		  return 1;
+	  else 
+		  return -1;
+  }
 }
 
 
