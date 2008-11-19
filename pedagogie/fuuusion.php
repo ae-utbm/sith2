a63e4b966150b4a96565616453ff5a8ae4081fa3
gliss <gliss@troll>
2008-11-19 10:57:15 +0000

    fuuuuuusion

---
+++ pedagogie/fuuusion.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Import de l'ancienne base vers la nouvelle
+ */
+$topdir="../";
+require_once($topdir."include/site.inc.php");
+/* on inclu tout pour voir deja si ya pas d erreur syntaxique */
+require_once("include/pedagogie.inc.php");
+require_once("include/uv.inc.php");
+require_once("include/uv_comment.inc.php");
+require_once("include/cursus.inc.php");
+require_once("include/pedag_user.inc.php");
+require_once("include/edt.inc.php");
+
+$site = new site();
+
+
+
+$site->end_page();
+?>
