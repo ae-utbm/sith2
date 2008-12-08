8db83d52abf75772135584edd968b20de5d07e4b
Ayolo <ayolo@troll>
2008-12-08 11:50:37 +0000

    bleh

--- include/cts/sas.inc.php
+++ include/cts/sas.inc.php
@@ -237,7 +237,7 @@ class sasphoto extends contents
     }
     else
     {
-      $imgcts->add(new image($photo->id,"images.php?/".$photo->id.".diapo.jpg","sas_img",true));
+      $imgcts->add(new image($photo->id,"images.php?/".$photo->id.".diapo.jpg"),false,true,gen_uid(),"sas_img");
       $_exif="<div id=\"exif\">\n";
       if(!empty($photo->manufacturer) || !empty($photo->manufacturer))
       {
