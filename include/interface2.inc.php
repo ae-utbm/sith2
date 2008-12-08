5a6f460c395f7ff2a4e650947cbb27cff620d36d
Ayolo <ayolo@troll>
2008-12-08 13:30:23 +0000

    bleh

--- include/interface2.inc.php
+++ include/interface2.inc.php
@@ -523,10 +523,12 @@ class interfaceweb
     $this->buffer .= "</div>\n"; // /site
 
     $this->buffer .= "<div id=\"endsite\">";
+    $this->buffer .= "<div id=\"endsitelinks\">";
     $this->buffer .= "<span id=\"endspanfirst\"><a href=\"". $wwwtopdir ."article.php?name=legals\">AE UTBM</a></span>";
     $this->buffer .= "<span><a href=\"". $wwwtopdir ."article.php?name=docs:index\">Aide et documentation</a></span>";
     $this->buffer .= "<span><a href=\"". $wwwtopdir ."article.php?name=rd\">R&amp;D</a></span>";
     $this->buffer .= "<span><a href=\"". $wwwtopdir ."wiki2/?name=ae:info\">Equipe info</a></span>";
+    $this->buffer .= "</div>";// /endsitelinks
     $this->buffer .= "<div id=\"logofooter\">";
     $this->buffer .= "&nbsp;";
     $this->buffer .= "</div>";// /logofooter
