760697988a89154e258fce1d9dab8611d9cbfb69
Ayolo <ayolo@troll>
2008-12-09 15:10:16 +0000

    bleh

--- include/cts/newsflow.inc.php
+++ include/cts/newsflow.inc.php
@@ -168,7 +168,7 @@ abstract class newslister extends stdcontents
       $this->puts("    var left=((12-hour)*60-min)*60-sec;\n");
       $this->puts("  else\n");
       $this->puts("    var left=((24-hour)*60-min)*60-sec;\n");
-      $this->puts("  expire.setTime(expire.getTime() + (left*1000));\n")
+      $this->puts("  expire.setTime(expire.getTime() + (left*1000));\n");
       $this->puts(" alert('bleh');");
       $this->puts(" alert(expire.toGMTString());\n");
       $this->puts("  document.cookie = name + '=1; expires='+expire.toGMTString();\n");
