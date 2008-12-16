f9f883cf06e97553a8c2e67dff85cf88e6dc658b
Ayolo <ayolo@troll>
2008-12-16 10:39:59 +0000

    bleh

--- rootplace/import_wiki.php
+++ rootplace/import_wiki.php
@@ -89,7 +89,7 @@ function process_namespace($path,$namespace,$config)
             foreach($revisions as $revision)
               $wiki->revision($lion->id,$_page,implode("",gzfile($path.$page.'.'.$revision.'.txt.gz')),'Édité le '.date('Y-m-d', $revision).' à '.date('H:i:s', $revision));
           }
-          elseif(is_null($pagename))
+          elseif(!$parent->is_valid())
             echo "bleh";
           continue;
         }
