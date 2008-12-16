f4bfd674acf0febd4d371f0340649d163f404318
Ayolo <ayolo@troll>
2008-12-16 15:14:51 +0000

    bleh

--- rootplace/import_wiki.php
+++ rootplace/import_wiki.php
@@ -78,7 +78,7 @@ function process_namespace($path,$namespace,$config)
           $_page=$config["unixname"];
           echo '<h2>page : '.$namespace.'</h2>';
           $pagename = $parent->load_or_create_parent($namespace, $lion, $config['rights'], $config['rights_id_group'], $config['rights_id_group_admin']);
-          if ( !is_null($pagename) && $parent->is_valid() && !$wiki->load_by_fullpath($parent,$namespace))
+          if ( !is_null($pagename) && $parent->is_valid() && !$wiki->load_by_fullpath($namespace))
           {
             $wiki->herit($parent);
             $parent->id_utilisateur=$site->user->id;
@@ -89,21 +89,11 @@ function process_namespace($path,$namespace,$config)
             foreach($revisions as $revision)
               $wiki->revision($lion->id,$_page,implode("",gzfile($path.$page.'.'.$revision.'.txt.gz')),'Édité le '.date('Y-m-d', $revision).' à '.date('H:i:s', $revision));
           }
-          elseif(!$parent->is_valid())
-          {
-            echo "pb de parent";
-            exit();
-          }
-          elseif($wiki->load_by_name($parent,$pagename))
-          {
-            echo "existe déja ???";
-            exit();
-          }
           continue;
         }
         echo '<h2>page : '.$namespace.':'.$page.'</h2>';
         $pagename = $parent->load_or_create_parent($namespace.':'.$page, $lion, $config['rights'], $config['rights_id_group'], $config['rights_id_group_admin']);
-        if ( !is_null($pagename) && $parent->is_valid() && !$wiki->load_by_fullpath($parent,$namespace.':'.$page) )
+        if ( !is_null($pagename) && $parent->is_valid() && !$wiki->load_by_fullpath($namespace.':'.$page) )
         {
           $wiki->herit($parent);
           $parent->id_utilisateur=$site->user->id;
