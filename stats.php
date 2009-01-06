87e3a6609c8d411799264398bd6740e06255ddaa
Ayolo <ayolo@troll>
2009-01-06 17:43:17 +0000

    bleh

--- stats.php
+++ stats.php
@@ -30,6 +30,7 @@ require_once($topdir. "include/site.inc.php");
 require_once($topdir. "include/cts/sqltable.inc.php");
 require_once($topdir. "include/cts/user.inc.php");
 require_once($topdir . "include/graph.inc.php");
+require_once($topdir."include/cts/progressbar.inc.php");
 $site = new site ();
 
 $site->start_page("none","Statistiques");
@@ -45,6 +46,7 @@ if(!$site->user->is_in_group ("gestion_ae"))
     $tabs[]=array("matmatronch","stats.php?view=matmatronch", "Matmatronch");
   if($site->user->is_asso_role ( 2, 9 ))
     $tabs[]=array("comptoirs","stats.php?view=comptoirs", "Comptoirs");
+  $tabs[]=array("electionss","stats.php?view=elections", "Élections");
 }
 else
 {
@@ -630,6 +632,52 @@ elseif ( $_REQUEST["view"] == "matmatronch" )
             ),true);
 
   $cts->add($mcts);
+}
+elseif ( $_REQUEST["view"] == "elections" )
+{
+  if (!$site->user->is_in_group ("gestion_ae"))
+    $site->error_forbidden("none","group",9);
+  $cts = new contents("Participation aux élections");
+  $req = new requete($site->db,
+         "SELECT ".
+         "  id_election".
+         ", nom_elec".
+         ", date_debut ".
+         ", date_fin ".
+         "FROM vt_election ".
+         "WHERE id_groupe=10000 ".
+         "ORDER BY date_debut, date_fin");
+  $elections = array();
+  while(list($id,$nom,$deb,$fin)=$req->get_row())
+  {
+    $req2 = new requete($site->db,
+      "SELECT ".
+      "COUNT(DISTINCT(id_utilisateur)) as nb ".
+      "FROM `ae_cotisations` ".
+      "WHERE ".
+      "`date_fin_cotis` > '".$fin."' ".
+      "AND `date_cotis` < '".$fin."' ");
+    list($cot)=$req2->get_row();
+
+    $req2 = new requete($site->db,
+      "SELECT ".
+      "COUNT(*) as nb ".
+      "FROM `vt_a_vote` ".
+      "WHERE `id_election` =".$id);
+    list($vot)=$req2->get_row();
+
+    $cts2 = new contents($nom);
+    $lst = new itemlist();
+    $lst->add("Début : ".date("d/m/Y H:i",strtotime($deb)));
+    $lst->add("Fin : ".date("d/m/Y H:i",strtotime($fin)));
+    $lst->add("Cotisants : ".$cot);
+    $lst->add("Votants : ".$vot);
+    $cts2->add($lst);
+
+    $part = round(($vot/$cot)*100,1);
+    $prog = new progressbar($part);
+    $cts2->add($prog);
+    $cts->add($cts2,true);
 }
 else
 {
