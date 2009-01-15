575c5f69ccea3a8bbfc9d0550c8cd094c8a88f05
Ayolo <ayolo@troll>
2009-01-15 11:24:29 +0000

    test

--- webdav.php
+++ webdav.php
@@ -47,7 +47,7 @@ require_once($topdir . "include/serverwebdavae.inc.php");
 require_once($topdir . "include/entities/files.inc.php");
 require_once($topdir . "include/entities/folder.inc.php");
 require_once($topdir . "include/entities/asso.inc.php");
-if(!isset($_SERVER["PATH_INFO"]))
+if(!isset($_SERVER["PATH_INFO"]) || empty($_SERVER["PATH_INFO"]))
 {
   if(isset($_REQUEST['__pathinfo__']))
     $_SERVER["PATH_INFO"]=$_REQUEST['__pathinfo__'];
