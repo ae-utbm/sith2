a98e7e7940e96c7b539200d7369fd2bec909cfd4
Ayolo <ayolo@troll>
2009-01-23 09:37:16 +0000

    bleh

--- include/pdf/carteae.inc.php
+++ include/pdf/carteae.inc.php
@@ -27,182 +27,182 @@ require_once($topdir . "include/lib/barcodefpdf.inc.php");
 
 class pdfcarteae extends FPDF
 {
-	var $img_front;
-	var $img_back;
-	var $width;
-	var $height;
-	var $xmargin;
-	var $ymargin;
-	var $pos;
-	var $npp;
-	var $npl;
+  var $img_front;
+  var $img_back;
+  var $width;
+  var $height;
+  var $xmargin;
+  var $ymargin;
+  var $pos;
+  var $npp;
+  var $npl;
 
-	function pdfcarteae()
-	{
-		global $topdir;
-
-
-		$this->FPDF();
-
-		$this->width = 80; // Largeur d'une carte
-		$this->height = 50; // Hauteur d'une carte
-		$this->xmargin = 25; // Marge X
-		$this->ymargin = 25; // Marge Y
-		$this->npp = 10; // Nombre par page
-		$this->npl=2; // Nombre par ligne
-
-		$this->img_front = $topdir."images/carteae/front-2006.png";
-		$this->img_back = $topdir."images/carteae/back-2006.png";
-
-		/* ATTENTION
-		 * - l'égalité suivante doit être respectée :
-		 * 	210=$this->npl*$this->width+2*$this->xmargin
-		 * 	sinon les cartes seront mal aligné entre le recto et le verso
-		 *
-		 * - l'égalité suivante doit être respectée :
-		 * 	290<$this->height*$this->npp/$this->npl+$this->ymargin
-		 *  sinon les cartes ne tiendrons pas dans la page
-		 */
+  function pdfcarteae()
+  {
+    global $topdir;
 
 
-		$this->pos = array (
-			"photo" => array ("x"=>4.1,"y"=>3.7,"w"=>26,8,"h"=>36,8),
-			"cbar" => array ("x"=>10.6,"y"=>3.8,"w"=>67,"h"=>25),
-			"front" =>
-				array (
-					"nom" => array ("x"=>51,"y"=>21,"w"=>27,"h"=>4.5),
-					"prenom" => array ("x"=>51,"y"=>25.5,"w"=>27,"h"=>4.5),
-					"surnom" => array ("x"=>51,"y"=>30,"w"=>27,"h"=>4.5),
-					"semestres" => array ("x"=>51,"y"=>34.5,"w"=>27,"h"=>4.5)
-				)
-			);
+    $this->FPDF();
 
-		$this->SetAutoPageBreak(false);
+    $this->width = 80; // Largeur d'une carte
+    $this->height = 50; // Hauteur d'une carte
+    $this->xmargin = 25; // Marge X
+    $this->ymargin = 25; // Marge Y
+    $this->npp = 10; // Nombre par page
+    $this->npl=2; // Nombre par ligne
 
+    $this->img_front = $topdir."images/carteae/front-2006.png";
+    $this->img_back = $topdir."images/carteae/back-2006.png";
 
-	}
-
-	function render_front ( $x, $y, $infos )
-	{
-		global $topdir;
-
-		$this->Image($this->img_front,$x,$y,$this->width,$this->height);
-
-		$src = "../var/img/matmatronch/".$infos['id'].".identity.jpg";
-
-		$this->Image($src,
-				$x+$this->pos['photo']['x'],
-				$y+$this->pos['photo']['y'],
-				$this->pos['photo']['w'],
-				$this->pos['photo']['h']);
-
-		$this->SetFont('Arial','',8);
-
-		foreach ( $this->pos['front'] as $name => $pos )
-		{
-			$this->SetXY($x+$pos['x'],$y+$pos['y']);
-			$this->Cell($pos['w'],$pos['h'],utf8_decode($infos[$name]));
-		}
-	}
-
-	function render_back ( $x, $y, $infos )
-	{
-		$this->Image($this->img_back,$x,$y,$this->width,$this->height);
-
-		$cbar = new PDF_C128AObject($this->pos['cbar']['w'], $this->pos['cbar']['h'],
-						BCS_ALIGN_CENTER | BCS_DRAW_TEXT,
-						$infos['cbar'],
-						&$this,
-						$x+$this->pos['cbar']['x'],
-						$y+$this->pos['cbar']['y']
-						);
-
-		$cbar->DrawObject(0.25);
-
-	}
-
-	function render_page ( $users )
-	{
-		$n = count($users);
-
-		$this->AddPage();
-		for($i=0;$i<$n;$i++)
-		{
-			$x = $this->xmargin+($i % $this->npl)*$this->width;
-			$y = $this->ymargin+intval($i/$this->npl)*$this->height;
-
-			$this->render_front($x,$y,$users[$i]);
-		}
-
-		$this->AddPage();
-		for($i=0;$i<$n;$i++)
-		{
-			$x = $this->xmargin+($this->npl-1-($i % $this->npl))*$this->width;
-			$y = $this->ymargin+intval($i/$this->npl)*$this->height;
-
-			$this->render_back($x,$y,$users[$i]);
-		}
-	}
-
-	function semestre ( $time )
-	{
-		$d = date("d",$time);
-		$m = date("m",$time);
-
-		if ( $m <= 2 )
-			return "A".sprintf("%02d",(date("y",$time)-1));
-
-		if ( $m > 8 )
-			return "A".date("y",$time);
-
-		return "P".date("y",$time);
-	}
+    /* ATTENTION
+     * - l'égalité suivante doit être respectée :
+     *   210=$this->npl*$this->width+2*$this->xmargin
+     *   sinon les cartes seront mal aligné entre le recto et le verso
+     *
+     * - l'égalité suivante doit être respectée :
+     *   290<$this->height*$this->npp/$this->npl+$this->ymargin
+     *  sinon les cartes ne tiendrons pas dans la page
+     */
 
 
-	function render ( $req )
-	{
-		global $topdir;
-		$i=0;
-		$users = array();
+    $this->pos = array (
+      "photo" => array ("x"=>4.1,"y"=>3.7,"w"=>26,8,"h"=>36,8),
+      "cbar" => array ("x"=>10.6,"y"=>3.8,"w"=>67,"h"=>25),
+      "front" =>
+        array (
+          "nom" => array ("x"=>51,"y"=>21,"w"=>27,"h"=>4.5),
+          "prenom" => array ("x"=>51,"y"=>25.5,"w"=>27,"h"=>4.5),
+          "surnom" => array ("x"=>51,"y"=>30,"w"=>27,"h"=>4.5),
+          "semestres" => array ("x"=>51,"y"=>34.5,"w"=>27,"h"=>4.5)
+        )
+      );
 
-		$acc=utf8_decode('ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËéèêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ');
-    		$noacc='AAAAAAaaaaaaOOOOOOooooooEEEEeeeeCcIIIIiiiiUUUUuuuuyNn';
+    $this->SetAutoPageBreak(false);
 
-		while ( $row= $req->get_row())
-		{
 
-			$users[$i]["id"] = $row["id_utilisateur"];
-			$users[$i]["nom"] = $row["nom_utl"];
-			$users[$i]["prenom"] = $row["prenom_utl"];
-			$users[$i]["surnom"] = $row["surnom_utbm"];
-			$fsem = $this->semestre(strtotime($row["date_fin_cotis"]));
-			$sem = $this->semestre(time());
+  }
 
-			if ( $fsem != $sem )
-				$users[$i]["semestres"] = $sem."-".$fsem;
-			else
-				$users[$i]["semestres"] = $fsem;
+  function render_front ( $x, $y, $infos )
+  {
+    global $topdir;
 
-			$users[$i]["cbar"] = $row["id_carte_ae"]." ".substr($row["prenom_utl"],0,6).".".substr($row["nom_utl"],0,6);
-			$users[$i]["cbar"] = utf8_encode(strtr(utf8_decode($users[$i]["cbar"]), $acc, $noacc));
-			$users[$i]["cbar"] = strtoupper(str_replace("-", "", $users[$i]["cbar"]));
+    $this->Image($this->img_front,$x,$y,$this->width,$this->height);
 
-			$i++;
+    $src = "../var/img/matmatronch/".$infos['id'].".identity.jpg";
 
-			if ( $i == $this->npp )
-			{
-				$this->render_page($users);
+    $this->Image($src,
+        $x+$this->pos['photo']['x'],
+        $y+$this->pos['photo']['y'],
+        $this->pos['photo']['w'],
+        $this->pos['photo']['h']);
 
-				$users = array();
-				$i = 0;
-			}
+    $this->SetFont('Arial','',8);
 
-		}
+    foreach ( $this->pos['front'] as $name => $pos )
+    {
+      $this->SetXY($x+$pos['x'],$y+$pos['y']);
+      $this->Cell($pos['w'],$pos['h'],utf8_decode($infos[$name]));
+    }
+  }
 
-		if ( $i != 0 )
-			$this->render_page($users);
+  function render_back ( $x, $y, $infos )
+  {
+    $this->Image($this->img_back,$x,$y,$this->width,$this->height);
 
-	}
+    $cbar = new PDF_C128AObject($this->pos['cbar']['w'], $this->pos['cbar']['h'],
+            BCS_ALIGN_CENTER | BCS_DRAW_TEXT,
+            $infos['cbar'],
+            &$this,
+            $x+$this->pos['cbar']['x'],
+            $y+$this->pos['cbar']['y']
+            );
+
+    $cbar->DrawObject(0.25);
+
+  }
+
+  function render_page ( $users )
+  {
+    $n = count($users);
+
+    $this->AddPage();
+    for($i=0;$i<$n;$i++)
+    {
+      $x = $this->xmargin+($i % $this->npl)*$this->width;
+      $y = $this->ymargin+intval($i/$this->npl)*$this->height;
+
+      $this->render_front($x,$y,$users[$i]);
+    }
+
+    $this->AddPage();
+    for($i=0;$i<$n;$i++)
+    {
+      $x = $this->xmargin+($this->npl-1-($i % $this->npl))*$this->width;
+      $y = $this->ymargin+intval($i/$this->npl)*$this->height;
+
+      $this->render_back($x,$y,$users[$i]);
+    }
+  }
+
+  function semestre ( $time )
+  {
+    $d = date("d",$time);
+    $m = date("m",$time);
+
+    if ( $m <= 2 )
+      return "A".sprintf("%02d",(date("y",$time)-1));
+
+    if ( $m > 8 )
+      return "A".date("y",$time);
+
+    return "P".date("y",$time);
+  }
+
+
+  function render ( $req )
+  {
+    global $topdir;
+    $i=0;
+    $users = array();
+
+    $acc=utf8_decode('ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËéèêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ');
+        $noacc='AAAAAAaaaaaaOOOOOOooooooEEEEeeeeCcIIIIiiiiUUUUuuuuyNn';
+
+    while ( $row= $req->get_row())
+    {
+
+      $users[$i]["id"] = $row["id_utilisateur"];
+      $users[$i]["nom"] = $row["nom_utl"];
+      $users[$i]["prenom"] = $row["prenom_utl"];
+      $users[$i]["surnom"] = $row["surnom_utbm"];
+      $fsem = $this->semestre(strtotime($row["date_fin_cotis"]));
+      $sem = $this->semestre(time());
+
+      if ( $fsem != $sem )
+        $users[$i]["semestres"] = $sem."-".$fsem;
+      else
+        $users[$i]["semestres"] = $fsem;
+
+      $users[$i]["cbar"] = $row["id_carte_ae"]." ".substr($row["prenom_utl"],0,6).".".substr($row["nom_utl"],0,6);
+      $users[$i]["cbar"] = utf8_encode(strtr(utf8_decode($users[$i]["cbar"]), $acc, $noacc));
+      $users[$i]["cbar"] = strtoupper(str_replace("-", "", $users[$i]["cbar"]));
+
+      $i++;
+
+      if ( $i == $this->npp )
+      {
+        $this->render_page($users);
+
+        $users = array();
+        $i = 0;
+      }
+
+    }
+
+    if ( $i != 0 )
+      $this->render_page($users);
+
+  }
 
 }
 
