fdaa960b132fae5adbd8ae0554e1082399b68c0b
Ayolo <ayolo@troll>
2009-01-24 11:13:15 +0000

    bleh

--- include/site.inc.php
+++ include/site.inc.php
@@ -206,7 +206,8 @@ class site extends interfaceweb
               "expire_sess" => $expire
               ));
     $domain = ($_SERVER['HTTP_HOST'] != 'localhost' && $_SERVER['HTTP_HOST'] != '127.0.0.1') ? $_SERVER['HTTP_HOST'] : false;
-//    if($_SERVER['HTTP_HOST']==
+    if($_SERVER['SCRIPT_URI']='http://ae.utbm.fr/boutique-utbm/connect.php')
+      $domain='.utbm.fr';
     setcookie ("AE2_SESS_ID", $sid, time() + 31536000, "/", $domain, 0);
 
     $this->user->visite();
