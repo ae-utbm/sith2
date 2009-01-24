bded3ebc4ac158981df2cdd19a63d0b2e9e2407f
Ayolo <ayolo@troll>
2009-01-24 09:20:02 +0000

    bleh

--- boutique-utbm/include/boutique.inc.php
+++ boutique-utbm/include/boutique.inc.php
@@ -58,8 +58,7 @@ class boutique extends site
   {
    global $topdir;
     $this->site();
-print_r($_SERVER["SCRIPT_NAME"]);
-    if(!isset($_REQUEST["domain"]) || $_SERVER["SCRIPT_NAME"]!='connect.php')
+    if(!isset($_REQUEST["domain"]) || $_SERVER["SCRIPT_NAME"]!='/boutique-utbm/connect.php')
       $this->allow_only_logged_users();
     if($this->user->type != "srv" && !$this->user->is_in_group("gestion_ae") && !$this->user->is_in_group("adminboutiqueutbm"))
       $this->error_forbidden();
