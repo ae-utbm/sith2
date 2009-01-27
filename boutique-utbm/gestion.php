ce034b48d1be0c97b5e3204fa409ce1a298f28ef
Ayolo <ayolo@troll>
2009-01-27 16:38:08 +0000

    bleh

--- boutique-utbm/gestion.php
+++ boutique-utbm/gestion.php
@@ -89,7 +89,7 @@ elseif( $_REQUEST["page"] == "factures" )
          "IF(f.id_utilisateur=0, CONCAT(u.prenom_utl,' ',u.nom_utl), CONCAT(f.prenom,' ',f.nom)) AS nom_utilisateur ".
          ", IF(f.id_utilisateur=0, -1, f.id_utilisateur) ".
          ", f.date_facture ".
-         ", IF(f.mode_paiement='UT', 'Boutique', IF(f.mode_paiement='CH','Chèque','Liquide')) AS mode ".
+         ", IF(f.mode_paiement='UT', 'Boutique', IF(f.mode_paiement='CH','Chèque','Espèces')) AS mode ".
          ", f.id_facture ".
          ", IF(f.ready=1,IF(f.etat_facture=1,'à retirer','retiré'),'en préparation') AS etat ".
          "FROM boutiqueut_debitfacture f ".
