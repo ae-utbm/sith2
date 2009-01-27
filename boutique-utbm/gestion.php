9b6e599f253f955c77ef2b714eeaeae5c9258501
Ayolo <ayolo@troll>
2009-01-27 16:16:35 +0000

    bleh

--- boutique-utbm/gestion.php
+++ boutique-utbm/gestion.php
@@ -87,11 +87,11 @@ elseif( $_REQUEST["page"] == "factures" )
   $req = new requete($site->db,
          "SELECT ".
          "IF(f.id_utilisateur=0, CONCAT(u.prenom_utl,' ',u.nom_utl), CONCAT(f.prenom,' ',f.nom)) AS nom_utilisateur ".
-         ", IF(f.id_utilisateur=0, -1, f.id_utilisateur ".
+         ", IF(f.id_utilisateur=0, -1, f.id_utilisateur) ".
          ", f.date_facture ".
          ", IF(f.mode_paiement='UT', 'Boutique', IF(f.mode_paiement='CH','Chèque','Liquide')) AS mode ".
          ", f.id_facture ".
-         ", IF(f.ready=1,IF(f.etat=1,'à retirer','retiré'),'en préparation') AS avancement ".
+         ", IF(f.ready=1,IF(f.etat_facture=1,'à retirer','retiré'),'en préparation') AS etat ".
          "FROM boutiqueut_debitfacture f ".
          "LEFT JOIN utilisateurs u USING(id_utilisateur) ".
          "ORDER BY f.id_facture DESC",1);
@@ -101,7 +101,7 @@ elseif( $_REQUEST["page"] == "factures" )
          $req,
          "admin_gen_fact.php",
          "id_facture",
-         array('date_facture'=>'Date',"nom_utilisateur" => "Utilisateur","mode"=>"Mode de paiement","avancement"=>"État"),
+         array('date_facture'=>'Date',"nom_utilisateur" => "Utilisateur","mode"=>"Mode de paiement","etat"=>"État"),
          array('info'=>'Détail'),
          array(),
          array()),
