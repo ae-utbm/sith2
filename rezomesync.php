0bd55521c9b34dd791f29723f311b5c06461eb73
Ayolo <ayolo@troll>
2009-02-09 13:42:28 +0000

    bleh

--- rezomesync.php
+++ rezomesync.php
@@ -31,11 +31,8 @@ function error($apikey)
 {
   $ip = explode(".", $_SERVER["REMOTE_ADDR"]);
 
-  if ( !$GLOBALS["is_using_ssl"] )
-  {
-    if($ip[0] != 192 || $ip[1] != 168 || $ip[2] != 2)
-      return "notAllowed";
-  }
+  //if($ip[0] != 192 || $ip[1] != 168 || $ip[2] != 2)
+  //  return "notAllowed";
   /*if ( $_SERVER["REMOTE_ADDR"] != "127.0.1.1" )
     return "httpsRequired";*/
 
