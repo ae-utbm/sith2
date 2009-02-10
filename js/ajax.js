80803945fdf91a702435832712c4878c6bf9071c
Ayolo <ayolo@troll>
2009-02-10 01:44:42 +0000

    bleh

--- js/ajax.js
+++ js/ajax.js
@@ -102,8 +102,7 @@ function evalCommand( page, data )
   {
     if (XhrObj.readyState == 4 && XhrObj.status == 200)
     {
-      eval(XhrObj.responseText);
-      alert(XhrObj.responseText);
+      alert(eval(XhrObj.responseText));
     }
   }    
 
