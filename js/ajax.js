5a8e340780e2b6b9537611e784868992579931d4
Ayolo <ayolo@troll>
2009-02-10 00:12:08 +0000

    bleh

--- js/ajax.js
+++ js/ajax.js
@@ -354,7 +354,7 @@ function fsfield_keyup ( event, topdir, field, myclass, constraints )
   if ( !obj ) return false;
     
   fsfield_sequence[field] = fsfield_sequence[field]+1;
-  alert(typeof(constraints));
+  alert(constraints.length);
   if(typeof(constraints) == 'object' && constraints.length)
   {
     alert('bleh');
