df6d91feed80a3404ed9ca2305b74340b2b84f2f
gliss <gliss@troll>
2009-02-26 14:09:15 +0000

    bleh

---
+++ pedagogie/pedagogie.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2008
+ * - Manuel Vonthron  <manuel DOT vonthron AT acadis DOT org>
+ *
+ * Ce fichier fait partie du site de l'Association des Étudiants de
+ * l'UTBM, http://ae.utbm.fr/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+var edt = {
+  add_uv_seance: function(id, type){
+    alert('ajout de type '+type+' pour l\'UV '+id);
+  },
+
+  disp_freq_choice: function(elemid, freq, uvid, type){
+    if(freq != 1) return;
+    
+    e = document.getElementById(elemid);
+    if(!e)  return;
+    
+    e.innerHTML += "<select name=\"_"+uvid+"_freq_"+type+"_\">  \
+                      <option value=\"A\">Semaine A</option> \
+                      <option value=\"B\">Semaine B</option> \
+                    </select>";
+  }
+}
