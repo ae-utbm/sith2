32a23b9ea9b065f68dec15473978c5a70ecd00d1
Ayolo <ayolo@troll>
2009-02-26 07:35:39 +0000

    bleh

--- boutique-utbm/graph.php
+++ boutique-utbm/graph.php
@@ -21,7 +21,6 @@
  * 02111-1307, USA.
  */
 $topdir="../";
-require_once($topdir."include/cts/sqltable.inc.php");
 require_once($topdir."include/graph.inc.php");
 $site = new boutique();
 if(!$site->user->is_in_group("gestion_ae") && !$site->user->is_in_group("adminboutiqueutbm"))
