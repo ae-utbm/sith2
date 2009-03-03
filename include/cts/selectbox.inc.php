79ccec0ab4bf306272918e934f96058d297574fc
gliss <gliss@troll>
2009-03-03 14:25:14 +0000

    have a fix

--- include/cts/selectbox.inc.php
+++ include/cts/selectbox.inc.php
@@ -56,7 +56,7 @@ class selectbox extends form
     $this->height = $height;
 
     $this->add_selectbox();
-    $this->set_event("onsubmit", "$this->sb_name.select_all($this->sb_name.sel_to)");
+    $this->set_event("onsubmit", "$this->sb_name.select_all($this->sb_name.to)");
   }
 
   private function add_selectbox(){
@@ -69,15 +69,15 @@ class selectbox extends form
     $this->buffer .= "<select name=\"$this->sel_from\" id=\"$this->sel_from\" multiple=\"multiple\">\n";
     foreach($this->values as $key => $value)
       $this->buffer .= "  <option value=\"".$key."\" "
-                        ."ondblclick=\"$this->sb_name.move($this->sb_name.sel_from, $this->sb_name.sel_to);\">"
+                        ."ondblclick=\"$this->sb_name.move($this->sb_name.sel_from, $this->sb_name.to);\">"
                         .$value."</option>\n";
     $this->buffer .= "</select>\n";
     $this->buffer .= "</div>\n";
 
     /* actions */
     $this->buffer .= "<ul class=\"selectbox_actions\">";
-    $this->buffer .= "  <li class=\"ajouter\" onclick=\"javascript:$this->sb_name.move($this->sb_name.sel_from, $this->sb_name.sel_to);\">&nbsp;</li>";
-    $this->buffer .= "  <li class=\"enlever\" onclick=\"javascript:$this->sb_name.move($this->sb_name.sel_to, $this->sb_name.sel_from);\">&nbsp;</li>";
+    $this->buffer .= "  <li class=\"ajouter\" onclick=\"javascript:$this->sb_name.move($this->sb_name.sel_from, $this->sb_name.to);\">&nbsp;</li>";
+    $this->buffer .= "  <li class=\"enlever\" onclick=\"javascript:$this->sb_name.move($this->sb_name.to, $this->sb_name.sel_from);\">&nbsp;</li>";
     $this->buffer .= "</ul>";
 
     /* div to */
@@ -88,7 +88,7 @@ class selectbox extends form
     if(!empty($this->right_values))
       foreach($this->right_values as $key => $value)
         $this->buffer .= "  <option value=\"".$key."\" "
-                          ."ondblclick=\"$this->sb_name.move($this->sb_name.sel_to, $this->sb_name.sel_from);\">"
+                          ."ondblclick=\"$this->sb_name.move($this->sb_name.to, $this->sb_name.sel_from);\">"
                           .$value."</option>\n";
     $this->buffer .= "</select>\n";
     $this->buffer .= "</div>\n";
@@ -96,7 +96,7 @@ class selectbox extends form
     $this->buffer .= "<div class=\"clearboth\"></div>\n";
 
     $this->buffer .= "</div>\n";
-    $this->buffer .= "<script type=\"text/javascript\">\nwindow.onload = function(e) {\n  var $this->sb_name = new select_box('$this->sel_from', '$this->sel_to'); /* select_box.sel_from = document.getElementById('".$this->sel_from."');\n  select_box.sel_to = document.getElementById('".$this->sel_to."'); */\n};\n</script>\n";
+    $this->buffer .= "<script type=\"text/javascript\">\n  var $this->sb_name = new select_box(document.getElementById('$this->sel_from'), document.getElementById('$this->sel_to'));\n};\n</script>\n";
 
     $this->buffer .= "<p></p>";
   }
@@ -109,7 +109,7 @@ class selectbox extends form
 
     $html .= "<form action=\"$this->action\" method=\"".strtolower($this->method)."\"".
               " name=\"".$this->name."\" id=\"".$this->name."\"".
-              " onsubmit=\"$this->sb_name.select_all($this->sb_name.sel_to)\">\n";
+              " onsubmit=\"$this->sb_name.select_all($this->sb_name.to)\">\n";
 
     foreach ( $this->hiddens as $key => $value )
       $html .= "<input type=\"hidden\" name=\"$key\" value=\"$value\" />\n";
