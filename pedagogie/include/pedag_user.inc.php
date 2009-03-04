2d87e745e705f4cf3efe389c238835a8d8e27f72
gliss <gliss@troll>
2009-03-04 21:33:10 +0000

    wip

--- pedagogie/include/pedag_user.inc.php
+++ pedagogie/include/pedag_user.inc.php
@@ -80,8 +80,8 @@ class pedag_user extends utilisateur{
   }
 
   public function join_uv_group($id_group, $semaine=null){
-    $sql = new insert($this->dbrw, "pedag_groupe_utl", array("id_utilisateur"=>$this->id, "id_groupe"=>$id_group, "semaine"=>$semaine), true);
-    print_r($sql);
+    $sql = "- join_uv_group("$this->id.", ".$id_group.", ".$semaine.")";//new insert($this->dbrw, "pedag_groupe_utl", array("id_utilisateur"=>$this->id, "id_groupe"=>$id_group, "semaine"=>$semaine), true);
+    echo($sql."\n");
     return $sql->is_success();
   }
 
