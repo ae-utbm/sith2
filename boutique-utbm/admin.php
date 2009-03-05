ba1cf00a67e2eb182a8e7ec044223c49272b0fff
Ayolo <ayolo@troll>
2009-03-05 17:20:56 +0000

    bleh

--- boutique-utbm/admin.php
+++ boutique-utbm/admin.php
@@ -133,7 +133,7 @@ else if ( $_REQUEST["action"] == "upproduit" && ($produit->id > 0) && ($typeprod
   $stock_global = -1;
   $file->load_by_id($_REQUEST["id_file"]);
   $produit_parent->load_by_id($_REQUEST["id_produit_parent"]);
-  print_r($_REQUEST);
+  print_r($produit_parent->id);
   if ( $_REQUEST["stock"] == "lim" )
     $stock_global = $_REQUEST["stock_value"];
   $produit->modifier ($typeprod->id,
