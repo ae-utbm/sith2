939218b2841b7406bae7f752b9b3916101b50607
Ayolo <ayolo@troll>
2009-03-11 23:47:29 +0000

    bleh

--- temp/repair_pedag.php
+++ temp/repair_pedag.php
@@ -8,7 +8,7 @@ LEFT JOIN `pedag_groupe` g USING(id_groupe)
  WHERE g.id_groupe IS NULL');
 while(list($grp)=$req->get_row())
 {
-  $req2=new requete($site->db,
+  $req2=new requete($site->dbrw,
 'DELETE FROM `pedag_groupe_utl`
 WHERE id_groupe=\''.$grp.'\'');
 }
