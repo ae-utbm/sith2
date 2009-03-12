44b7c64c7e0ce95155c9c40f11d8cba0897ea278
Ayolo <ayolo@troll>
2009-03-12 12:21:42 +0000

    bleh

---
+++ temp/asso_mail.php
@@ -0,0 +1,10 @@
+<?php
+$topdir = "../";
+require_once($topdir. "include/site.inc.php");
+$site = new site ();
+$req = new requete($site->db,'SELECT `nom_asso`,`email_asso` FROM `asso`');
+echo "<pre>";
+while(list($asso,$email)=$req->get_row())
+  echo $asso.';'.$email."\n";
+echo "</pre">;
+?>
