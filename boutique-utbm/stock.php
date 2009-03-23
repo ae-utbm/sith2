1f7d2deb0da0b45a36dfacf893c52897efa602f3
Ayolo <ayolo@troll>
2009-03-23 10:37:59 +0000

    bleh

--- boutique-utbm/stock.php
+++ boutique-utbm/stock.php
@@ -110,6 +110,7 @@ $cts->add(new sqltable('stock','Stock',$lst,'admin.php', 'id_produit',array('nom
 
 if(!isset($_REQUEST['date']))
 {
+  $fdate = date("d/m/Y");
   $req = new requete($site->db,
                      'SELECT '.
                      'IF(mode_paiement=\'UT\', IF(u.type_utl=\'srv\',\'Facture interne\',\'À régler\'), IF(mode_paiement=\'CH\',\'Chèque\',\'Espèce\')) AS mode '.
@@ -117,6 +118,8 @@ if(!isset($_REQUEST['date']))
                      'FROM boutiqueut_debitfacture '.
                      "LEFT JOIN utilisateurs u USING(id_utilisateur) ".
                      'WHERE ready=1 '.
+                     'AND date_facture > \''.$date.'\' 00:00:00 '.
+                     'AND date_facture < \''.$date.'\' 23:59:59'.
                      'GROUP BY mode_paiement');
   if($req->lines>0)
   {
