4a45e7ff0e01fedf648a5270bfa6318a92cb81f3
Ayolo <ayolo@troll>
2009-03-29 10:12:34 +0000

    bleh

--- aecms/index.php
+++ aecms/index.php
@@ -135,7 +135,7 @@ if ( !$page->is_right($site->user,DROIT_LECTURE) )
   if(!$site->user->is_valid())
     $site->allow_only_logged_users();
   if(!$page->is_right($site->user,DROIT_LECTURE))
-   exit();
+   print_r($site->user);
   if($site->user->is_valid() && !$page->is_right($site->user,DROIT_LECTURE))
   {
     $site->start_page ( $section, "Erreur" );
