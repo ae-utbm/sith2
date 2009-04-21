ce5973de51590337b4ae7834b09154e72e07e3c7
Ayolo <ayolo@troll>
2009-04-21 10:56:26 +0000

    bleh

--- include/interface.inc.php
+++ include/interface.inc.php
@@ -286,6 +286,7 @@ class interfaceweb
     if(!defined('NOTAE'))
     {
       $important=$this->get_param('box.Important');
+print_r($important);
       if(!empty($important) && $important!="")
       {
         $this->buffer .= "<div class=\"box\" id=\"important\"><div class=\"body\">\n";
