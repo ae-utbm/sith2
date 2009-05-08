598c7d689963825712cccd370622ebfc82999fa4
Ayolo <ayolo@troll>
2009-05-08 17:30:57 +0000

    bleh

--- aecms/blog.php
+++ aecms/blog.php
@@ -26,10 +26,9 @@
 
 require_once("include/site.inc.php");
 require_once("include/blog.inc.php");
-if( $site->is_user_admin() )
-  $blog = new blog($site->db,$site->dbrw);
-else
-  $blog = new blog($site->db);
+
+$blog = new blog($site->db,$site->dbrw);
+
 if(defined('CMS_ALTERNATE'))
   $blog->load($site->asso,CMS_ALTERNATE);
 else
@@ -39,9 +38,20 @@ if(!$blog->is_valid())
   header("Location: ".$site->pubUrl);
   exit();
 }
+
 $site->start_page ( CMS_PREFIX."blog", "Blog" );
+$cts = new contents("un Blog averti en vaut deux...");
 if ( $blog->is_writer($site->user) )
 {
+  $tabs = array(
+          array("","blog.php","Le blog"),
+          array("blogueur","blog.php?view=bloguer", "Espache blogueur"));
+  if( $site->is_user_admin() )
+    $tabs[]=array("admin","blog.php?view=admin","Administration");
+
+
+  $cts->add(new tabshead($tabs,$_REQUEST["view"]));
+/*
   $admin = array(0=>CMS_PREFIX."blog",
                  1=>"blog.php",
                  2=>"Blog",
@@ -57,11 +67,13 @@ if ( $blog->is_writer($site->user) )
   }
   if(isset($admin))
     $site->tab_array[] = $admin;
+*/
 }
 
 if(isset($_REQUEST['id_entry']))
 {
-  $site->add_contents($blog->get_cts_entry($_REQUEST['id_entry'],$site->user));
+  $cts->add($blog->get_cts_entry($_REQUEST['id_entry'],$site->user));
+  $site->add_contents($cts);
   $site->end_page();
 }
 
@@ -70,13 +82,15 @@ if(isset($_REQUEST['id_cat']) && $blog->is_cat($_REQUEST['id_cat']))
   $page=0;
   if(isset($_REQUEST['id_page']))
     $page=intval($_REQUEST['id_page']);
-  $site->add_contents($blog->get_cts_cat($_REQUEST['id_cat'],$page));
+  $cts->add($blog->get_cts_cat($_REQUEST['id_cat'],$page));
+  $site->add_contents($cts);
   $site->end_page();
 }
 $page=0;
 if(isset($_REQUEST['id_page']))
   $page=intval($_REQUEST['id_page']);
-$site->add_contents($blog->get_cts($page));
+$cts->add($blog->get_cts($page));
+$site->add_contents($cts);
 $site->end_page();
 
 ?>
