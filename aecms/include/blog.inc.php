a07c171ea05a8e3270d8f505a19a0f8d7054fa18
Ayolo <ayolo@troll>
2009-05-08 19:52:34 +0000

    bleh

--- aecms/include/blog.inc.php
+++ aecms/include/blog.inc.php
@@ -325,6 +325,8 @@ class blog extends basedb
       return false;
     if ( !$user->is_valid() )
       return false;
+    if ( $this->is_admin($user) )
+      return true;
     $req = new insert($this->dbrw,
                       "aecms_blog_writers",
                       array('id_blog'=>$this->id,
