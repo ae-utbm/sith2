7f22ea5b84b07affe78afac295f62591208ed8a8
Ayolo <ayolo@troll>
2009-05-08 21:44:39 +0000

    bleh

--- aecms/include/blog.inc.php
+++ aecms/include/blog.inc.php
@@ -398,7 +398,7 @@ class blog extends basedb
   public function add_entry(&$user,
                             $titre,
                             $intro,
-                            $content,
+                            $contenu,
                             $pub=true,
                             $idcat=null,
                             $date=null)
@@ -425,7 +425,7 @@ class blog extends basedb
                             'pub'=>$pub,
                             'titre'=>$titre,
                             'intro'=>$intro,
-                            'contenu'=>$contenu),1);
+                            'contenu'=>$contenu));
     if( !$req->is_success() )
       return false;
     return $req->get_id();
