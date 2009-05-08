fde88e97dd6d8a758ba6b74005fa84d39e30b0c0
Ayolo <ayolo@troll>
2009-05-08 18:12:49 +0000

    bleh

--- aecms/blog.php
+++ aecms/blog.php
@@ -45,29 +45,52 @@ if ( $blog->is_writer($site->user) )
 {
   $tabs = array(
           array("","blog.php","Le blog"),
-          array("blogueur","blog.php?view=bloguer", "Espache blogueur"));
+          array("blogueur","blog.php?view=bloguer", "Espace blogueur"));
   if( $site->is_user_admin() )
     $tabs[]=array("admin","blog.php?view=admin","Administration");
-
-
   $cts->add(new tabshead($tabs,$_REQUEST["view"]));
-/*
-  $admin = array(0=>CMS_PREFIX."blog",
-                 1=>"blog.php",
-                 2=>"Blog",
-                 4=>array(array('blog.php?bloguer','Bloguer')));
-  foreach($site->tab_array as $id=>$tab)
+
+
+  if( isset($_REQUEST['view']) )
   {
-    if($tab[0]==CMS_PREFIX."blog")
+    if($_REQUEST['view']=='admin')
     {
-      $site->tab_array[$id]=$admin;
-      unset($admin);
-      break;
+      /* cas simples */
+      if ( $_REQUEST["action"] == "delwriter" )
+        if(isset($_REQUEST['id_utilisateur']) )
+          $blog->del_writer(new utilisateur($qite->db,null,$_REQUEST['id_utilisateur']));
+      elseif( $_REQUEST["action"] == "addwriter" )
+        if(isset($_REQUEST['id_utilisateur']) )
+          $blog->add_writer(new utilisateur($qite->db,null,$_REQUEST['id_utilisateur']));
+      elseif ( $_REQUEST["action"] == "delcat" )
+        if(isset($_REQUEST['id_cat']) )
+          $blog->del_cat($_REQUEST['id_cat']);
+      elseif( $_REQUEST["action"] == "addcat" )
+        if(isset($_REQUEST['cat_name']) )
+          $blog->add_cat($_REQUEST['cat_name']);
+      /* cas multiples */
+      elseif ( $_REQUEST["action"] == "delwriters"
+              && is_array($_REQUEST["id_utilisateurs"])
+              && !empty($_REQUEST["id_utilisateurs"]) )
+        foreach($_REQUEST["id_utilisateurs"] as $id )
+          $blog->del_writer(new utilisateur($qite->db,null,$id));
+      elseif ( $_REQUEST["action"] == "delcats"
+              && is_array($_REQUEST["id_cats"])
+              && !empty($_REQUEST["id_cats"]) )
+        foreach($_REQUEST["id_cats"] as $id )
+          $blog->del_cat($id);
+
+      $site->add_contents($cts);
+      $site->end_page();
+      exit();
+    }
+    if($_REQUEST['view']=='bloguer')
+    {
+      $site->add_contents($cts);
+      $site->end_page();
+      exit();
     }
   }
-  if(isset($admin))
-    $site->tab_array[] = $admin;
-*/
 }
 
 if(isset($_REQUEST['id_entry']))
@@ -75,6 +98,7 @@ if(isset($_REQUEST['id_entry']))
   $cts->add($blog->get_cts_entry($_REQUEST['id_entry'],$site->user));
   $site->add_contents($cts);
   $site->end_page();
+  exit();
 }
 
 if(isset($_REQUEST['id_cat']) && $blog->is_cat($_REQUEST['id_cat']))
@@ -85,6 +109,7 @@ if(isset($_REQUEST['id_cat']) && $blog->is_cat($_REQUEST['id_cat']))
   $cts->add($blog->get_cts_cat($_REQUEST['id_cat'],$page));
   $site->add_contents($cts);
   $site->end_page();
+  exit();
 }
 $page=0;
 if(isset($_REQUEST['id_page']))
