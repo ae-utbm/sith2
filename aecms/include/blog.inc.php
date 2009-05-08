b186423b5da14e471f6dcc039f6213f2b078a3a0
Ayolo <ayolo@troll>
2009-05-08 22:33:04 +0000

    bleh

--- aecms/include/blog.inc.php
+++ aecms/include/blog.inc.php
@@ -64,7 +64,7 @@ class blogentrycts extends contents
     if( !$this->content )
       $this->buffer.= '<div class"blogentryreadmore"><a href="?id_entry='.$id.'>Lire la suite</a></div>'."\n";
     else
-      $this->buffer.= '<div class="blogentrycontent">'.doku2xhtml($this->contents).'</div>'."\n";
+      $this->buffer.= '<div class="blogentrycontent">'.doku2xhtml($this->content).'</div>'."\n";
     return $this->buffer;
   }
 }
