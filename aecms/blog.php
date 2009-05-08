239bd987db59420693f1c180c3fc42f1e992dec7
Ayolo <ayolo@troll>
2009-05-08 16:04:49 +0000

    bleh

--- aecms/blog.php
+++ aecms/blog.php
@@ -59,12 +59,11 @@ if(isset($_REQUEST['id_cat']) && $blog->is_cat($_REQUEST['id_cat']))
   $site->add_contents($blog->get_cts_cat($_REQUEST['id_cat'],$page));
   $site->end_page();
 }
-
+exit();
 $page=0;
 if(isset($_REQUEST['id_page']))
   $page=intval($_REQUEST['id_page']);
 $site->add_contents($blog->get_cts($page));
-exit();
 $site->end_page();
 
 ?>
