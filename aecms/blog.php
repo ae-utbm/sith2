b1108bc50c232a4de2d0f7849db4b2a8c813a54b
Ayolo <ayolo@troll>
2009-05-09 15:12:06 +0000

    bleh

--- aecms/blog.php
+++ aecms/blog.php
@@ -41,7 +41,7 @@ if(!$blog->is_valid())
 
 $site->start_page ( CMS_PREFIX."blog", "Blog" );
 $cts = new contents();
-$cts->cssclass='blog';
+$cts->cssclass='page blog';
 if ( $blog->is_writer($site->user) )
 {
   $tabs = array(
