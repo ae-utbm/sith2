2b42a04a95b42051ff37085f9df50513d39b7c48
Ayolo <ayolo@troll>
2009-05-09 15:05:45 +0000

    bleh

--- aecms/include/blog.inc.php
+++ aecms/include/blog.inc.php
@@ -649,8 +649,6 @@ class blog extends basedb
       return new contents('Billet non trouvé');
     list($id_entry,$utl,$date,$titre,$intro,$content,$id_cat)=$req->get_row();
     $_REQUEST['id_cat']=$id_cat;
-print_r($_REQUEST['id_cat'].'cat');
-
     $cache = new cachedcontents("aecmsblog_".$this->id."_".$id_entry);
     if ( $cache->is_cached() )
       return $cache->get_cache();
