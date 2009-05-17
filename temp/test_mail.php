bdbd868eed61a71cd57bfd55eec917290fb0448e
Ayolo <ayolo@troll>
2009-05-17 23:15:19 +0000

    bleh

--- temp/test_mail.php
+++ temp/test_mail.php
@@ -3,14 +3,12 @@ require_once('../include/lib/mailer.inc.php');
 require_once("../include/globals.inc.php");
 $mailer = new mailer('Association des Étudiants <ae@utbm.fr>',
                      '[WEEKMAIL] de test');
-$mailer->add_dest(array('simon.lopez@utbm.fr',
-                        'simon.lopez@ayolo.org'));
+$mailer->add_dest(array('simon.lopez@ayolo.org'));
 $mailer->add_dest('m.simon.lopez@gmail.com');
 $mailer->add_img('../themes/default2/images/important.png');
 $mailer->set_plain('vive le html ?');
 $html = '<html><body><table width="200px">
 <tr><td>bleh : <img src="../themes/default2/images/important.png" /></td></tr>
-<tr><td>bleh : <img src="http://ae.utbm.fr/themes/default2/images/important.png" /></td></tr>
 </table></body></html>';
 $mailer->set_html($html);
 $mailer->send();
