52f1a20246ca07b9afc48d7acc55858120d31b68
Ayolo <ayolo@troll>
2009-05-18 08:47:59 +0000

    bleh

--- temp/test_mail.php
+++ temp/test_mail.php
@@ -12,7 +12,7 @@ $mailer = new mailer('Association des Étudiants <ae@utbm.fr>',
                         'enseignants@utbm.fr',
                         'iatoss@utbm.fr',
                         'aude.petit@utbm.fr'));*/
-$mailer->add_dest('ae.info@utbm.fr, m.simon.lopez@gmail.com');
+$mailer->add_dest('ae.info@utbm.fr');
 $file = new dfile($site->db);
 $file->load_by_id(3957);
 $mailer->add_img($file);
