af03928d4e238a799fff8e61123baf156b27d8b3
Ayolo <ayolo@troll>
2009-05-26 09:28:04 +0000

    bleh

--- boutique-utbm/index.php
+++ boutique-utbm/index.php
@@ -102,7 +102,7 @@ if ($_REQUEST['act'] == "add")
   /* produit non trouve ou stock insuffisant */
   if ($ret == false)
     $add_rs = new error("Ajout",
-      "<b>Impossible d'ajouter le produit dans le panier</b>. Soit le produit ne peut être acheté, soit il est incompatible avec un produit se trouvant déjà dans le panier.");
+      "<b>Impossible d'ajouter le produit dans le panier</b>. Soit le produit ne peut être acheté, soit vous avez indiqué une quantité incorrecte.");
   /*ajout possible */
   else
   {
