51848a664a00a55ba42d44dd9ee488155626bd91
Ayolo <ayolo@troll>
2009-07-09 18:43:16 +0000

    bleh

---
+++ boutique-utbm/check.php
@@ -0,0 +1,42 @@
+<?php
+$topdir="../";
+
+require_once("include/boutique.inc.php");
+require_once($topdir . "include/cts/sqltable.inc.php");
+
+
+$site = new boutique();
+$req = new requete($site->db,
+"SELECT id_produit, quantite FROM boutiqueut_vendu");
+$vendu = array();
+while(list($id,$q)=$req->get_row())
+{
+  if(isset($vendu[$id]))
+    $vendu[$id]+=$q;
+  else
+    $vendu[$id]=$q;
+}
+
+$req = new requete($site->db,
+"SELECT id_produit, quantite FROM boutiqueut_reapro");
+$reapro = array();
+while(list($id,$q)=$req->get_row())
+{
+  if(isset($reapro[$id]))
+    $reapro[$id]+=$q;
+  else
+    $reapro[$id]=$q;
+  if(!isset($vendu[$id]))
+    $vendu[$id]=0;
+}
+
+$req = new requete($site->db,
+"SELECT id_produit, stock_global_prod FROM boutiqueut_produits");
+
+while(list($id,$q)=$req->get_row())
+{
+  if($q!=-1 && $q != ($reapro[$id]-$vendu[$id]))
+    echo "$id - erreur de stock<br>";
+}
+
+?>
