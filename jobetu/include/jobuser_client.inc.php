b4186533e39d171d343ced1253762f786e80dd2c
slopez <slopez@troll>
2009-08-12 00:22:29 +0000

    bleh

--- jobetu/include/jobuser_client.inc.php
+++ jobetu/include/jobuser_client.inc.php
@@ -12,7 +12,7 @@
  *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
@@ -25,19 +25,19 @@ require_once($topdir. "include/cts/user.inc.php");
 
 class jobuser_client extends utilisateur
 {
-	var $annonces = array();
-	var $prefs = array();
+  var $annonces = array();
+  var $prefs = array();
 
-  function new_particulier( $nom, $prenom, $alias, $email, $password, $droit_image, $date_naissance, $sexe)
+  function new_particulier( $nom, $prenom, $email, $password, $droit_image, $date_naissance, $sexe)
   {
-  	$this->create_user( $nom, $prenom, $alias, $email, $password, $droit_image, $date_naissance, $sexe);
+    $this->create_user( $nom, $prenom, $email, $password, $droit_image, $date_naissance, $sexe);
 
   }
 
-  function new_societe( $nom, $prenom, $nom_ste, $email, $password, $droit_image, $date_naissance, $sexe) //Ne prévoit pas la France de demain :(
+  function new_societe( $nom, $prenom, $email, $password, $droit_image, $date_naissance, $sexe) //Ne prévoit pas la France de demain :(
   {
-  	//En attente du flag sur la table utilisateur
-  	$this->create_user( $nom, $prenom, "Société ". $nom_ste, $email, $password, $droit_image, $date_naissance, $sexe);
+    //En attente du flag sur la table utilisateur
+    $this->create_user( $nom, $prenom, $email, $password, $droit_image, $date_naissance, $sexe);
   }
 
 
@@ -51,43 +51,43 @@ class jobuser_client extends utilisateur
   {
    // if( is_jobetu_client() )
       {
-      	$sql = new requete($this->db, "SELECT * FROM job_annonces WHERE id_client = $this->id AND closed <> '1' ORDER BY date DESC");
+        $sql = new requete($this->db, "SELECT * FROM job_annonces WHERE id_client = $this->id AND closed <> '1' ORDER BY date DESC");
 
         while($line = $sql->get_row())
-			    $this->annonces[] = $line;
-	    }
+          $this->annonces[] = $line;
+      }
   }
 
-	function load_prefs()
-	{
-		$sql = new requete($this->db, "SELECT mail_prefs, pub_num FROM `job_prefs` WHERE `id_utilisateur` = $this->id LIMIT 1");
-		$row = $sql->get_row();
+  function load_prefs()
+  {
+    $sql = new requete($this->db, "SELECT mail_prefs, pub_num FROM `job_prefs` WHERE `id_utilisateur` = $this->id LIMIT 1");
+    $row = $sql->get_row();
 
-		if($sql->lines == 0)
-			$this->prefs = null;
-		else
-		{
-			$this->prefs['mail_prefs'] = $row['mail_prefs'];
-			$this->prefs['pub_num'] = $row['pub_num'];
-		}
-	}
+    if($sql->lines == 0)
+      $this->prefs = null;
+    else
+    {
+      $this->prefs['mail_prefs'] = $row['mail_prefs'];
+      $this->prefs['pub_num'] = $row['pub_num'];
+    }
+  }
 
-	function update_prefs($new_pub_profil, $new_mail_prefs, $new_pub_num)
-	{
-		$this->publique = $new_pub_profil;
-		$this->saveinfos(); //fonction matmatronch
+  function update_prefs($new_pub_profil, $new_mail_prefs, $new_pub_num)
+  {
+    $this->publique = $new_pub_profil;
+    $this->saveinfos(); //fonction matmatronch
 
-		if(empty($this->prefs))
-			$sql = new insert($this->dbrw, "job_prefs", array("id_utilisateur" => $this->id, "mail_prefs" => $new_mail_prefs, "pub_num" => $new_pub_num));
-		else
-			$sql = new update($this->dbrw, "job_prefs", array("mail_prefs" => $new_mail_prefs, "pub_num" => $new_pub_num), array("id_utilisateur" => $this->id));
+    if(empty($this->prefs))
+      $sql = new insert($this->dbrw, "job_prefs", array("id_utilisateur" => $this->id, "mail_prefs" => $new_mail_prefs, "pub_num" => $new_pub_num));
+    else
+      $sql = new update($this->dbrw, "job_prefs", array("mail_prefs" => $new_mail_prefs, "pub_num" => $new_pub_num), array("id_utilisateur" => $this->id));
 
-		$this->load_prefs();
+    $this->load_prefs();
 
-		if($sql)
-			return true;
-		else
-			return false;
-	}
+    if($sql)
+      return true;
+    else
+      return false;
+  }
 }
 ?>
