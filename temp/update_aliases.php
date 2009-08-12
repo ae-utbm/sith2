e45db6040ec7d6c6ed7190ecacdaf64c2d7973aa
slopez <slopez@troll>
2009-08-12 00:01:54 +0000

    bleh

--- temp/update_aliases.php
+++ temp/update_aliases.php
@@ -34,7 +34,7 @@ while(list($id,$nom,$prenom,$alias)=$req->get_row())
 {
   $a=strtolower($prenom{0}.str_replace(' ','',$nom));
   $a = substr($a,0,8);
-  if($alias[$a])
+  if(!$alias[$a])
     $alias[$a]=(int)0;
   else
   {
