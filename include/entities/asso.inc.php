079a217f19bb30c5ffdfc1ba959f28172315895d
slopez <slopez@troll>
2009-09-22 10:46:07 +0000

    prevent fail due to vache downtime

--- include/entities/asso.inc.php
+++ include/entities/asso.inc.php
@@ -720,7 +720,8 @@ class asso extends stdentity
   {
     if (strlen($this->nom_unix) <= 0)
       return 0;
-
+    if(file_exists("/tmp/vachedown"))
+      return 0;
     $pendings = file_get_contents("http://barty.me.aeinfo.net/list_heldmsgs.php");
     $pendings = explode("\n", $pendings);
 
