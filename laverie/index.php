7b0d20cf2d7493b3e8e53bfbabffcfb8ecb3b902
mbriand <mbriand@troll>
2009-09-28 12:34:39 +0000

    Laverie : les responsables peuvent voir les noms pour les créneaux réservés

--- laverie/index.php
+++ laverie/index.php
@@ -213,7 +213,7 @@ elseif ( $_REQUEST["action"] == "searchmc" )
       INNER JOIN mc_machines AS ml ON ( cl.id_machine = ml.id AND ml.type='laver' )
       INNER JOIN mc_creneaux AS cs ON ( cs.debut_creneau = cl.fin_creneau )
       INNER JOIN mc_machines AS ms ON ( cs.id_machine = ms.id AND ms.type='secher' )
-      LEFT JOIN utilisateurs ON ( cs.id_utilisateur = utilisateurs.id_utilisateur )
+      LEFT JOIN utilisateurs ON ( cl.id_utilisateur = utilisateurs.id_utilisateur )
       WHERE ml.loc='".mysql_real_escape_string($_REQUEST["id_salle"])."'
       AND ms.loc='".mysql_real_escape_string($_REQUEST["id_salle"])."'
       AND cl.debut_creneau > NOW()";
