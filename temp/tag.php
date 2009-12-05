a03c71bef1d41ba4b8efc1f12740397541178c4b
slopez <slopez@troll>
2009-12-05 10:45:23 +0000

    debug tags

--- temp/tag.php
+++ temp/tag.php
@@ -20,7 +20,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
  * 02111-1307, USA.
  */
-$topdir = "./";
+$topdir = "../";
 include($topdir. "include/site.inc.php");
 require_once($topdir. "include/entities/tag.inc.php");
 require_once($topdir. "include/entities/asso.inc.php");
