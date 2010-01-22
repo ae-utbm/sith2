acd071de3119249718ed48ec682b91a73b14dc59
slopez <slopez@troll>
2010-01-22 09:29:36 +0000

    plus de version papier

--- matmatronch/index.php
+++ matmatronch/index.php
@@ -32,7 +32,6 @@ $info = new contents("Mat'Matronch");
 $info->add_paragraph("Le Mat'Matronch donne enfin un nom a un visage, le numero de portable du binome fantome, l'adresse de ce confrere que l'on recherche tant depuis cette fameuse soiree ...");
 
 $list = new itemlist("Voir aussi");
-$list->add("<a href=\"".$topdir."e-boutic/?id_typeprod=25\">Acheter la version papier</a>");
 $list->add("<a href=\"javascript:window.external.AddSearchProvider('http://ae.utbm.fr/matmatronch/matmatronch.xml');\">Extension Firefox</a>");
 $list->add("La version mobile : <a href=\"http://ae.utbm.fr/i/\">http://ae.utbm.fr/i/</a> <a href=\"".$topdir."iinfo.php\">Informations</a>");
 $info->add($list,true);
@@ -40,8 +39,6 @@ $info->add($list,true);
 $list = new itemlist("Aide");
 $list->add("<a href=\"".$topdir."article.php?name=docs:matmatronch\">Matmatronch</a>");
 $list->add("<a href=\"".$topdir."article.php?name=docs:profil\">Profil personnel</a>");
-$list->add("<a href=\"".$topdir."article.php?name=docs:inscription\">Inscription</a>");
-$list->add("<a href=\"".$topdir."article.php?name=docs:connexion\">Connexion</a>");
 $info->add($list,true);
 
 $site->add_box("mmtinfo",$info);
