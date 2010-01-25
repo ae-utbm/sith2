c6cc398bfb79e6eb340524eb0900f6f2edbe54a9
mbriand <mbriand@troll>
2010-01-25 23:26:56 +0000

    et hop !

--- matmatronch/index.php
+++ matmatronch/index.php
@@ -193,7 +193,7 @@ if ( $_REQUEST["action"] == "search" || $_REQUEST["action"] == "simplesearch" )
         FROM `utilisateurs`
         LEFT JOIN `utl_etu` ON `utl_etu`.`id_utilisateur`=`utilisateurs`.`id_utilisateur`
         LEFT JOIN `utl_etu_utbm` ON `utl_etu_utbm`.`id_utilisateur`=`utilisateurs`.`id_utilisateur`
-        WHERE "  .implode(" AND ",$elements)." $order LIMIT $st,$npp", 1);
+        WHERE "  .implode(" AND ",$elements)." $order LIMIT $st,$npp");
 
       $user = new utilisateur($site->db);
 
