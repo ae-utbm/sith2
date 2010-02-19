9a6b6444448e078f76b8f2004bcae47a555cbb07
mbriand <mbriand@troll>
2010-02-19 16:33:41 +0000

    Gestion des écrans d'affichage : soumission et modération des affiches

--- ae/com.php
+++ ae/com.php
@@ -63,7 +63,7 @@ $req = new requete($site->db,"SELECT COUNT(*) FROM `planet_tags`  WHERE `modere`
 list($nbtags) = $req->get_row();
 $nbflux+=$nbtags;
 
-$req = new requete($site->db,"SELECT COUNT(*) FROM `aff_affiches`  WHERE `modere_nvl`='0' ");
+$req = new requete($site->db,"SELECT COUNT(*) FROM `aff_affiches`  WHERE `modere_aff`='0' ");
 list($nbaffiches) = $req->get_row();
 
 if ( $nbnews > 0 )
@@ -82,16 +82,16 @@ else
   $sublist->add("<a href=\"".$topdir."planet/index.php?view=modere\">Modération des flux (Aucun)</a>");
 
 if ( $nbaffiches > 0 )
-  $sublist->add("<a href=\"".$topdir."planet/index.php?view=modere\"><b>Modération des affiches ($nbaffiches)</b></a>");
+  $sublist->add("<a href=\"modereaffiches.php\"><b>Modération des affiches ($nbaffiches)</b></a>");
 else
-  $sublist->add("<a href=\"".$topdir."planet/index.php?view=modere\">Modération des affiches (Aucune)</a>");
+  $sublist->add("<a href=\"modereaffiches.php\">Modération des affiches (Aucune)</a>");
 
 $board->add($sublist,true);
 
 $sublist = new itemlist("Divers");
 $sublist->add("<a href=\"sondage.php\">Sondages</a>");
 $sublist->add("<a href=\"weekmail.php\">Weekmail</a>");
-$sublist->add("<a href=\"affiches.php?page=list\">Affiches actuelles ou à venir</a>");
+$sublist->add("<a href=\"".$topdir."affiches.php?page=list\">Affiches actuelles ou à venir</a>");
 $board->add($sublist,true);
 
 $cts->add($board);
