7711d51f0f5c56b6e89bf28830870d86340d8041
slopez <slopez@troll>
2010-03-11 09:57:39 +0000

    voyons voir si le lien avec super flux est pas tout cassé :)

---
+++ temp/superflux.php
@@ -0,0 +1,8 @@
+<?
+$topdir = '../';
+//require_once($topdir. "include/site.inc.php");
+//$site = new site();
+print_r(unserialize(file_get_contents($topdir."var/cache/stream")));
+print_r(unserialize(file_get_contents($topdir."var/cache/stream-prod")));
+
+?>
