4d6c9f496f00ef0430bb240f5d8d04f62c488932
mbriand <mbriand@troll>
2010-05-08 16:11:55 +0000

    bleh

--- include/globals.inc.php
+++ include/globals.inc.php
@@ -330,14 +330,7 @@ function utf8_pattern_accents ($text)
  */
 function gen_uid ()
 {
-  $micro = substr (microtime(), 2, 6) ;
-  $concat = time() . $micro ;
-  $dec_1 = substr ($concat, 0, 8) ;
-  $dec_2 = substr ($concat, 8, 8) ;
-  $hex_1 = dechex ($dec_1) ;
-  $hex_2 = dechex ($dec_2) ;
-  $id = $hex_1 . $hex_2 ;
-  return $id ;
+  return uniqid('', true);
 }
 
 /**
