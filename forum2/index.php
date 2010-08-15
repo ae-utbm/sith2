79f6caf7c80e974b5bf15040444171db17f2e582
mbriand <mbriand@troll>
2010-08-15 17:52:20 +0000

    forum: confirmation de suppression d'un message

--- forum2/index.php
+++ forum2/index.php
@@ -249,8 +249,8 @@ if ( $_REQUEST['page'] == 'delete' )
   $site->allow_only_logged_users("forum");
   if ( $message->is_valid() )
   {
-    if (($forum->is_admin($site->user))
-        || ($message->id_utilisateur == $site->user->id) && $site->is_sure("", "Supprimer le message ".$message->id." ?", 1))
+    if ((($forum->is_admin($site->user))
+        || ($message->id_utilisateur == $site->user->id)) && $site->is_sure("", "Supprimer le message ".$message->id." ?", 1))
     {
       $message_initial = new message($site->db);
       $message_initial->load_initial_of_sujet($sujet->id);
