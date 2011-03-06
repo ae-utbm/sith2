5a9575a699384122327ba9de02ecd6719a45def4
jlaval <jlaval@troll>
2011-03-06 20:53:07 +0000

    Ajout page détail

--- ae/infotodo.php
+++ ae/infotodo.php
@@ -24,6 +24,8 @@
 $topdir="../";
 
 require_once($topdir."include/site.inc.php");
+require_once($topdir."include/entities/utilisateur.inc.php");
+require_once($topdir."include/entities/asso.inc.php");
 require_once($topdir."include/cts/user.inc.php");
 require_once($topdir."include/entities/todoitem.inc.php");
 require_once($topdir."include/cts/sqltable.inc.php");
@@ -33,51 +35,74 @@ $site = new site ();
 if ( !$site->user->is_in_group("gestion_ae") )
   $site->error_forbidden("none","group",7);
 
-//$site->add_js("js/sqltable2.js");
 $site->start_page("none","TODO list");
 
-$cts = new contents ('Foo');
-$frmfilter = new form('filter', '?', false, 'GET', 'Filter');
-$frmfilter->add_select_field('etat', 'Etat', array('' => 'Tout', 'new' => 'Nouveau', 'resolu' => 'Résolu', 'encours' => 'En cours'), '');
-$frmfilter->add_checkbox ('onlyme', 'Uniquement ceux assigné à moi');
-$frmfilter->add_submit ('submit', 'Filtrer');
-$cts->add ($frmfilter, false);
+if (isset ($_REQUEST['action']) && $_REQUEST['action'] == 'detail') {
 
-$where = array();
-if (isset ($_REQUEST['onlyme']) && $_REQUEST['onlyme'])
-    $where[] = '`id_user_assignee` = '.$site->user;
-if (isset ($_REQUEST['etat'])) {
-    $etats = array('new' => 0, 'resolu' => 4, 'encours' => 3);
-    if (array_key_exists ($_REQUEST['etat'], $etats))
-        $where[] = $etats[$_REQUEST['etat']];
+} else if (isset ($_REQUEST['id_task']) && isset ($_REQUEST['action']) && $_REQUEST['action'] == 'detail') {
+    $idtask = $_REQUEST['id_task'];
+    $todo = new todoitem ();
+    $todo->load_by_id ($idtask);
+
+    $util_reporter = new utilisateur ();
+    $util_reporter->load_by_id ($todo->id_user_reporter);
+    $util_assignee = new utilisateur ();
+    $util_assignee->load_by_id ($todo->id_user_assignee);
+    $asso_concerne = new asso ();
+    $asso_concerne->load_by_id ($todo->id_asso_concerned);
+
+    $frm = new form ('details', '?', false, 'POST', 'TODO');
+    $frm->add_hidden ('id_task', $id_task);
+    $frm->add_hidden ('action', 'modif');
+    $frm->add_entity_smartselect ('utilisateur_reporter', 'Rapporteur', $util_reporter);
+    $frm->add_entity_smartselect ('utilisateur_assignee', 'Assigné à', $util_reporter);
+    $frm->add_entity_smartselect ('asso_concerned', 'Asso lié', $asso_concerne);
+    $frm->add_date_field ('date_deadline', 'Deadline', strtotime ($todo->date_deadline));
+    $frm->add_date_field ('date_submitted', 'Soumis le', strtotime ($todo->date_submitted), $enabled = false);
+    $frm->add_select_field ('priority', 'Priorité', $todo_priorities, $todo->priority);
+    $frm->add_select_field ('status', 'Statut', $todo_status, $todo->status);
+} else {
+    $cts = new contents ('TODO');
+    $frmfilter = new form('filter', '?', false, 'GET', 'Filter');
+    $frmfilter->add_select_field('etat', 'Etat', array('' => 'Tout', 'new' => 'Nouveau', 'resolu' => 'Résolu', 'encours' => 'En cours'), '');
+    $frmfilter->add_checkbox ('onlyme', 'Uniquement ceux assigné à moi');
+    $frmfilter->add_submit ('submit', 'Filtrer');
+    $cts->add ($frmfilter, false);
+
+    $where = array();
+    if (isset ($_REQUEST['onlyme']) && $_REQUEST['onlyme'])
+        $where[] = '`id_user_assignee` = '.$site->user;
+    if (isset ($_REQUEST['etat'])) {
+        $etats = array('new' => 0, 'resolu' => 4, 'encours' => 3);
+        if (array_key_exists ($_REQUEST['etat'], $etats))
+            $where[] = $etats[$_REQUEST['etat']];
+    }
+
+    $sql = 'SELECT * FROM ae_info_todo ORDER BY priority, date_deadline, date_submitted';
+    $req = new requete($site->db, $sql);
+    if (!empty ($where))
+        $sql .= ' WHERE '.implode(' AND ', $where);
+
+    $tblcts = new contents('TODO list');
+    $tbl = new sqltable ('infotodo', 'Liste des tâches', $req, 'infotodo.php', 'id_task',
+                         array('nom_utilisateur_reporter' => 'Demandeur',
+                               'nom_utilisateur_assignee' => 'Assigné à',
+                               'nom_asso_concerned' => array('Club associé', 'nom_asso'),
+                               'date_deadline' => 'Deadline',
+                               'date_submitted' => 'Date soumission',
+                               'priority' => 'Priorité',
+                               'enh_or_bug' => 'Type',
+                               'status' => 'Statut',
+                               'description' => 'Description'),
+                         array('detail' => 'Détails'),
+                         array(),
+                         array());
+    $tblcts->add ($tbl);
+
+    $site->add_contents ($cts);
+    $site->add_contents ($tblcts);
 }
 
-$sql = 'SELECT * FROM ae_info_todo ORDER BY priority, date_deadline, date_submitted';
-$req = new requete($site->db, $sql);
-if (!empty ($where))
-    $sql .= ' WHERE '.implode(' AND ', $where);
-
-/*$tbl = new sqltable2 ('todos', 'Liste TODO', 'infotodo.php');
-$tbl->add_column_entity ('id_user_reporter', 'Reporter', array('nom_utilisateur_reporter'));
-$tbl->add_column_entity ('id_user_assignee', 'Assigné à', array('nom_utilisateur_assignee'));
-$tbl->set_sql ($site->db, 'id_task', $sql);*/
-
-$tbl = new sqltable ('infotodo', 'Liste des tâches', $req, 'infotodo.php', 'id_task',
-                     array('nom_utilisateur_reporter' => 'Demandeur',
-                           'nom_utilisateur_assignee' => 'Assigné à',
-                           'nom_asso_concerned' => array('Club associé', 'nom_asso'),
-                           'date_deadline' => 'Deadline',
-                           'date_submitted' => 'Date soumission',
-                           'priority' => 'Priorité',
-                           'enh_or_bug' => 'Type',
-                           'status' => 'Statut',
-                           'description' => 'Description'),
-                     array('detail', 'Détails'),
-                     array(),
-                     array());
-$cts->add ($tbl);
-
-$site->add_contents ($cts);
 $site->end_page();
 
 ?>
