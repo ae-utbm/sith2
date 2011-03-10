3a831341e3fcd8d301c46a8dbccb65be494f2879
Antoine Tenart <aj@troll>
2011-03-10 01:20:59 +0000

    bleh

--- d.php
+++ d.php
@@ -381,8 +381,14 @@ if ( $file->is_valid() )
   else
     $cts = new contents($path);
 
-  if( !$file->is_moderated() )
-    $cts->add_paragraph("<b>Attention : fichier non modéré.</b>");
+  if( !$file->is_moderated() ) {
+    $mod_text = "<b>Attention : fichier non modéré.</b>";
+
+    if( $site->user->is_in_group("moderateur_site") )
+      $mod_text .= "<br /><b><a href=\"d.php?id_file=".$file->id."&amp;action=moderate\">Modérer</a></b>";
+
+    $cts->add_paragraph($mod_text);
+  }
 
   if ( isset($Notice) )
     $cts->add_paragraph("<b>$Notice</b>");
