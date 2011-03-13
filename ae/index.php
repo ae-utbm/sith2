071c062bf32ebfb179e50b5ddc6c780a3fde66d5
jlaval <jlaval@troll>
2011-03-13 19:23:46 +0000

    Ajoute lien vers le todolist de l'équipe info

--- ae/index.php
+++ ae/index.php
@@ -84,6 +84,7 @@ $sublist->add("<a href=\"".$topdir."activites.php?view=list\">Listing des respon
 $sublist->add("<a href=\"".$topdir."asso/sendfax.php?id_asso=1\">Envoyer des fax</a>");
 $sublist->add("<a href=\"".$topdir."group.php\">Gestion des groupes</a>");
 $sublist->add("<a href=\"".$topdir."ae/partenariats.php\">Avantages partenariats en attente</a>");
+$sublist->add('<a href="'.$topdir.'"ae/infotodo.php">Tâches équipe info</a>');
 $board->add($sublist,true);
 
 $cts->add($board);
