16766f3fb6b107684e1a24735186f41964823f3f
Antoine Tenart <aj@troll>
2011-03-14 14:47:53 +0000

    test sans icone

--- d.php
+++ d.php
@@ -708,13 +708,13 @@ if ( isset($_SESSION["d_clipboard"]) )
     {
       $inffile->load_by_id($id);
       $clip_txt = $inffile->get_html_link();
-      $clip_txt .= " <a href=\"d.php?id_folder=".$folder->id."&amp;undo_cut_file=".$id."\"><img src=\"".$topdir."images/actions/decrease.png\" alt=\"Annuler couper\" title=\"Annuler couper\" class=\"icon\" /></a>";
+      $clip_txt .= "\t—\t<a href=\"d.php?id_folder=".$folder->id."&amp;undo_cut_file=".$id."\">retirer du presse papier</a>";
     }
     else
     {
       $inffolder->load_by_id($id);
       $clip_txt=$inffolder->get_html_link();
-      $clip_txt .= " <a href=\"d.php?id_folder=".$folder->id."&amp;undo_cut_folder=".$id."\"><img src=\"".$topdir."images/actions/decrease.png\" alt=\"Annuler couper\" title=\"Annuler couper\" class=\"icon\" /></a>";
+      $clip_txt .= "\t—\t<a href=\"d.php?id_folder=".$folder->id."&amp;undo_cut_folder=".$id."\">retirer du presse papier</a>";
     }
     $lst->add($clip_txt);
   }
