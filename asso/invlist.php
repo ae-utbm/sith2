cf7501e309c3531100f6b189d967cb754ed38d0d
jlaval <jlaval@troll>
2011-03-27 13:28:14 +0000

    Fix SQL

--- asso/invlist.php
+++ asso/invlist.php
@@ -41,7 +41,7 @@ if (isset ($_REQUEST['id_asso'])) {
         exit();
     }
 
-    $sql = 'SELECT inv_objet.nom_objet AS nom, salle.nom_salle AS lien, inv_objet.date_achat AS date, inv_objet.prix_objet AS prix'
+    $sql = 'SELECT inv_objet.nom_objet AS nom, sl_salle.nom_salle AS lien, inv_objet.date_achat AS date, inv_objet.prix_objet AS prix'
         .'FROM inv_objet LEFT JOIN sl_salle on sl_salle.id_salle=inv_objet.id_salle'
         .'WHERE inv_objet.id_asso='.intval ($asso->id);
     $req = new requete ($site->db, $req);
