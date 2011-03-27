220aebf92fe0719f3af09e2f615edd1deccf6318
jlaval <jlaval@troll>
2011-03-27 13:37:37 +0000

    Désactive debug

--- asso/invlist.php
+++ asso/invlist.php
@@ -44,7 +44,7 @@ if (isset ($_REQUEST['id_asso'])) {
     $sql = 'SELECT inv_objet.nom_objet AS nom, sl_salle.nom_salle AS lien, inv_objet.date_achat AS date, inv_objet.prix_objet AS prix'
         .' FROM inv_objet LEFT JOIN sl_salle on sl_salle.id_salle=inv_objet.id_salle'
         .' WHERE inv_objet.id_asso='.intval ($asso->id);
-    $req = new requete ($site->db, $sql, 1);
+    $req = new requete ($site->db, $sql);
     $lines = array ();
 
     while (($line = $req->get_row ()) != null)
