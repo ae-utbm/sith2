7fc323144cfe9ca0a9364ea4027701fb55de81a7
jlaval <jlaval@troll>
2011-03-28 06:46:56 +0000

    Un "fichier non modéré" que tu l'a bien dans ta geule

--- d.php
+++ d.php
@@ -414,7 +414,7 @@ if ( $file->is_valid() )
     $cts = new contents($path);
 
   if( !$file->is_moderated() ) {
-    $mod_text = "<b>Attention : fichier non modéré.</b>";
+    $mod_text = '<span class="alertunmod">Attention : fichier non modéré.</span>';
 
     if( $site->user->is_in_group("moderateur_site") ) {
       $mod_text .= "<br /><br /><b><a href=\"d.php?id_file=".$file->id."&amp;action=accepter\">Accepter ce fichier</a></b>";
