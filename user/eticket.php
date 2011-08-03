64304faacfa41048e216a71949852a11a4a8d145
jlaval <jlaval@troll>
2011-08-03 21:12:24 +0000

    Pas besoin de l'id produit non plus

--- user/eticket.php
+++ user/eticket.php
@@ -48,22 +48,28 @@ else
 
 $user->load_all_extra ();
 
-if (!isset ($_REQUEST['id_ticket']) || !isset($_REQUEST['id_produit']))
+if (!isset ($_REQUEST['id_ticket']))
     $site->error_not_found ('none', '/user/compteae.php');
 
 $id_ticket = intval ($_REQUEST['id_ticket']);
-$id_produit = intval ($_REQUEST['id_produit']);
 
 $eticket = new eticket ($site->db, $site->dbrw);
-if (!$eticket->load_by_id ($id_ticket) || $eticket->id_produit != $id_produit)
+if (!$eticket->load_by_id ($id_ticket))
+    $site->error_not_found ('none', '/user/compteae.php');
+
+$id_produit = $eticket->id_produit;
+
+$req = new requete ($site->db, 'SELECT vendu.quantite AS quantite FROM cpt_vendu AS vendu LEFT JOIN cpt_debitfacture AS debit ON vendu.id_facture = debit.id_facture WHERE vendu.id_produit='.$id_produit.' AND debit.id_utilisateur_client='.intval($user->id).' LIMIT 1');
+if ($req->lines == 0)
+    $site->error_not_found ('none', '/user/compteae.php');
+
+$row = $req->get_row();
+$quantite = intval($row['quantite']);
+if ($quantite == 0)
     $site->error_not_found ('none', '/user/compteae.php');
 
 $file = new dfile($site->db, $site->dbrw);
 $file->load_by_id ($eticket->banner);
-
-$req = new requete ($site->db, 'SELECT vendu.quantite AS quantite FROM cpt_vendu AS vendu LEFT JOIN cpt_debitfacture AS debit ON vendu.id_facture = debit.id_facture WHERE vendu.id_produit='.$id_produit.' AND debit.id_utilisateur_client='.intval($user->id).' LIMIT 1');
-$row = $req->get_row();
-$quantite = intval($row['quantite']);
 
 $code = $user->id . ' ' . $id_produit . ' ' . $quantite;
 $hash = $eticket->compute_hash_for($code);
