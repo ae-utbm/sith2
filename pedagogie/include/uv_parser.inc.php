294f80972f35e316f08da10018498db4f787a225
Antoine Tenart <aj@troll>
2011-08-13 16:42:03 +0000

    tss

--- pedagogie/include/uv_parser.inc.php
+++ pedagogie/include/uv_parser.inc.php
@@ -87,13 +87,15 @@ class UVParser
     $txt = $this->get_real_uv($txt);
     $this->_target = explode("\n",$txt);
 
+    print_r( $this->_target );
+/*
     $i = -1;
     while( isset($this->_target[++$i]) ) {
       if( preg_match('/'.$this->_phrase.'et('.$this->_info.')/', $this->_target[i], $matches) )
         print_r( $matches );
       unset( $matches );
     }
-
+*/
     $this->parse();
 
     if( $load_next )
