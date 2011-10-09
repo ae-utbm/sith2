f2132a55168a5cf9c0cda8b48a1c10f0df5211b2
jlaval <jlaval@troll>
2011-10-09 19:09:47 +0000

    Allez c'est parti pour le pifomètre à cadran

--- include/pdf/carteae.inc.php
+++ include/pdf/carteae.inc.php
@@ -72,7 +72,7 @@ class pdfcarteae extends FPDF
 
 
     $this->pos[1] = array (
-      "photo" => array ("x"=>4.5,"y"=>7.8,"w"=>24.0,"h"=>33.0),
+      "photo" => array ("x"=>4.7,"y"=>8.2,"w"=>24.0,"h"=>33.0),
       "cbar" => array ("x"=>8.7,"y"=>4,2,"w"=>67,"h"=>25),
       "front" =>
         array (
