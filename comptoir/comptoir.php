002d6f6980aa3f51033778451186f2f710c1f56c
Smoi <ecarre@troll>
2011-10-31 16:57:57 +0000

    On reprend

--- comptoir/comptoir.php
+++ comptoir/comptoir.php
@@ -131,14 +131,14 @@ $site->add_box("comptoir",$cts);
 unset($cts);
 
 // Test by Smoi pour les baguettes BDF (il prie pour pas faire de conneries)
-if($_REQUEST["id_comptoir"] == 2)
-{
+//if($_REQUEST["id_comptoir"] == 2)
+//{
   $bgts = new contents("Baguettes");
   $bgts->add_paragraph("Smoi : 1 baguettes<br />Kiri : 2 baguettes (l'en faut pour le frometon)");
 
   $site->add_box("baguettes",$bgts);
   unset($bgts);
-}
+//}
 
 include("frontend.inc.php");
 
