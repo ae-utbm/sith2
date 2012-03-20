61156c3284b11bcaeaf084f63ae810a5650dc6d6
ariss <ariss@troll>
2012-03-20 17:43:04 +0000

    Eboutic: modif au niveau du total 2

--- e-boutic/include/e-boutic.inc.php
+++ e-boutic/include/e-boutic.inc.php
@@ -344,7 +344,8 @@ class eboutic extends site
         $list->add($_SESSION['eboutic_cart'][$item->id]." - ".$item->nom);
       }
       $panier->add($list,true);
-      $panier->add_paragraph("<b>Total</b>".sprintf("%.2f",$this->total/100)." Euros");
+      $panier->add_paragraph("<span align=\"right\"><b>Total : </b>".sprintf("%.2f",$this->total/100).
+        " Euros</span>");
       $panier->add_paragraph("<a href=\"cart.php\">Passer la commande</a>");
     }
 
