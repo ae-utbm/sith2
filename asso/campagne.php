e73aad340ef7feb3a87cb02b665d54ffed3681c0
ariss <ariss@troll>
2012-03-21 14:32:54 +0000

    campagne: TROLOLOLOL

--- asso/campagne.php
+++ asso/campagne.php
@@ -257,10 +257,11 @@ elseif(!is_null($cpg->id) && $_REQUEST["action"]=="results" && $cpg->asso==$_REQ
 
   $cam = new camembert(750,400,array(),2,0,0,0,0,0,0,10,240);
 
+  $tem = "";
   while(list($nombre_reponses, $id_question, $nom_question, $valeur_reponse) = $req->get_row()) {
-    if ($id_question != $id_question_precedente) {
+    if ($tem == "" || $id_question == $tem) {
       $cam->data($nombre_reponses, $valeur_reponse);
-      $id_question_precedente = $id_question;
+      $tem = $id_question;
     }
   }
   $cam->png_render();
