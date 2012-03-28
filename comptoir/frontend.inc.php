83fd33e91b71c20437435ee9036d30e9282a799d
Antoine Tenart <aj@troll>
2012-03-28 17:45:24 +0000

    fix

--- comptoir/frontend.inc.php
+++ comptoir/frontend.inc.php
@@ -532,9 +532,9 @@ else if ( $site->comptoir->client->id > 0 )
 
         $prix = $vp->produit->obtenir_prix($site->comptoir->prix_barman,$site->comptoir->client);
 
-        $tbl->add_row(array("<a href=\"#\" onclick=\"return decrease('".$vp->produit->code_barre."', ".$prix.", ".($vp->produit->plateau && !$site->comptoir->prix_barman).");\">-</a>",
+        $tbl->add_row(array("<a href=\"#\" onclick=\"return decrease('".$vp->produit->code_barre."', ".$prix.", ".(($vp->produit->plateau && !$site->comptoir->prix_barman) ? '1' : '0').");\">-</a>",
             array($nbP, false, "nbProd".$vp->produit->code_barre),
-            "<a href=\"#\" onclick=\"return increase('".$vp->produit->code_barre."', ".$prix.", ".($vp->produit->plateau && !$site->comptoir->prix_barman).");\">+</a>",
+            "<a href=\"#\" onclick=\"return increase('".$vp->produit->code_barre."', ".$prix.", ".(($vp->produit->plateau && !$site->comptoir->prix_barman) ? '1' : '0').");\">+</a>",
             $vp->produit->nom,
             array(($prix*$nb/100)." &euro;", false, "priceProd".$vp->produit->code_barre)),
           false, "prod".$vp->produit->code_barre);
