d40f69a939230929d53e37380e6f0a13b30f1430
Antoine Tenart <aj@troll>
2012-03-28 01:13:29 +0000

    fix

--- ae/infotodo.php
+++ ae/infotodo.php
@@ -43,7 +43,7 @@ if (isset ($_REQUEST['action']) && $_REQUEST['action'] == 'commit') {
     $todo->id_user_reporter = $_REQUEST['utilisateur_reporter'];
     $todo->id_user_assignee = $_REQUEST['utilisateur_assignee'];
     $todo->id_asso_concerned = $_REQUEST['asso_concerned'];
-    $todo->date_submitted = $_REQUEST['date_submitted'];
+    $todo->date_submitted = time ();
     $todo->priority = $_REQUEST['priority'];
     $todo->status = $_REQUEST['status'];
     $todo->enh_or_bug = $_REQUEST['type'];
