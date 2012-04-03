917f4fc7266a86e89588df1e8e49532551584779
Antoine Tenart <aj@troll>
2012-04-03 13:41:29 +0000

    plop

--- phpcron/daily.midnight.php
+++ phpcron/daily.midnight.php
@@ -1,4 +1,26 @@
 <?php
+/* Copyright 2006-2012
+ * - Antoine Ténart <antoine dot tenart at utbm dot fr>
+ *
+ * Ce fichier fait partie du site de l'Association des Étudiants de
+ * l'UTBM, http://ae.utbm.fr.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License a
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
 if (!isset ($argc))
   exit ();
 
