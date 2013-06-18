f4ce6e2654cc62af608e809f3e02b28bd2597b53
Bate <slelann@troll>
2013-06-18 11:47:39 +0000

    Planning2

--- include/entities/planning2.inc.php
+++ include/entities/planning2.inc.php
@@ -124,9 +124,9 @@ class planning2 extends stdentity
 			return -1;
 		if($start >= $end)
 			return -1;
-		if($this->weekly && $start < $this->start)
+		if(!$this->weekly && $start < $this->start)
 			return -1;
-		if($this->weekly && $end > $this->end)
+		if(!$this->weekly && $end > $this->end)
 			return -1;
 		if($this->weekly)
 		{
