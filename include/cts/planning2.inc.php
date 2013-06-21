a3ef227fe06d1d05f81694123fecd229cacbf435
Bate <slelann@troll>
2013-06-21 08:53:49 +0000

    Planning2

--- include/cts/planning2.inc.php
+++ include/cts/planning2.inc.php
@@ -58,7 +58,7 @@ class planningv extends stdcontents
 	$buffer = "<table class=\"pl2_multi\">\n<tr>\n";
 	foreach($days as $day)
 	{
-		$buffer .= "<th class=\"pl2_day_name\">".strftime("%A %d/%m",strtotime($day)+$planning->weekly?$this->week_start:0)."</th>";
+		$buffer .= "<th class=\"pl2_day_name\">".strftime("%A %d/%m",strtotime($day)+(($planning->weekly)?($this->week_start):0))."</th>";
 	}
 	$buffer .= "</tr>\n<tr><td class=\"pl2_multi\">";
 	$buffer .= $body;
