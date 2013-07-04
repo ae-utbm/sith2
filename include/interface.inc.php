1c74f5708b0e7a676de53b5bd91355754a314a23
Bate <slelann@troll>
2013-07-04 08:55:03 +0000

    Changement de wwwtopdir

--- include/interface.inc.php
+++ include/interface.inc.php
@@ -36,6 +36,7 @@ if( !strncmp('/var/www/taiste', $_SERVER['SCRIPT_FILENAME'], 22) )
   $GLOBALS["taiste"] = true;
 else
   $GLOBALS["taiste"] = false;
+echo $_SERVER['SCRIPT_FILENAME'];
 
 require_once($topdir . "include/mysql.inc.php");
 require_once($topdir . "include/mysqlae.inc.php");
