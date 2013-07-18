9c64a5badfbf38d96f3e2409093ba5e89b665d48
Bate <slelann@troll>
2013-07-18 07:57:42 +0000

    Correction erreur requete sql dans galaxy

--- include/galaxy.inc.php
+++ include/galaxy.inc.php
@@ -451,8 +451,8 @@ class galaxy
     $bottom_x = ceil($bottom_x);
     $bottom_y = ceil($bottom_y);
 
-    $mult_x = 10000/($bottom_x-$top_x);
-    $mult_y = 10000/($bottom_y-$top_y);
+    $mult_x = floor(10000/($bottom_x-$top_x));
+    $mult_y = floor(10000/($bottom_y-$top_y));
 
     $this->width = $tx;//($bottom_x-$top_x)*$tx;
     $this->height = $tx;//($bottom_y-$top_y)*$tx;
