385f59691ceb1d7ca3578a819f5cfbf110ef416d
Bate <slelann@troll>
2013-07-23 20:50:47 +0000

    Correction Chemin video flash

--- include/cts/video.inc.php
+++ include/cts/video.inc.php
@@ -56,7 +56,7 @@ class flvideo extends stdcontents
 
 		return
 "<object type=\"application/x-shockwave-flash\" data=\"".$wwwtopdir."images/flash/flvplayer.swf\" width=\"400\" height=\"300\">".
-"<param name=\"movie\" value=\"".$wwwtopdir."images/flash/flvplayer.swf\" />"."<param name=\"FlashVars\" value=\"flv=../../".$this->src."\" />"."<param name=\"wmode\" value=\"transparent\" />"."</object>";
+"<param name=\"movie\" value=\"".$wwwtopdir."images/flash/flvplayer.swf\" />"."<param name=\"FlashVars\" value=\"flv=".$wwwtopdir."sas2/".$this->src."\" />"."<param name=\"wmode\" value=\"transparent\" />"."</object>";
 	}
 
 }
