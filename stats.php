1b924109b2b67b59df238c56144d240d3a1280e0
Bate <slelann@troll>
2013-08-05 15:18:36 +0000

    MaJ stats matma

--- stats.php
+++ stats.php
@@ -961,8 +961,8 @@ elseif ( $_REQUEST["view"] == "matmatronch" )
 
 
   $req = new requete($site->db,"SELECT `publique_utl`, COUNT(*) total, ".
-                              	"SUM(IF(`date_maj_utl` > '$debut_semestre' OR `publique_utl` = '2', 1, 0 )) `semestre_chg` ".
-                              	"SUM(IF(`date_maj_utl` > '$debut_annee' OR `publique_utl` = '2', 1, 0 )) `annee_chg` ".
+                              	"SUM(IF(`date_maj_utl` > '$debut_semestre' OR `publique_utl` = '2', 1, 0 )) `semestre_chg`, ".
+                              	"SUM(IF(`date_maj_utl` > '$debut_annee' OR `publique_utl` = '2', 1, 0 )) `annee_chg`, ".
                               	"SUM(IF(`date_maj_utl` > '2010-09-01' OR `publique_utl` = '2', 1, 0 )) `apres_chg` ".
                               	"FROM `utilisateurs` ".
                               	"GROUP BY `publique_utl` ".
