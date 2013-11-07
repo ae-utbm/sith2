84a54b5997c8bddc7578af57950368af84d6d666
Bate <slelann@troll>
2013-11-07 23:22:07 +0000

    Debut stats comptoirs TR

--- stats.php
+++ stats.php
@@ -1402,22 +1402,18 @@ elseif ( ($site->user->is_in_group ("gestion_ae") || $site->user->is_asso_role (
 			<head>
 	      			<title>Top temps réel</title>
 				<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-		    		<script src=\"https://code.jquery.com/jquery.js\"></script>
-				<script src=\"//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js\"></script>
-				<!-- Latest compiled and minified CSS -->
+		    						<!-- Latest compiled and minified CSS -->
 				<link rel=\"stylesheet\" href=\"//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css\">
 
-				<!-- Optional theme -->
-				<link rel=\"stylesheet\" href=\"//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap-theme.min.css\">
-
 				<!-- Latest compiled and minified JavaScript -->
+				<script src=\"https://code.jquery.com/jquery.js\"></script>
 				<script src=\"//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js\"></script>
 
 
 				<script src=\"https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js\"></script>
       				<script src=\"https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js\"></script>
       			</head>
-        		<body>
+			<body>
 				<script>function reload() { window.location.replace(\"$addresse\"); }
 
 					$( document ).ready(function() { setTimeout(reload,60000) });
