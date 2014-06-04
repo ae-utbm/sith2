597db6b97a4feaadcc6e3b786672bf3125346739
Bate <slelann@troll>
2014-06-04 18:57:34 +0000

    Smilies

--- include/lib/dokusyntax.inc.php
+++ include/lib/dokusyntax.inc.php
@@ -667,7 +667,7 @@ class dokusyntax
       'FIXME'=>'fixme.gif',
       'DELETEME'=>'delete.gif'
                      );
-    $smPath = $wwwtopdir."images/forum/smilies/";
+    $smPath = $_SERVER['DOCUMENT_ROOT']."/".$wwwtopdir."images/forum/smilies/";
     foreach($smileys as $tag => $img)
     {
       echo "$smPath/$img ".(file_exists($smPath . "/" . $img)?"existe":"n'existe pas")."\n";
