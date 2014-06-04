e64e1a8eaa74418cd6d35f3597d342a4d4abe8a1
Bate <slelann@troll>
2014-06-04 18:18:32 +0000

    Syntaxe doku https

--- include/lib/dokusyntax.inc.php
+++ include/lib/dokusyntax.inc.php
@@ -1162,7 +1162,7 @@ class dokusyntax
     if (empty($xml->html))
       return 'Fichier oEmbed non supporté.';
 
-    return $xml->html;
+    return preg_replace("http\:\/\/","https\:\/\/",$xml->html);
   }
 
   function get_oembed_url($url)
