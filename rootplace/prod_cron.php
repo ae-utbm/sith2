13a59cfb6405f652bf0f16f4b4964d0aadb09230
Skia <skia@troll>
2015-02-04 01:36:30 +0000

    Amend: Add script to refresh taiste db

--- rootplace/prod_cron.php
+++ rootplace/prod_cron.php
@@ -37,7 +37,7 @@ if ( !$site->user->is_in_group("root") )
 $site->start_page("none","Administration / passage en prod");
 $cts = new contents("<a href=\"./\">Administration</a> / Passage en production");
 $tabs = array(array("","rootplace/prod_cron.php","Passage en prod"),
-              array("","rootplace/prod_cron.php?view=refreshdb","Rafraichir taiste"),
+              array("refresh","rootplace/prod_cron.php?view=refreshdb","Rafraichir taiste"),
               array("script","rootplace/prod_cron.php?view=script","Script de passage en prod"),
               array("commit","rootplace/prod_cron.php?view=commit","Script de post commit"),
               array("clone","rootplace/prod_cron.php?view=refreshdbscript","Script de refresh de taiste"));
