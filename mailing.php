5fe0136930aca895464ffe61ffdda66efbab7c3d
Skia <lordbanana25@mailoo.org>
2015-07-13 19:14:19 +0200

    Add mailing page

---
+++ mailing.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once($topdir. "include/mysql.inc.php");
+require_once($topdir. "include/mysqlae.inc.php");
+
+if ( isset ($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != "on" )
+{
+  echo "ERROR HTTPS REQUIRED";
+  exit();
+}
+
+$db = new mysqlae ();
+
+if ( !$db->dbh )
+{
+  echo "ERROR DB UNAVAILABLE";
+  exit();
+}
+
+$valid = new requete($db,
+  "SELECT `key` ".
+  "FROM `sso_api_keys` ".
+  "WHERE `key` = '".mysql_real_escape_string($_REQUEST["key"])."'");
+
+if ( $valid->lines != 1 )
+{
+  echo "ERROR KEY NOT VALID\n";
+  exit();
+}
+
+$req = new requete($db,"SELECT * FROM mailing");
+while ( $row = $req->get_row() )
+{
+    if ($row["role"] >= 2) {
+        $ml_bureau[$row["nom_unix_asso"]] .= $row["email_utl"] . " ";
+    }
+    $ml_membre[$row["nom_unix_asso"]] .= $row["email_utl"] . " ";
+}
+foreach ($ml_bureau as $k => $v) {
+    echo $k . ".bureau: ".$v;
+    echo "\n";
+}
+foreach ($ml_membre as $k => $v) {
+    echo $k . ".membres: ".$v;
+    echo "\n";
+}
+
+?>
