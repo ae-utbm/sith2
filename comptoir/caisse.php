db258e38143fc46b23a1aaa81c4126d89606e3d7
Krophil <pierre.brunet@krophil.fr>
2015-10-05 11:00:38 +0200

    correction d'ambiguïté sur la caisse du foyer. Voir si j'ai pas fait de la merde

--- comptoir/caisse.php
+++ comptoir/caisse.php
@@ -231,7 +231,14 @@ elseif ($_REQUEST['action'] == "new")
 
     if ($user->is_in_group("gestion_syscarteae"))
     {
-      $frm->add_checkbox("caisse_videe", "Caisse vidée");
+      if (id_comptoir=2)
+      {
+        $frm->add_checkbox("caisse_videe", "Coffre vidé");
+      }
+      else 
+      {
+        $frm->add_checkbox("caisse_videe", "Caisse vidée");
+      }
     }
 
     $frm->add_text_area("comment", "Commentaire", "", 60, 12);
