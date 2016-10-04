b7914835e6957b45d17d27fef8a6c678bfb272a8
Skia <lordbanana25@mailoo.org>
2016-10-04 17:17:11 +0200

    Redirect the accounting

--- compta/index.php
+++ compta/index.php
@@ -20,6 +20,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
  * 02111-1307, USA.
  */
+header("Location: https://ae2.utbm.fr/user/tools");
 $topdir="../";
 
 require_once("include/compta.inc.php");
