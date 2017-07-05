5a270dd749882b1f9ef6dd1a86135ceaacfc5514
Krophil <pierre.brunet@krophil.fr>
2017-07-05 23:28:07 +0200

    Ragnarök: it's time forum2...

--- forum2/search.php
+++ forum2/search.php
@@ -67,6 +67,8 @@ $forum->load_by_id(1);
 
 if ( $_REQUEST["page"] == "unread" )
 {
+  header("Location: https://ae2.utbm.fr/forum/last_unread")
+
   $site->allow_only_logged_users("forum");
 
   $site->start_page("forum","Messages non lus");
@@ -183,6 +185,8 @@ if ( $_REQUEST["page"] == "unread" )
 }
 elseif ( $_REQUEST["page"] == "starred" )
 {
+  header("Location: https://ae2.utbm.fr/forum")
+
   $site->allow_only_logged_users("forum");
 
   $site->start_page("forum","Sujets favoris");
